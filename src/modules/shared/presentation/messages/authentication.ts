4b9476af05a8c1625964d60ce970bbd47400fe9f
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-02-25 18:01:54 +0000

    feat: Mensagens do sistema para seção de autenticação baseadas no documento de mensageiria

---
+++ src/modules/shared/presentation/messages/authentication.ts
@@ -0,0 +1,22 @@
+type MessagesAuthenticationType = {
+  [key: string]: string
+}
+
+export const MESSAGES_AUTHENTICATION: MessagesAuthenticationType = {
+  '1.1': 'Bem-vindo de volta!',
+  '1.2':
+    'Acesse sua conta para continuar. Insira suas credenciais abaixo e aproveite todos os recursos.',
+  '1.3': 'O nome de usuário é obrigatório.',
+  '1.4': 'A senha é obrigatória.',
+  '1.5':
+    'Nome de usuário ou senha inválidos. Verifique os dados e tente novamente.',
+  '1.6': '© Copyright Traffic Safe Tecnologia S/A',
+  '1.7': 'O campo de nome de usuário é obrigatório.',
+  '1.8': 'O campo de senha é obrigatório.',
+  '1.9': 'A senha pode ter no máximo 64 caracteres.',
+  '1.10': 'A senha deve conter pelo menos uma letra maiúscula.',
+  '1.11': 'A senha deve conter pelo menos uma letra minúscula.',
+  '1.12': 'A senha deve conter pelo menos um número.',
+  '1.13':
+    'A senha deve conter pelo menos um caractere especial (#, @, $, !, %, *, ?, &).'
+}
