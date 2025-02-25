fbcc539565a259fe202c89e2ccb1f80b9aec02aa
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-02-25 17:47:47 +0000

    feat: esquema para validação do zod

---
+++ src/modules/authentication/presentation/schemas/authentication-form.schema.ts
@@ -0,0 +1,28 @@
+import { MESSAGES_AUTHENTICATION } from '@/modules/shared/presentation/messages/authentication'
+import { z } from 'zod'
+
+export const AuthenticationFormSchema = z.object({
+  username: z.string().nonempty({
+    message: MESSAGES_AUTHENTICATION['1.7']
+  }),
+  password: z
+    .string()
+    .nonempty({
+      message: MESSAGES_AUTHENTICATION['1.8']
+    })
+    .max(64, {
+      message: MESSAGES_AUTHENTICATION['1.9']
+    })
+    .regex(/[A-Z]/, {
+      message: MESSAGES_AUTHENTICATION['1.10']
+    })
+    .regex(/[a-z]/, {
+      message: MESSAGES_AUTHENTICATION['1.11']
+    })
+    .regex(/[0-9]/, {
+      message: MESSAGES_AUTHENTICATION['1.12']
+    })
+    .regex(/[#@$!%*?&]/, {
+      message: MESSAGES_AUTHENTICATION['1.13']
+    })
+})
