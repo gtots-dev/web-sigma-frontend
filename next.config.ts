4ee64b36c10f28ddc8f356c64d7ba2632bfada0e
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-02-25 17:41:14 +0000

    feat: Variáveis de ambiente para as imagens utilizadas na seção de autenticação

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   env: {
-    REDIRECT_ROUTER_DEFAULT: process.env.REDIRECT_ROUTER_DEFAULT
+    REDIRECT_ROUTER_DEFAULT: process.env.REDIRECT_ROUTER_DEFAULT,
+    BANNER_IMAGE_SRC: process.env.BANNER_IMAGE_SRC,
+    LIGHT_LOGO_SRC: process.env.LIGHT_LOGO_SRC,
+    DARK_LOGO_SRC: process.env.DARK_LOGO_SRC,
+    MOBILE_LIGHT_LOGO_SRC: process.env.MOBILE_LIGHT_LOGO_SRC,
+    MOBILE_DARK_LOGO_SRC: process.env.MOBILE_DARK_LOGO_SRC,
   },
   async redirects() {
     return [
