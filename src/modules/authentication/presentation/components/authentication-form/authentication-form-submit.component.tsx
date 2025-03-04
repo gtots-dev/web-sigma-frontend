4aaf58f888a2e782da5ac62c0f6e7c983e12eab5
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-03-04 08:57:49 -0300

    feat: Componentes Mensagem de erro e Carregamento para o botão de autenticar

--- src/modules/authentication/presentation/components/authentication-form/authentication-form-submit.component.tsx
+++ src/modules/authentication/presentation/components/authentication-form/authentication-form-submit.component.tsx
@@ -1,20 +1,27 @@
 'use client'
 
 import { Button } from '@/modules/shared/presentation/components/shadcn/button'
+import { ErrorMessageComponent } from '@/modules/shared/presentation/components/error-message/error-message.component'
 import { useFormContext } from 'react-hook-form'
 import { useAuthenticationFormSubmitHook } from '../../hooks/use-authentication-form-submit.hook'
 import type { AuthenticationFormType } from '../../schemas/authentication-form.schema'
+import { LoadingSpinComponent } from '@/modules/shared/presentation/components/loading-spin/loading-spin.component'
 
 export function AuthenticationFormInputSubmitComponent() {
   const { handleSubmit } = useFormContext<AuthenticationFormType>()
-  const { onSubmit } = useAuthenticationFormSubmitHook()
+  const { onSubmitSignIn, loading, error } = useAuthenticationFormSubmitHook()
   return (
-    <Button
-      className="w-full text-sm bg-primary-600 text-zinc-50 hover:bg-primary-600/90"
-      type="submit"
-      onClick={handleSubmit(onSubmit)}
-    >
-      Acessar
-    </Button>
+    <div className="flex flex-col gap-y-3">
+      <ErrorMessageComponent error={error} />
+      <Button
+        disabled={loading}
+        className="w-full text-sm bg-primary-600 text-zinc-50 hover:bg-primary-600/90"
+        type="submit"
+        onClick={handleSubmit(onSubmitSignIn)}
+      >
+        <LoadingSpinComponent loading={loading} />
+        Acessar
+      </Button>
+    </div>
   )
 }
