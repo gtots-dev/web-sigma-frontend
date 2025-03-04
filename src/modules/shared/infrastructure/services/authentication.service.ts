9e38aa290a82779202343815e2d34c582e36b123
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-03-04 14:10:48 -0300

    feat: Serviço de autenticação do Auth.JS

--- src/modules/shared/infrastructure/services/authentication.service.ts
+++ src/modules/shared/infrastructure/services/authentication.service.ts
@@ -2,9 +2,9 @@ import { signIn, signOut } from 'next-auth/react'
 import { redirect } from 'next/navigation'
 import { MESSAGES_AUTHENTICATION } from '@/modules/shared/presentation/messages/authentication'
 import type { AuthenticationFormType } from '@/modules/authentication/presentation/schemas/authentication-form.schema'
+import { PATHNAMES } from '../config/pathnames.config'
 
 export const authenticationService = {
-  
   async signIn(data: AuthenticationFormType): Promise<string | null> {
     const { code, error } = await signIn('credentials', {
       ...data,
@@ -12,12 +12,12 @@ export const authenticationService = {
     })
 
     if (error) return authenticationService.getErrorMessage(code)
-    redirect('/system')
+    redirect(PATHNAMES.SYSTEM)
   },
 
   async signOut(): Promise<void> {
     await signOut({ redirect: false })
-    redirect(process.env.REDIRECT_ROUTER_DEFAULT)
+    redirect(PATHNAMES.AUTHENTICATION)
   },
 
   getErrorMessage(code: string): string | null {
