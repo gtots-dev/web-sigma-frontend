eaceaa9d53f0506d64e4a37fe39967f85625a4d8
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-03-04 08:16:57 -0300

    feat: Interface de carregamento

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+export default function Loading() {
+  return (
+    <main className="grid place-content-center h-svh w-screen">
+      <img
+        src="/logos/gtots_principal_hor.svg"
+        className="h-[100px] aspect-auto animate-pulse duration-1000"
+      />
+    </main>
+  )
+}
