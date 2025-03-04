c79c05dca2e33d64f4f6787502e2b7611ff36887
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-03-04 11:18:21 -0300

    feat: Variáveis de ambiente referentes as rotas da aplicação

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   env: {
     REDIRECT_ROUTER_DEFAULT: process.env.REDIRECT_ROUTER_DEFAULT,
+    AUTHENTICATION_ROUTE_PATH: process.env.AUTHENTICATION_ROUTE_PATH,
+    SYSTEM_ROUTE_PATH: process.env.SYSTEM_ROUTE_PATH,
     HOST_API: process.env.HOST_API,
     PATH_LOGIN_FOR_ACCESS_TOKEN: process.env.PATH_LOGIN_FOR_ACCESS_TOKEN
   },
