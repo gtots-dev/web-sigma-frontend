d8995d99d50d57fe9b1d1898c79923f4a219610c
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-03-04 08:28:44 -0300

    feat: Camada HTTP usando Axios

---
+++ src/modules/shared/infrastructure/http/axios-http-client.ts
@@ -0,0 +1,31 @@
+import type { AxiosInstance } from 'axios'
+import type { HttpRequestConfig } from '../../domain/types/http-request-config.type'
+import axios from 'axios'
+import type { HttpResponse } from '../../domain/types/http-response.type'
+import type { HttpClientInterface } from '../../domain/interfaces/http-client.interface'
+
+export class AxiosHttpClient implements HttpClientInterface {
+  private axiosInstance: AxiosInstance
+
+  constructor(baseURL: string) {
+    this.axiosInstance = axios.create({ baseURL })
+  }
+
+  async request<T>(config: HttpRequestConfig): Promise<HttpResponse<T>> {
+    try {
+      const response = await this.axiosInstance.request<T>(config)
+      return {
+        success: true,
+        status: String(response.status),
+        data: response.data
+      }
+    } catch (error: any) {
+      return {
+        success: false,
+        status: String(error.response?.status) || '500',
+        data: null as any,
+        message: error.message || 'Unexpected error occurred'
+      }
+    }
+  }
+}
