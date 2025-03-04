0c50300c3d5cfc9683e7a1e17cfb8e8d4018e215
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-03-04 09:20:28 -0300

    feat: Funções de "Sign-in" e "sign-out" para o "hook" de "submit"

--- src/modules/authentication/presentation/hooks/use-authentication-form-submit.hook.ts
+++ src/modules/authentication/presentation/hooks/use-authentication-form-submit.hook.ts
@@ -1,7 +1,28 @@
+import { useState } from 'react'
 import type { AuthenticationFormType } from '../schemas/authentication-form.schema'
+import { authenticationService } from '@/modules/shared/infrastructure/services/authentication.service'
 
 export function useAuthenticationFormSubmitHook() {
-  const onSubmit = (data: AuthenticationFormType) =>
-    console.log('Form data from SubmitButton:', data)
-  return { onSubmit }
+  const [error, setError] = useState<string | null>(null)
+  const [loading, setLoading] = useState<boolean>(false)
+
+  const onSubmitSignIn = async (
+    data: AuthenticationFormType
+  ): Promise<void> => {
+    setError(null)
+    setLoading(true)
+
+    const errorMessage = await authenticationService.signIn(data)
+
+    if (errorMessage) setError(errorMessage)
+    setLoading(false)
+  }
+
+  const onSubmitSignOut = async (): Promise<void> => {
+    setLoading(true)
+    await authenticationService.signOut()
+    setLoading(false)
+  }
+
+  return { onSubmitSignIn, onSubmitSignOut, error, loading }
 }
