57e77b5434df8814f4520d51bb1c2608ce111f37
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-03-07 10:38:46 -0300

    feat: Componente de menu lateral do sistema

---
+++ src/modules/system/presentation/components/sidebar-system/sidebar-system-header.component.tsx
@@ -0,0 +1,28 @@
+import { SidebarHeader } from '@/modules/shared/presentation/components/shadcn/sidebar'
+import { ThemeLogoComponent } from '@/modules/shared/presentation/components/theme-logo/theme-logo.component'
+import { ThemeToggle } from '@/modules/shared/presentation/components/theme-toggle/theme-toggle.component'
+
+interface SidebarSystemHeaderComponentProps {}
+
+
+export function SidebarSystemHeaderComponent({}: SidebarSystemHeaderComponentProps) {
+  const images = {
+    lightLogoSrc: '/logos/gtots_principal_hor.svg',
+    darkLogoSrc: '/logos/gtots_branco_hor.svg',
+    lightLogoAlt: 'Logotipo claro da GTOTS',
+    darkLogoAlt: 'Logotipo escuro da GTOTS'
+  }
+
+  return (
+    <SidebarHeader className="flex flex-row justify-between h-16 p-3 border-b shrink-0 items-center">
+      <ThemeLogoComponent
+        className="h-9"
+        logoLightSrc={images.lightLogoSrc}
+        logoLightAlt={images.lightLogoAlt}
+        logoDarkSrc={images.darkLogoSrc}
+        logoDarkAlt={images.darkLogoAlt}
+      />
+      <ThemeToggle />
+    </SidebarHeader>
+  )
+}
