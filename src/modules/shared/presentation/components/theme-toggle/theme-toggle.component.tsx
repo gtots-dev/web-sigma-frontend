8b50c31bc664e8cbcd548b765a000e89d49bc4c9
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-03-07 10:50:55 -0300

    feat: Componente de alternador de temas claro e escuro

---
+++ src/modules/shared/presentation/components/theme-toggle/theme-toggle.component.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import * as React from 'react'
+import { Moon, Sun } from 'lucide-react'
+import { useTheme } from 'next-themes'
+import { Button } from '../shadcn/button'
+
+export function ThemeToggle() {
+  const { setTheme, theme } = useTheme()
+
+  return (
+    <Button
+    className='bg-transparent'
+      variant="outline"
+      size="icon"
+      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+    >
+      <Sun className="h-[1.5rem] w-[1.3rem] hidden dark:block" />
+      <Moon className="h-5 w-5 dark:hidden" />
+    </Button>
+  )
+}
