5c69483575057ab666aa9f2917e6c4824948333b
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-03-12 16:57:41 -0300

    feat: Chave secreta para descriptografar a JWT

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   env: {
     HOST_API: process.env.HOST_API,
     AUTH_SECRET: process.env.AUTH_SECRET,
+    SECRET_KEY_ACCESS_TOKEN: process.env.SECRET_KEY_ACCESS_TOKEN
   },
   async redirects() {
     return [
