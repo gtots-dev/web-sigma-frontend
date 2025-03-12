1363c0e3502c8b59863c4674dbd7863e37ca4853
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-03-12 19:38:58 -0300

    feat: Teste do conversor de json para form data

---
+++ src/modules/shared/infrastructure/services/__tests__/convert-josn-to-form-data.test.ts
@@ -0,0 +1,29 @@
+import type { FormDataConverterInterface } from '@/modules/shared/domain/interfaces/form-data-converter.interface'
+import { ConvertJsonToFormData } from '../convert-json-to-form-data.service'
+
+describe('ConvertJsonToFormData', () => {
+  let formDataConverterMock: jest.Mocked<FormDataConverterInterface>
+  let convertJsonToFormData: ConvertJsonToFormData
+
+  beforeEach(() => {
+    formDataConverterMock = {
+      convert: jest.fn()
+    } as jest.Mocked<FormDataConverterInterface>
+    convertJsonToFormData = new ConvertJsonToFormData(formDataConverterMock)
+  })
+
+ test('should call formDataConverter.convert with correct data and return the result', () => {
+    const mockData = { key: 'value', anotherKey: 42 }
+    
+    const mockFormData = new FormData()
+    mockFormData.append('key', 'value')
+    mockFormData.append('anotherKey', '42')
+
+    formDataConverterMock.convert.mockReturnValue(mockFormData)
+
+    const result = convertJsonToFormData.execute(mockData)
+
+    expect(formDataConverterMock.convert).toHaveBeenCalledWith(mockData)
+    expect(result).toBe(mockFormData)
+  })
+})
