15ad277ffc776448ab22f10d977bc5185e4b3d89
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-03-13 08:56:40 -0300

    fix: Ao passar o cursor sobre o botão ativo da sidebar no modo escuro mudava a estilização

--- src/modules/system/presentation/components/sidebar-system/sidebar-system-item-grandchild.component.tsx
+++ src/modules/system/presentation/components/sidebar-system/sidebar-system-item-grandchild.component.tsx
@@ -24,7 +24,7 @@ export function SidebarSystemItemGrandchildComponent({
   const buttonClassNames = clsx(
     'flex h-7 w-full mb-1 min-w-0 items-center gap-2 overflow-hidden rounded-md px-2 [&>span:last-child]:truncate [&>svg]:size-4 hover:bg-sidebar-accent hover:text-sidebar-accent-foreground ',
     {
-      'disabled:opacity-100 bg-primary-600 text-white [&>svg]:text-white hover:bg-primary-600 hover:text-white active:bg-primary-600 active:text-white':
+      'disabled:opacity-100 bg-primary-600 text-white [&>svg]:text-white hover:bg-primary-600 hover:text-white dark:hover:bg-primary-600 dark:hover:text-white active:bg-primary-600 active:text-white':
         isActive,
       'hover:underline hover:underline-offset-2 cursor-pointer': !isActive
     }
@@ -39,7 +39,7 @@ export function SidebarSystemItemGrandchildComponent({
 
   return (
     <Collapsible open={isOpen} onOpenChange={setIsOpen} className="h-full">
-      <SidebarMenuSubItem className="w-full mb-1">
+      <SidebarMenuSubItem className="w-full">
         <div className={buttonClassNames}>
           <Button className='justify-start w-full px-0 hover:bg-transparent disabled:opacity-100' variant="ghost" disabled={isActive} onClick={handleClick}>
             {item.icon && <item.icon />}
