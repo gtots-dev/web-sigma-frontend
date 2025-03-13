5b7471bf95726f918c20a9cdb47935da86798e81
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-03-13 12:49:00 -0300

    refactor: Utilização das classes de estilo

--- src/modules/system/presentation/components/sidebar-system/sidebar-system-item-grandchild.component.tsx
+++ src/modules/system/presentation/components/sidebar-system/sidebar-system-item-grandchild.component.tsx
@@ -21,36 +21,40 @@ export function SidebarSystemItemGrandchildComponent({
   const { handleClick, isActive } = useSidebarSystemItem(item)
   const [isOpen, setIsOpen] = useState(true)
 
-  const buttonClassNames = clsx(
-    'flex h-7 w-full mb-1 min-w-0 items-center gap-2 overflow-hidden rounded-md px-2 [&>span:last-child]:truncate [&>svg]:size-4 hover:bg-sidebar-accent hover:text-sidebar-accent-foreground ',
-    {
-      'disabled:opacity-100 bg-primary-600 text-white [&>svg]:text-white hover:!bg-primary-600 hover:!text-white dark:hover:!bg-primary-600 dark:!hover:text-white active:bg-primary-600 active:text-white':
-        isActive,
-      'hover:underline hover:underline-offset-2 cursor-pointer': !isActive
-    }
+  const sidebarButtonClass = clsx(
+    'flex h-7 w-full mb-1 min-w-0 items-center gap-2 overflow-hidden rounded-md px-2 [&>span:last-child]:truncate [&>svg]:size-4',
+    'hover:bg-sidebar-accent hover:text-sidebar-accent-foreground',
+    isActive
+      ? 'bg-primary-600 text-white [&>svg]:text-white hover:!bg-primary-600 hover:!text-white dark:hover:!bg-primary-600 dark:hover:!text-white active:bg-primary-600 active:text-white'
+      : 'hover:underline hover:underline-offset-2 cursor-pointer'
   )
 
   const rotateArrowClassNames = clsx(
     'ml-auto transition-transform duration-200 h-5 w-5 hover:opacity-50',
-    {
-      'rotate-90': isOpen
-    }
+    { 'rotate-90': isOpen }
   )
 
   return (
     <Collapsible open={isOpen} onOpenChange={setIsOpen} className="h-full">
       <SidebarMenuSubItem className="w-full">
-        <div className={buttonClassNames}>
-          <Button className='justify-start w-full px-0 hover:bg-transparent disabled:opacity-100' variant="ghost" disabled={isActive} onClick={handleClick}>
+        <div className={sidebarButtonClass}>
+          <Button
+            className="justify-start w-full px-0 disabled:opacity-100"
+            variant="ghost"
+            disabled={isActive}
+            onClick={handleClick}
+          >
             {item.icon && <item.icon />}
             <span>{item.title}</span>
           </Button>
-          <CollapsibleTrigger className="group aspect-square">
-            {item.items && <ChevronRight className={rotateArrowClassNames} />}
-          </CollapsibleTrigger>
-        </div>
-        <CollapsibleContent className="h-full">
           {item.items && (
+            <CollapsibleTrigger className="group aspect-square">
+              <ChevronRight className={rotateArrowClassNames} />
+            </CollapsibleTrigger>
+          )}
+        </div>
+        {item.items && (
+          <CollapsibleContent className="h-full">
             <SidebarMenuSub className="h-full !me-0 pe-0">
               {item.items.map((subItem) => (
                 <SidebarSystemItemGrandchildComponent
@@ -60,8 +64,8 @@ export function SidebarSystemItemGrandchildComponent({
                 />
               ))}
             </SidebarMenuSub>
-          )}
-        </CollapsibleContent>
+          </CollapsibleContent>
+        )}
       </SidebarMenuSubItem>
     </Collapsible>
   )
