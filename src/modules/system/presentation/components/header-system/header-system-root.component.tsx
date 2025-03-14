bf3889afdfbbbffd82cceba14d4fe99bfc8ff985
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-03-14 10:15:27 -0300

    fix: Fundo transparente para o cabeçalho do sistema

--- src/modules/system/presentation/components/header-system/header-system-root.component.tsx
+++ src/modules/system/presentation/components/header-system/header-system-root.component.tsx
@@ -8,7 +8,7 @@ export default function HeaderSystemRootComponent({
   children
 }: HeaderSystemRootComponentProps) {
   return (
-    <header className="fixed w-full flex h-16 border-b shrink-0 items-center gap-2 px-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+    <header className="fixed w-full flex h-16 border-b shrink-0 items-center gap-2 px-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 bg-white dark:bg-zinc-950">
       {children}
     </header>
   )
