080ca9055d87d0bcd8e55e25509ef9c1bf9c2e90
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-03-25 10:27:29 -0300

    feat: Componente de cartão (Container) para seção de opções de operação

---
+++ src/modules/operation-options/presentation/components/card-option/index.ts
@@ -0,0 +1,13 @@
+import { CardOptionDescriptionComponent } from "./card-option-description.components";
+import { CardOptionFooterComponent } from "./card-option-footer.components";
+import { CardOptionHeaderComponent } from "./card-option-header.components";
+import { CardOptionRootComponent } from "./card-option-root.components";
+import { CardOptionTitleComponent } from "./card-option-title.components";
+
+export const CardOption = {
+  Root: CardOptionRootComponent,
+  Header: CardOptionHeaderComponent,
+  Footer: CardOptionFooterComponent,
+  Title: CardOptionTitleComponent,
+  Description: CardOptionDescriptionComponent,
+}
