54324382e4419fb635066138c2b1231673fa431c
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-03-25 10:28:59 -0300

    feat: componente de cabeçalho para seção de opções de operação

---
+++ src/modules/operation-options/presentation/components/header-options/header-options-root.components.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from 'react'
+
+interface HeaderOptionsRootComponentProps {
+  children: ReactNode
+}
+
+export function HeaderOptionsRootComponent({
+  children
+}: HeaderOptionsRootComponentProps) {
+  return (
+    <header className="flex flex-col justify-items-center justify-center be gap-5 w-full xl:flex-row xl:items-center xl:justify-between mt-6">
+      {children}
+    </header>
+  )
+}
