8ebf05b3722e8246a94434d12ecc30680c9c1067
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-03-25 10:30:51 -0300

    feat: Pagina de opções de operação

---
+++ src/app/system/operations/operation-options/page.tsx
@@ -0,0 +1,90 @@
+import { CardOperationOptions } from '@/modules/operation-options/presentation/components/card-operation-options'
+import { CardOption } from '@/modules/operation-options/presentation/components/card-option'
+import { HeaderOptions } from '@/modules/operation-options/presentation/components/header-options'
+import { SelectOperation } from '@/modules/operation-options/presentation/components/select-operation'
+import { PATHNAMES } from '@/modules/shared/infrastructure/config/pathnames.config'
+import { MESSAGES_OPTIONS_OPERATION } from '@/modules/shared/presentation/messages/options-operation'
+import { FileKey2, FileText, UsersRound, type LucideIcon } from 'lucide-react'
+
+interface OperationOptions {
+  title: string
+  description: string
+  icon: LucideIcon
+  pathName: string
+}
+
+interface Data {
+  title: string
+  description: string
+  subDescription: string
+  operationOptions: OperationOptions[]
+}
+
+export default async function OperationOptionsPage({
+  searchParams
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+}) {
+  const { operation, id } = await searchParams
+
+  const data: Data = {
+    title: MESSAGES_OPTIONS_OPERATION['11.1'],
+    description: MESSAGES_OPTIONS_OPERATION['11.2'],
+    subDescription: MESSAGES_OPTIONS_OPERATION[11.3],
+    operationOptions: [
+      {
+        title: MESSAGES_OPTIONS_OPERATION[11.4],
+        description: MESSAGES_OPTIONS_OPERATION[11.5],
+        pathName: PATHNAMES.CONTRACTS,
+        icon: FileText,
+      },
+      {
+        title: MESSAGES_OPTIONS_OPERATION[11.6],
+        description: MESSAGES_OPTIONS_OPERATION[11.7],
+        pathName: PATHNAMES.USERS,
+        icon: UsersRound,
+      },
+      {
+        title: MESSAGES_OPTIONS_OPERATION[11.8],
+        description: MESSAGES_OPTIONS_OPERATION[11.9],
+        pathName: PATHNAMES.PERMISSIONS,
+        icon: FileKey2,
+      }
+    ]
+  }
+
+  return (
+    <CardOperationOptions.Root>
+      <CardOperationOptions.Header>
+        <HeaderOptions.Root>
+          <div className="flex flex-col gap-1">
+            <HeaderOptions.Title>{data.title}</HeaderOptions.Title>
+            <HeaderOptions.Description>
+              {data.description}
+            </HeaderOptions.Description>
+            <HeaderOptions.Description>
+              {data.subDescription}
+              <b className="text-primary-300 font-medium underline underline-offset-4">
+                {operation}
+              </b>
+            </HeaderOptions.Description>
+          </div>
+          <SelectOperation />
+        </HeaderOptions.Root>
+      </CardOperationOptions.Header>
+      <CardOperationOptions.Content>
+        {data.operationOptions.map((options, index) => (
+          <CardOption.Root key={index} pathName={options.pathName}>
+            <CardOption.Header Icon={options.icon} />
+            <CardOption.Footer>
+              <CardOption.Title>{options.title}</CardOption.Title>
+              <CardOption.Description>
+                {options.description}
+              </CardOption.Description>
+            </CardOption.Footer>
+          </CardOption.Root>
+        ))}
+      </CardOperationOptions.Content>
+    </CardOperationOptions.Root>
+  )
+}
