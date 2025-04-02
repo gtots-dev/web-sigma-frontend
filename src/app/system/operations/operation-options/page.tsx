a4170b9849c66686ce15ce3f6da89b5e1c572de5
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-04-02 15:29:00 -0300

    feat: Exibição do nome da operação selecionada

--- src/app/system/operations/operation-options/page.tsx
+++ src/app/system/operations/operation-options/page.tsx
@@ -1,7 +1,11 @@
+'use client'
+
+import { useEffect, useState } from 'react'
 import { CardOperationOptions } from '@/modules/operation-options/presentation/components/card-operation-options'
 import { CardOption } from '@/modules/operation-options/presentation/components/card-option'
 import { HeaderOptions } from '@/modules/operation-options/presentation/components/header-options'
 import { SelectOperation } from '@/modules/operation-options/presentation/components/select-operation'
+import { useSelectionOperation } from '@/modules/operations/presentation/hooks/use-selection-operation.hook'
 import { PATHNAMES } from '@/modules/shared/infrastructure/config/pathnames.config'
 import { MESSAGES_OPTIONS_OPERATION } from '@/modules/shared/presentation/messages/options-operation'
 import { FileKey2, FileText, UsersRound, type LucideIcon } from 'lucide-react'
@@ -20,35 +24,42 @@ interface Data {
   operationOptions: OperationOptions[]
 }
 
-export default async function OperationOptionsPage({
-  searchParams
-}: {
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
-}) {
-  const { operation, id } = await searchParams
+export default function OperationOptionsPage() {
+  const { getOperation } = useSelectionOperation()
+  const [operationName, setOperationName] = useState<string | undefined>(
+    undefined
+  )
+  const fetchOperation = async () => {
+    const { name } = await getOperation()
+    setOperationName(name)
+  }
+
+  useEffect(() => {
+    fetchOperation()
+  }, [getOperation])
 
   const data: Data = {
     title: MESSAGES_OPTIONS_OPERATION['11.1'],
     description: MESSAGES_OPTIONS_OPERATION['11.2'],
-    subDescription: MESSAGES_OPTIONS_OPERATION[11.3],
+    subDescription: MESSAGES_OPTIONS_OPERATION['11.3'],
     operationOptions: [
       {
-        title: MESSAGES_OPTIONS_OPERATION[11.4],
-        description: MESSAGES_OPTIONS_OPERATION[11.5],
+        title: MESSAGES_OPTIONS_OPERATION['11.4'],
+        description: MESSAGES_OPTIONS_OPERATION['11.5'],
         pathName: PATHNAMES.CONTRACTS,
-        icon: FileText,
+        icon: FileText
       },
       {
-        title: MESSAGES_OPTIONS_OPERATION[11.6],
-        description: MESSAGES_OPTIONS_OPERATION[11.7],
+        title: MESSAGES_OPTIONS_OPERATION['11.6'],
+        description: MESSAGES_OPTIONS_OPERATION['11.7'],
         pathName: PATHNAMES.USERS,
-        icon: UsersRound,
+        icon: UsersRound
       },
       {
-        title: MESSAGES_OPTIONS_OPERATION[11.8],
-        description: MESSAGES_OPTIONS_OPERATION[11.9],
+        title: MESSAGES_OPTIONS_OPERATION['11.8'],
+        description: MESSAGES_OPTIONS_OPERATION['11.9'],
         pathName: PATHNAMES.PERMISSIONS,
-        icon: FileKey2,
+        icon: FileKey2
       }
     ]
   }
@@ -65,7 +76,7 @@ export default async function OperationOptionsPage({
             <HeaderOptions.Description>
               {data.subDescription}
               <b className="text-primary-300 font-medium underline underline-offset-4">
-                {operation}
+                {operationName}
               </b>
             </HeaderOptions.Description>
           </div>
