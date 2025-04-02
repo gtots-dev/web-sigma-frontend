a73acec73a97a1aa8141c465af89b18130c23507
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-04-02 15:28:07 -0300

    feat: LIsta estática de operações disponíveis

--- src/app/system/operations/page.tsx
+++ src/app/system/operations/page.tsx
@@ -1,16 +1,23 @@
-'use client'
-
 import { HeaderOperation } from '@/modules/operations/presentation/components/header-operation'
 import { TableOperations } from '@/modules/operations/presentation/components/table-operations'
 import { Separator } from '@/modules/shared/presentation/components/shadcn/separator'
 import { MESSAGES_OPERATIONS } from '@/modules/shared/presentation/messages/operations'
+import { useJwtInfo } from '@/modules/system/presentation/hooks/use-jwt-Info.hook'
 
 interface Data {
   title: string
   description: string
 }
 
-export default function OperationsPage() {
+export default async function OperationsPage() {
+  const { operation_ids } = await useJwtInfo()
+  const listModifier = operation_ids.map((id) => {
+    return {
+      name: `Operation ${id}`,
+      id: String(id)
+    }
+  })
+
   const data: Data = {
     title: MESSAGES_OPERATIONS['4.1'],
     description: MESSAGES_OPERATIONS['4.2']
@@ -25,13 +32,7 @@ export default function OperationsPage() {
         </HeaderOperation.Description>
       </HeaderOperation.Root>
       <Separator orientation="horizontal" />
-      <TableOperations.Root
-        limitTo={11}
-        data={[
-          { name: 'São Paulo', id: crypto.randomUUID() },
-          { name: 'Rio de Janeiro', id: crypto.randomUUID() }
-        ]}
-      />
+      <TableOperations.Root limitTo={11} data={listModifier} />
     </main>
   )
 }
