bbcb537276b52add87000e6167ce618eaa8b3ab9
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-04-02 16:00:15 -0300

    feat: Fabrica para apagar a operação selecionada

---
+++ src/modules/operations/infrastructure/factories/delete-selection-operation-factory.ts
@@ -0,0 +1,11 @@
+import { HttpClientFactory } from '@/modules/shared/infrastructure/factories/http-client.factory'
+import { ExecuteRequestFactory } from '@/modules/shared/infrastructure/factories/request.factory'
+import { DeleteSelectionOperationService } from '../services/delete-operation.service'
+
+export class DeleteSelectionOperationFactory {
+  static create() {
+    const httpClient = HttpClientFactory.create('/')
+    const executeRequest = ExecuteRequestFactory.create(httpClient)
+    return new DeleteSelectionOperationService(executeRequest)
+  }
+}
