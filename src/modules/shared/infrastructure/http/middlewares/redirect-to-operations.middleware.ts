1bc0b301135e3d86c1683c4ed9a267f80fe7a804
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-04-02 15:36:48 -0300

    feat: Middleware de Seleção automática de operação

--- src/modules/shared/infrastructure/http/middlewares/redirect-to-operations.middleware.ts
+++ src/modules/shared/infrastructure/http/middlewares/redirect-to-operations.middleware.ts
@@ -1,15 +1,43 @@
 import { NextResponse, type NextRequest } from 'next/server'
-import { PATHNAMES } from '../../config/pathnames.config'
+import { getToken } from 'next-auth/jwt'
 
-export async function RedirectToOperationsMiddleware(
-  req: NextRequest
-) {
+import { PATHNAMES } from '../../config/pathnames.config'
+import { JwtTokenDecodeFactory } from '../../factories/jwt-decode.factory'
+import { SelectOperationFactory } from '@/modules/api/infrastructure/factories/select-operation.factory'
+import { OperationFactory } from '@/modules/operations/infrastructure/factories/operation.factory'
+
+export async function RedirectToOperationsMiddleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.AUTH_SECRET })
+
+  if (!token?.accessToken) return NextResponse.next()
+
+  const jwtDecode = JwtTokenDecodeFactory.create()
+  const { operation_ids } = jwtDecode.decode(token.accessToken)
+
+  if (!operation_ids?.length) return NextResponse.next()
+
   const currentPathname = req.nextUrl.pathname
-  const { SYSTEM, OPERATIONS } = PATHNAMES
+  const { SYSTEM, OPERATIONS, OPERATION_OPTIONS } = PATHNAMES
 
   if (currentPathname === SYSTEM) {
-    return NextResponse.redirect(new URL(OPERATIONS, req.url))
+    const response = NextResponse.redirect(
+      new URL(
+        operation_ids.length === 1 ? OPERATION_OPTIONS : OPERATIONS,
+        req.url
+      )
+    )
+
+    if (operation_ids.length === 1) {
+      const repository = SelectOperationFactory.create(req, response)
+      const operation = OperationFactory.create({
+        id: String(operation_ids[0]),
+        name: 'operation-automatic'
+      })
+      repository.saveToCookies(operation)
+    }
+
+    return response
   }
 
-  return null
+  return NextResponse.next()
 }
