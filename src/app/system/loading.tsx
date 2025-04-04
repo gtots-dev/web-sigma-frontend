f6e924e7b2e05ffef9cdcba0348a47e01e678511
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-04-04 14:54:52 -0300

    fix: Inserções de tributos para correção de SEO

--- src/app/system/loading.tsx
+++ src/app/system/loading.tsx
@@ -3,7 +3,10 @@ export default async function Loading() {
     <main className="grid place-content-center gap-3 h-full w-full shrink-0">
       <img
         src="/logos/gtots_principal_hor.svg"
-        className="h-[100px] aspect-auto animate-pulse duration-1000"
+        className="aspect-auto animate-pulse duration-1000"
+        height={100}
+        width="auto"
+        fetchPriority="high"
       />
     </main>
   )
