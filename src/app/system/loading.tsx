ca6b145e58098c3b02651cbb3fb1e6d8865d0b23
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-04-04 10:57:21 -0300

    fix: Uso de propriedades das imagens para melhoria de CEO

--- src/app/system/loading.tsx
+++ src/app/system/loading.tsx
@@ -3,6 +3,9 @@ export default async function Loading() {
     <main className="grid place-content-center gap-3 h-full w-full shrink-0">
       <img
         src="/logos/gtots_principal_hor.svg"
+        alt="Loading spinner"
+        width="100%"
+        height="100%"
         className="h-[100px] aspect-auto animate-pulse duration-1000"
       />
     </main>
