34ea31b0435b2d9e279281ec9417a54b3b74ce60
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-04-04 10:58:26 -0300

    feat: Fabrica de contração do Token

---
+++ src/modules/authentication/infrastructure/factories/token.factory.ts
@@ -0,0 +1,15 @@
+import { HttpClientFactory } from '@/modules/shared/infrastructure/factories/http-client.factory'
+import { ExecuteRequestFactory } from '@/modules/shared/infrastructure/factories/request.factory'
+import { TokenService } from '../services/token.service'
+import { JwtValidatorFactory } from '@/modules/shared/infrastructure/factories/jwt-validator.factory'
+
+export class TokenFactory {
+  static create() {
+    const httpClient = HttpClientFactory.create(process.env.HOST_API)
+    const executeRequest = ExecuteRequestFactory.create(httpClient)
+    const tokenValidator = JwtValidatorFactory.create(
+      process.env.SECRET_KEY_ACCESS_TOKEN
+    )
+    return new TokenService(executeRequest, tokenValidator)
+  }
+}
