816ea0e87f70d77c29d29cdbfef6cb2054a49f5f
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-04-04 11:08:54 -0300

    refactor: utilização do get operations para o retorno da operação selecionada

--- src/modules/shared/infrastructure/http/middlewares/redirect-to-operations.middleware.ts
+++ src/modules/shared/infrastructure/http/middlewares/redirect-to-operations.middleware.ts
@@ -1,12 +1,13 @@
 import { NextResponse, type NextRequest } from 'next/server'
 import { getToken } from 'next-auth/jwt'
-
 import { PATHNAMES } from '../../configs/pathnames.config'
 import { JwtTokenDecodeFactory } from '../../factories/jwt-decode.factory'
 import { SelectOperationFactory } from '@/modules/api/infrastructure/factories/select-operation.factory'
 import { OperationFactory } from '@/modules/operations/infrastructure/factories/operation.factory'
+import { GetOperationsFactory } from '@/modules/operations/infrastructure/factories/get-operations.factory'
 
 export async function RedirectToOperationsMiddleware(req: NextRequest) {
+  const getOperation = GetOperationsFactory.create()
   const token = await getToken({ req, secret: process.env.AUTH_SECRET })
 
   if (!token?.accessToken) return NextResponse.next()
@@ -27,13 +28,10 @@ export async function RedirectToOperationsMiddleware(req: NextRequest) {
       )
     )
 
-    if (operation_ids.length === 1) {
-      const repository = SelectOperationFactory.create(req, response)
-      const operation = OperationFactory.create({
-        id: String(operation_ids[0]),
-        name: 'operation-automatic'
-      })
-      repository.saveToCookies(operation)
+      if (operation_ids.length === 1) {
+      const operation = await getOperation.execute(operation_ids)
+      const repository = SelectOperationFactory.create(req, response)      
+      repository.saveToCookies(OperationFactory.create(operation[0]))
     }
 
     return response
