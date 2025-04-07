1faa07c6f338cf28af90949b7ee7cac6964de70d
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-04-07 11:41:32 -0300

    feat: Tipagem para o corpo da requisição

--- src/modules/shared/domain/interfaces/http-request-config.interface.ts
+++ src/modules/shared/domain/interfaces/http-request-config.interface.ts
@@ -1,8 +1,7 @@
-export interface HttpRequestConfig<TData = unknown, TParams = Record<string, string | number | boolean>> {
+export interface HttpRequestConfig<TData = unknown, TParams = unknown> {
   method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH'
   url: string
   data?: TData
   params?: TParams
   headers?: Record<string, string>
 }
-  
