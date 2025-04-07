da5ae93b0529adf8ac1170b1913ca691cc34e4cc
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-04-07 11:42:57 -0300

    refactor: Alterado o formato do hook para renderizar do lado so client side

--- src/modules/system/presentation/hooks/use-jwt-Info.hook.ts
+++ src/modules/system/presentation/hooks/use-jwt-Info.hook.ts
@@ -1,9 +1,29 @@
-import { JwtTokenDecodeFactory } from '@/modules/shared/infrastructure/factories/jwt-decode.factory';
-import { auth } from '@/auth';
-import type { JwtDecodeDataInterface } from '@/modules/shared/domain/interfaces/jwt-decode-data.interface';
+'use client'
 
-export async function useJwtInfo(): Promise<JwtDecodeDataInterface> {
-  const { accessToken } = await auth();
-  const JwtDecode = JwtTokenDecodeFactory.create();
-  return JwtDecode.decode(accessToken) as JwtDecodeDataInterface;
+import { useEffect, useState } from 'react'
+import { JwtTokenDecodeFactory } from '@/modules/shared/infrastructure/factories/jwt-decode.factory'
+import { useSession } from 'next-auth/react'
+import type { JwtDecodeDataInterface } from '@/modules/shared/domain/interfaces/jwt-decode-data.interface'
+
+export function useJwtInfo() {
+  const [jwtInfo, setJwtInfo] = useState<JwtDecodeDataInterface | null>(null)
+  const [error, setError] = useState<Error | null>(null)
+  const { data: session, status } = useSession()
+
+  useEffect(() => {
+    if (status !== 'authenticated' || !session?.accessToken) return
+
+    try {
+      const jwtDecoder = JwtTokenDecodeFactory.create()
+      const decoded = jwtDecoder.decode(session.accessToken)
+      setJwtInfo(decoded as JwtDecodeDataInterface)
+    } catch (err) {
+      setError(err as Error)
+    }
+  }, [session, status])
+
+  const loading =
+    status === 'loading' || (status === 'authenticated' && !jwtInfo && !error)
+
+  return { jwtInfo, loading, error }
 }
