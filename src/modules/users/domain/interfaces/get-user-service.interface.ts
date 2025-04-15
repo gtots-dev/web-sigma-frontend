79d96c3e2b615993556225b4f0efd176a17b3b14
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-04-15 05:28:14 -0300

    feat: Interface de implementação de serviço de pegar o usuário desejado

---
+++ src/modules/users/domain/interfaces/get-user-service.interface.ts
@@ -0,0 +1,9 @@
+import type { TokenEntities } from '@/modules/authentication/domain/entities/token.entity'
+import type { UserInterface } from './user.interface'
+
+export interface GetUserServiceInterface {
+  execute(
+    token: TokenEntities,
+    operationSelectedId: number,
+  ): Promise<UserInterface>
+}
