86f6250812c62420784955d930a76808f7ff2cd1
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-04-15 05:48:52 -0300

    refactor: Alterado nomenclatura condizente ao componente

---
+++ src/modules/operation-options/presentation/components/menu-select-operation/index.ts
@@ -0,0 +1,11 @@
+import { MenuSelectOperationRootComponent } from './menu-select-operation-root.components'
+import { MenuSelectOperationContentComponent } from './menu-select-operation-content.components'
+import { MenuSelectOperationFooterComponent } from './menu-select-operation-footer.components'
+import { MenuSelectOperationTriggerComponent } from './menu-select-operation-trigger.components'
+
+export const MenuSelectOperation = {
+  Root: MenuSelectOperationRootComponent,
+  Trigger: MenuSelectOperationTriggerComponent,
+  Content: MenuSelectOperationContentComponent,
+  Footer: MenuSelectOperationFooterComponent
+}
