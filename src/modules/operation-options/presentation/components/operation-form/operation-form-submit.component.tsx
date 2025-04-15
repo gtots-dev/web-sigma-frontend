4d015710fcc67d503b2f6e4a0d2b5d222cd7c627
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-04-15 09:43:41 -0300

    feat: Integração do toast para o botão de confirmar do menu

--- src/modules/operation-options/presentation/components/operation-form/operation-form-submit.component.tsx
+++ src/modules/operation-options/presentation/components/operation-form/operation-form-submit.component.tsx
@@ -1,32 +1,49 @@
-'use client'
+  'use client'
 
-import type { OperationInterface } from '@/modules/operations/domain/interfaces/operation.interface'
-import { useSetSelectOperation } from '@/modules/operations/presentation/hooks/use-set-selection-operation.hook'
-import { Button } from '@/modules/shared/presentation/components/shadcn/button'
-import { useFormContext } from 'react-hook-form'
+  import type { OperationInterface } from '@/modules/operations/domain/interfaces/operation.interface'
+  import { useSelectedOperation } from '@/modules/operations/presentation/hooks/use-get-selection-operation.hook'
+  import { useSetSelectOperation } from '@/modules/operations/presentation/hooks/use-set-selection-operation.hook'
+  import { useToast } from '@/modules/shared/presentation/components/hooks/use-toast'
+  import { Button } from '@/modules/shared/presentation/components/shadcn/button'
+  import { useFormContext } from 'react-hook-form'
 
-interface OperationFormSubmitComponentProps {
-  close: VoidFunction
-}
-
-export function OperationFormSubmitComponent({
-  close
-}: OperationFormSubmitComponentProps) {
-  const { setOperation } = useSetSelectOperation()
-  const { handleSubmit } = useFormContext()
-
-  const onSubmit = ({ operation }: { operation: OperationInterface }) => {
-    setOperation(operation)
-    close()
+  interface OperationFormSubmitComponentProps {
+    close: VoidFunction
   }
 
-  return (
-    <Button
-      className="w-[150px] text-sm bg-primary-600 text-zinc-50 hover:bg-primary-600/90"
-      type="submit"
-      onClick={handleSubmit(onSubmit)}
-    >
-      Confirmar
-    </Button>
-  )
-}
+  export function OperationFormSubmitComponent({
+    close
+  }: OperationFormSubmitComponentProps) {
+    const operationSelected = useSelectedOperation()
+    const { setOperation } = useSetSelectOperation()
+    const { toast } = useToast()
+    const { handleSubmit } = useFormContext()
+
+    const onSubmit = ({ operation }: { operation: OperationInterface }) => {
+      if (operation.id === operationSelected.id) {
+        return toast({
+          variant: 'destructive',
+          title: 'Operação já selecionada',
+          description: `A operação marcada já está selecionada. Escolha outra!`
+        })
+      }
+
+      setOperation(operation)
+      close()
+      toast({
+        variant: 'success',
+        title: 'Operação selecionada',
+        description: `Sua operação selecionada foi alterada para ${operation.name}`
+      })
+    }
+
+    return (
+      <Button
+        className="w-[150px] text-sm bg-primary-600 text-zinc-50 hover:bg-primary-600/90"
+        type="submit"
+        onClick={handleSubmit(onSubmit)}
+      >
+        Confirmar
+      </Button>
+    )
+  }
