a4cd48a0aa4801612f631957ac95ac65c377b52c
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-04-15 04:55:32 -0300

    fix: Removido o uso do singleton memorização de requisição

--- src/modules/shared/infrastructure/factories/http-client.factory.ts
+++ src/modules/shared/infrastructure/factories/http-client.factory.ts
@@ -1,12 +1,8 @@
 import { FetchHttpClient } from '../http/fetch/fetch-http-client'
 
 export class HttpClientFactory {
-  private static instance: FetchHttpClient | null = null
-
   static create(baseURL: string): FetchHttpClient {
-    if (!this.instance) {
-      this.instance = new FetchHttpClient(baseURL)
-    }
-    return this.instance
+    const client = new FetchHttpClient(baseURL)
+    return client
   }
 }
--- src/modules/shared/infrastructure/factories/request.factory.ts
+++ src/modules/shared/infrastructure/factories/request.factory.ts
@@ -2,12 +2,7 @@ import type { FetchHttpClient } from '../http/fetch/fetch-http-client'
 import { ExecuteRequest } from '../services/execute-request.service'
 
 export class ExecuteRequestFactory {
-  private static instance: ExecuteRequest | null = null
-
   static create(httpClient: FetchHttpClient): ExecuteRequest {
-    if (!this.instance) {
-      this.instance = new ExecuteRequest(httpClient)
-    }
-    return this.instance
+    return new ExecuteRequest(httpClient)
   }
 }
