5e242a94061ba21c0aae6b8d211c96d952c57a50
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-04-15 22:24:41 -0300

    refactor: Middleware de redirecionamento

--- src/modules/shared/infrastructure/http/middlewares/redirect-to-operations.middleware.ts
+++ src/modules/shared/infrastructure/http/middlewares/redirect-to-operations.middleware.ts
@@ -1,38 +1,44 @@
 import { type NextRequest, NextResponse } from 'next/server'
-import { PATHNAMES } from '../../configs/pathnames.config'
-import { JwtTokenDecodeFactory } from '../../factories/jwt-decode.factory'
 import { SelectOperationFactory } from '@/modules/api/infrastructure/factories/select-operation.factory'
 import { OperationFactory } from '@/modules/operations/infrastructure/factories/operation.factory'
 import { GetOperationsFactory } from '@/modules/operations/infrastructure/factories/get-operations.factory'
 import { auth } from '@/auth'
+import { handleRedirectToOperationsUtil } from '@/modules/shared/presentation/utils/handle-redirect-to-operations.util'
+import { JwtTokenDecodeFactory } from '../../factories/jwt-decode.factory'
+import type { TokenEntities } from "@/modules/authentication/domain/entities/token.entity"
+import { PATHNAMES } from '../../configs/pathnames.config'
 
 export async function RedirectToOperationsMiddleware(req: NextRequest) {
   const { pathname, origin } = req.nextUrl
-  const { SYSTEM, OPERATIONS, OPERATION_OPTIONS, AUTHENTICATION } = PATHNAMES
+  const { SYSTEM, OPERATION_OPTIONS, OPERATIONS } = PATHNAMES
 
-  if (pathname !== SYSTEM) return
+  const response = NextResponse.next()
 
-  const { token } = await auth()
-  const accessToken = token?.access_token
-  if (!accessToken) NextResponse.redirect(new URL(AUTHENTICATION, origin))
+  const redirectUrl = await handleRedirectToOperationsUtil(pathname, SYSTEM, {
+    async getAuthToken() {
+      const result = await auth()
+      return result?.token || null
+    },
+    decodeToken(token: TokenEntities) {
+      const jwtDecoder = JwtTokenDecodeFactory.create()
+      return jwtDecoder.decode(token.access_token)
+    },
+    async getOperations(token: TokenEntities, ids: number[]) {
+      const getOperations = GetOperationsFactory.create()
+      return await getOperations.execute(token, ids)
+    },
+    createOperation(data) {
+      return OperationFactory.create(data)
+    },
+    saveOperationToCookies(operation) {
+      const repo = SelectOperationFactory.create(req, response)
+      repo.saveToCookies(operation)
+    },
+    getRedirectUrl(single: boolean) {
+      const path = single ? OPERATION_OPTIONS : OPERATIONS
+      return new URL(path, origin).toString()
+    },
+  })
 
-  const jwtDecoder = JwtTokenDecodeFactory.create()
-  const { operation_ids: operationIds } = jwtDecoder.decode(accessToken)
-
-  if (!operationIds?.length) return
-
-  const getOperations = GetOperationsFactory.create()
-  const operations = await getOperations.execute(token, operationIds)
-
-  const hasSingleOperation = operationIds.length === 1
-  const redirectTarget = hasSingleOperation ? OPERATION_OPTIONS : OPERATIONS
-  const response = NextResponse.redirect(new URL(redirectTarget, origin))
-
-  if (hasSingleOperation && operations.length) {
-    const operation = OperationFactory.create(operations[0])
-    const operationRepository = SelectOperationFactory.create(req, response)
-    operationRepository.saveToCookies(operation)
-  }
-
-  return response
+  return redirectUrl ? NextResponse.redirect(redirectUrl) : response
 }
