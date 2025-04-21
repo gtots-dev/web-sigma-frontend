5f8b87f5990b38399ae8c8756a826c9426eacecc
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-04-21 20:03:14 -0300

    feat: Dropdown de opções de usuários

---
+++ src/modules/users/presentation/components/user-options-dropdown/user-options-dropdown-item.component.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { DropdownMenuItem } from '@/modules/shared/presentation/components/shadcn/dropdown-menu'
+import type { DropdownMenuItemProps } from '@radix-ui/react-dropdown-menu'
+import type { ReactNode } from 'react'
+
+interface UserOptionsDropdownItemComponentProps extends DropdownMenuItemProps {
+  children?: ReactNode
+}
+
+export function UserOptionsDropdownItemComponent({
+  children,
+  ...props
+}: UserOptionsDropdownItemComponentProps) {
+  return (
+    <DropdownMenuItem className="p-0" {...props}>
+      {children} 
+    </DropdownMenuItem>
+  )
+}
