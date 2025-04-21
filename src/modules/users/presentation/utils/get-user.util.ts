4d14c841d5057762cc625a25f04a415518627b3f
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-04-21 15:33:14 -0300

    feat: Utilitário de pegar usuário autenticado

--- src/modules/users/presentation/utils/get-user.util.ts
+++ src/modules/users/presentation/utils/get-user.util.ts
@@ -1,9 +1,9 @@
 import { auth } from '@/auth'
 import { JwtTokenDecodeFactory } from '@/modules/shared/infrastructure/factories/jwt-decode.factory'
 import { GetUserFactory } from '@/modules/users/infrastructure/factories/get-user.factory'
-import type { UserEntity } from '../../domain/entities/user.entity'
+import type { UserInterface } from '../../domain/interfaces/user.interface'
 
-export async function getUser(): Promise<UserEntity> {
+export async function getUser(): Promise<UserInterface> {
   const { token } = await auth()
   const jwtDecode = JwtTokenDecodeFactory.create()
   const { id } = jwtDecode.decode(token.access_token)
