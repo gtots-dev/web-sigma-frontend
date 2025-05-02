ac5e3d5978c3278057cfe0f5aa5a4ae4e4a4aa02
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-05-02 13:45:16 -0300

    fix: Adaptado teste

--- src/modules/authentication/infrastructure/services/__tests__/token.service.test.ts
+++ src/modules/authentication/infrastructure/services/__tests__/token.service.test.ts
@@ -1,6 +1,5 @@
 import type { ExecuteRequest } from '@/modules/shared/infrastructure/services/execute-request.service'
 import { TokenService } from '../token.service'
-import type { TokenValidator } from '@/modules/authentication/domain/validators/token.validator'
 import type { UserCredentialsInterface } from '@/modules/authentication/domain/interfaces/user-credentials.interface'
 import type { OAuthResponseInterface } from '@/modules/authentication/domain/interfaces/o-auth-response.interface'
 import type { HttpResponse } from '@/modules/shared/domain/interfaces/http-response.interface'
@@ -8,12 +7,10 @@ import { TokenEntities } from '@/modules/authentication/domain/entities/token.en
 import { HttpStatusCodeEnum } from '@/modules/authentication/domain/enums/status-codes.enum'
 
 jest.mock('@/modules/shared/infrastructure/services/execute-request.service')
-jest.mock('@/modules/authentication/domain/validators/token.validator')
 
 describe('TokenService', () => {
   let tokenService: TokenService
   let executeRequestMock: jest.Mocked<ExecuteRequest>
-  let tokenValidatorMock: jest.Mocked<TokenValidator>
   const invalidStatusMock = HttpStatusCodeEnum.UNAUTHORIZED
   const validStatusMock = HttpStatusCodeEnum.OK
 
@@ -22,11 +19,7 @@ describe('TokenService', () => {
       execute: jest.fn()
     } as unknown as jest.Mocked<ExecuteRequest>
 
-    tokenValidatorMock = {
-      validate: jest.fn()
-    } as unknown as jest.Mocked<TokenValidator>
-
-    tokenService = new TokenService(executeRequestMock, tokenValidatorMock)
+    tokenService = new TokenService(executeRequestMock)
   })
 
   test('should retrieve a valid token when credentials are correct', async () => {
@@ -53,9 +46,6 @@ describe('TokenService', () => {
     expect(token.token_type).toBe('Bearer')
 
     expect(executeRequestMock.execute).toHaveBeenCalledTimes(1)
-    expect(tokenValidatorMock.validate).toHaveBeenCalledWith(
-      'fakeAccessToken123'
-    )
   })
 
   test('should throw an error when the response is unsuccessful', async () => {
@@ -77,6 +67,5 @@ describe('TokenService', () => {
     )
 
     expect(executeRequestMock.execute).toHaveBeenCalledTimes(1)
-    expect(tokenValidatorMock.validate).not.toHaveBeenCalled()
   })
 })
