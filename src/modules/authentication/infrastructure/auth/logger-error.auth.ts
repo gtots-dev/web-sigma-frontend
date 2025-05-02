2e79963836006ba667a9e96b02226fc370166ea3
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-05-02 13:44:33 -0300

    feat: Console de error do auth.js/

---
+++ src/modules/authentication/infrastructure/auth/logger-error.auth.ts
@@ -0,0 +1,7 @@
+import { CredentialsSignin } from 'next-auth'
+
+export function LoggerErrorAuth(error: Error): void {
+  if (error instanceof CredentialsSignin) {
+    console.log('[Auth] Credenciais inválidas.')
+  }
+}
