3d98dc112be70152953951d4c4404b62215d3839
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-05-04 12:34:34 -0300

    fix: Correções de retorno

--- src/modules/operations/infrastructure/factories/get-operations.factory.ts
+++ src/modules/operations/infrastructure/factories/get-operations.factory.ts
@@ -1,9 +1,10 @@
 import { HttpClientFactory } from '@/modules/shared/infrastructure/factories/http-client.factory'
 import { ExecuteRequestFactory } from '@/modules/shared/infrastructure/factories/request.factory'
 import { GetOperationsService } from '../services/get-operations.service'
+import type { GetOperationsServiceInterface } from '../../domain/interfaces/get-operations-service.interface'
 
 export class GetOperationsFactory {
-  static create() {
+  static create(): GetOperationsServiceInterface {
     const httpClient = HttpClientFactory.create(process.env.HOST_API)
     const executeRequest = ExecuteRequestFactory.create(httpClient)
     return new GetOperationsService(executeRequest)
