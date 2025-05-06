cbf9d1e3b0a04b7249a8cb269db4f40518c8a7cd
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-05-06 01:54:02 -0300

    feat: Interface do serviço de inserção de usuário

--- src/modules/users/domain/interfaces/post-user-service.interface.ts
+++ src/modules/users/domain/interfaces/post-user-service.interface.ts
@@ -1,11 +1,9 @@
 import type { TokenEntities } from '@/modules/authentication/domain/entities/token.entity'
-import type { UserInterface } from './user.interface'
-import type { UserEntity } from '../entities/user.entity'
 
 export interface PostUserServiceInterface {
   execute(
     token: TokenEntities,
-    user: UserEntity,
+    user: FormData,
     operationSelectedId: number
   ): Promise<void>
 }
