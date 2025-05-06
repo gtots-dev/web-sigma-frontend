6daa805ba42d35554ca7e20163ecca8ed256cd96
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-05-06 01:44:54 -0300

    feat: Tratativa de erro para auth credentials

--- src/modules/authentication/infrastructure/services/auth-sign-in.service.ts
+++ src/modules/authentication/infrastructure/services/auth-sign-in.service.ts
@@ -5,13 +5,13 @@ import type { AuthSignInRepository } from '../repositories/auth-sign-in.reposito
 export class AuthSignInService {
   constructor(private readonly authSignIn: AuthSignInRepository) {}
 
-  async signIn(data: AuthenticationFormType): Promise<string | null> {
+  async signIn(data: AuthenticationFormType): Promise<null> {
     const { code, error } = await this.authSignIn.execute({
       data,
       type: 'credentials',
       options: { redirect: false }
     })
-    if (error) return AuthMessagesError.message(code)
+    if (error) throw AuthMessagesError.message(code)
     return null
   }
 }
