e4eeeb0e475a177aa94c6c06b19dd91e73c55a11
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-05-06 01:33:34 -0300

    feat: Schema de formulário para inserção de usuários

--- src/modules/users/presentation/schemas/add-user-form.schema.ts
+++ src/modules/users/presentation/schemas/add-user-form.schema.ts
@@ -1,6 +1,9 @@
 import { MESSAGES_USERS } from '@/modules/shared/presentation/messages/users'
 import { z } from 'zod'
 
+const VALID_TYPES = ['image/png', 'image/jpg', 'image/jpeg', 'application/pdf']
+const MAX_SIZE_MB = 10
+
 export const AddUserFormSchema = z.object({
   name: z.string().nonempty({
     message: MESSAGES_USERS['5.6']
@@ -29,7 +32,19 @@ export const AddUserFormSchema = z.object({
     })
     .max(64, {
       message: MESSAGES_USERS['5.9']
+    }),
+  passwd_reg_deadline: z.number().nullable(),
+  files: z
+    .array(z.instanceof(File))
+    .refine((files) => files.every((file) => VALID_TYPES.includes(file.type)), {
+      message: MESSAGES_USERS['5.20']
     })
+    .refine(
+      (files) => files.every((file) => file.size <= MAX_SIZE_MB * 1024 * 1024),
+      { message: 'Cada arquivo deve ter no máximo 10MB.' }
+    )
+    .optional(),
+  description: z.string().optional()
 })
 
 export type AddUserFormType = z.infer<typeof AddUserFormSchema>
