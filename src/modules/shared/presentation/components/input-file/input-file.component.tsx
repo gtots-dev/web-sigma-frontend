de9ad125fa1423127e28a8445c40ad9bff75daf9
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-05-12 09:11:56 -0300

    refactor: input de seleção de arquivo

--- src/modules/shared/presentation/components/input-file/input-file.component.tsx
+++ src/modules/shared/presentation/components/input-file/input-file.component.tsx
@@ -1,92 +1,95 @@
-import * as React from 'react'
+import { forwardRef } from 'react'
 import { Trash } from 'lucide-react'
 
 import { cn } from '@/modules/shared/presentation/lib/utils'
-import { FormLabel } from '../shadcn/form'
 import { Button } from '../shadcn/button'
+import { useFileInput } from '../../hooks/use-file-Input'
 
 type FileInputProps = {
   id: string
-  value?: Blob[]
-  onChange?: (files: Blob[]) => void
+  value?: File[]
+  onChange?: (files: File[]) => void
   accept?: string
   multiple?: boolean
   className?: string
 }
 
-const FileInput = React.forwardRef<HTMLInputElement, FileInputProps>(
+const FileInput = forwardRef<HTMLInputElement, FileInputProps>(
   ({ id, value = [], onChange, accept, multiple, className }, ref) => {
-    const [files, setFiles] = React.useState<Blob[]>(value)
-    const [isDragging, setIsDragging] = React.useState(false)
-
-    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      const selected = Array.from(e.target.files || [])
-      handleNewFiles(selected)
-      e.target.value = ''
-    }
-
-    const handleNewFiles = (newFiles: Blob[]) => {
-      const updated = [...files, ...newFiles]
-      setFiles(updated)
-      onChange?.(updated)
-    }
-
-    const handleDrop = (e: React.DragEvent<HTMLLabelElement>) => {
-      e.preventDefault()
-      e.stopPropagation()
-      setIsDragging(false)
-      const droppedFiles = Array.from(e.dataTransfer.files)
-      handleNewFiles(droppedFiles)
-    }
-
-    const handleDragOver = (e: React.DragEvent<HTMLLabelElement>) => {
-      e.preventDefault()
-      e.stopPropagation()
-      if (!isDragging) setIsDragging(true)
-    }
-
-    const handleDragLeave = (e: React.DragEvent<HTMLLabelElement>) => {
-      e.preventDefault()
-      e.stopPropagation()
-      setIsDragging(false)
-    }
-
-    function formatFileSize(sizeInBytes: number) {
-      const units = ['B', 'KB', 'MB', 'GB', 'TB']
-      let size = sizeInBytes
-      let unitIndex = 0
-
-      while (size >= 1024 && unitIndex < units.length - 1) {
-        size /= 1024
-        unitIndex++
-      }
-
-      return `${size.toFixed(1)} ${units[unitIndex]}`
-    }
+    const {
+      files,
+      isDragging,
+      handleDrop,
+      handleDragOver,
+      handleDragLeave,
+      handleFileChange,
+      removeFile,
+      formatFileSize
+    } = useFileInput({
+      initialFiles: value,
+      onChange
+    })
 
     return (
-      <div>
-        <label
-          htmlFor={id}
-          onDrop={handleDrop}
-          onDragOver={handleDragOver}
-          onDragLeave={handleDragLeave}
-          className={cn(
-            'flex flex-col gap-y-9 justify-center items-center border border-dashed py-16 md:px-40 rounded-md cursor-pointer transition-colors',
-            isDragging ? 'border-ring bg-muted/40' : '',
-            className
-          )}
-        >
-          <p className="text-center text-sm px-10">
-            Arraste e solte os arquivos dentro da marcação ou{' '}
-            <b className="underline underline-offset-4">
-              clique aqui para o envio
-            </b>
-          </p>
-          <p className="text-center text-sm opacity-45">
-            PDF, PNG, JPEG e JPG (Máximo 10.0 MB)
-          </p>
-        </label>
+      <div
+        onDrop={handleDrop}
+        onDragOver={handleDragOver}
+        onDragLeave={handleDragLeave}
+        className={cn(
+          'flex flex-col gap-y-9 min-h-56 p-2 transition-colors rounded-md border-2 border-spacing-2 border-separate border-dashed bg-zinc-100/25 dark:bg-zinc-900/25',
+          isDragging && 'border-ring bg-muted/40',
+          className
+        )}
+      >
+        {files.length === 0 ? (
+          <label
+            htmlFor={id}
+            className="flex flex-col items-center justify-center flex-1 gap-y-3 cursor-pointer hover:bg-zinc-100/35 dark:hover:bg-zinc-900/35"
+          >
+            <p className="text-center text-sm px-10">
+              Arraste e solte os arquivos dentro da marcação <br /> ou <br />
+              <b className="underline underline-offset-4">
+                clique aqui para o envio
+              </b>
+            </p>
+            <p className="text-center text-sm opacity-45">
+              PDF, PNG, JPEG e JPG (Máximo 10.0 MB)
+            </p>
+          </label>
+        ) : (
+          <ul className="flex flex-col gap-y-3 flex-1">
+            {files.map((file: File, index: number) => (
+              <li
+                key={index}
+                className="flex justify-between items-center gap-4 h-[55.3px] w-full border border-input rounded-md px-2 ps-3.5"
+              >
+                <div className="flex flex-col gap-y-0.5 hover:bg-zinc-50/40 hover:dark:bg-zinc-900/40">
+                  <h4 className="text-sm">{file?.name}</h4>
+                  <span className="text-xs opacity-80">
+                    {formatFileSize(file.size)}
+                  </span>
+                </div>
+                <Button
+                  type="button"
+                  variant="outline"
+                  size="icon"
+                  onClick={() => removeFile(index)}
+                >
+                  <Trash />
+                </Button>
+              </li>
+            ))}
+            <li className="w-full rounded-md hover:bg-zinc-100/35 dark:hover:bg-zinc-900/35 cursor-pointer mt-auto">
+              <label
+                htmlFor={id}
+                className="flex justify-center items-center h-[37px] text-sm px-10 py-2 gap-x-1.5 opacity-70 cursor-pointer"
+              >
+                <p>Arraste os arquivos ou</p>
+                <b className="underline underline-offset-4">clique aqui</b>
+              </label>
+            </li>
+          </ul>
+        )}
 
         <input
           ref={ref}
@@ -97,45 +100,6 @@ const FileInput = React.forwardRef<HTMLInputElement, FileInputProps>(
           className="hidden"
           onChange={handleFileChange}
         />
-
-        <FormLabel className="text-sm flex items-center gap-x-1.5 mt-6">
-          Arquivos Selecionados:
-        </FormLabel>
-        {files.length === 0 ? (
-          <div className="flex items-center !border-b bg-zinc-50/40 dark:bg-zinc-900/40 h-[55.3px] p-4 mt-2 space-y-1">
-            <p className=" text-zinc-700 dark:text-zinc-50 text-sm">
-              Nenhum arquivo selecionado
-            </p>
-          </div>
-        ) : (
-          <ul className="mt-2 space-y-1">
-            {files.map((file, index) => (
-              <li
-                key={index}
-                className="flex justify-between items-center gap-4 h-[55.3px] w-full border border-input rounded-md px-2 ps-3.5"
-              >
-                <div className="flex flex-col gap-y-0.5 hover:bg-zinc-50/40 hover:dark:bg-zinc-900/40">
-                  <h4 className="text-sm">{file.name}</h4>
-                  <span className="text-xs opacity-80">
-                    {formatFileSize(file.size)}
-                  </span>
-                </div>
-                <Button
-                  type="button"
-                  variant="outline"
-                  size="icon"
-                  onClick={() => {
-                    const updated = files.filter((_, i) => i !== index)
-                    setFiles(updated)
-                    onChange?.(updated.length ? updated : [])
-                  }}
-                >
-                  <Trash />
-                </Button>
-              </li>
-            ))}
-          </ul>
-        )}
       </div>
     )
   }
