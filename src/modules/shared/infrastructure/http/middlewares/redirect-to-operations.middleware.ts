4064903f90da28c26d0fa8cd7527a025e31fad7e
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-05-12 16:46:57 -0300

    feat: Redirecionamento automático para seção de autenticação caso acesso nao autorizado

--- src/modules/shared/infrastructure/http/middlewares/redirect-to-operations.middleware.ts
+++ src/modules/shared/infrastructure/http/middlewares/redirect-to-operations.middleware.ts
@@ -10,6 +10,8 @@ import type { TokenEntities } from '@/modules/authentication/domain/entities/tok
 import { PATHNAMES } from '../../configs/pathnames.config'
 import type { JwtDecodeDataInterface } from '@/modules/shared/domain/interfaces/jwt-decode-data.interface'
 import type { OperationEntities } from '@/modules/operations/domain/entities/operation.entity'
+import { HttpResponseError } from '../../errors/http-response.error'
+import { HttpStatusCodeEnum } from '@/modules/authentication/domain/enums/status-codes.enum'
 
 export async function RedirectToOperationsMiddleware(
   req: NextRequest
@@ -19,41 +21,47 @@ export async function RedirectToOperationsMiddleware(
 
   let response = NextResponse.next()
 
-  const redirectTo = await handleRedirectToOperationsUtil(pathname, SYSTEM, {
-    async getAuthToken(): Promise<TokenEntities | null> {
-      const session = await auth()
-      return session?.token ?? null
-    },
-    decodeToken(token: TokenEntities): JwtDecodeDataInterface {
-      return JwtTokenDecodeFactory.create().decode(token.access_token)
-    },
-    async getOperations(
-      token: TokenEntities,
-      ids: number[]
-    ): Promise<OperationInterface[]> {
-      return await GetOperationsFactory.create().execute(token, ids)
-    },
-    createOperation(data): OperationEntities {
-      return OperationFactory.create(data)
-    },
-    saveOperationToCookies(operation): void {
-      const repo = SelectOperationFactory.create(req, response)
-      repo.saveToCookies(operation)
-    },
-    getRedirectUrl(isSingle: boolean): string {
-      const targetPath = isSingle ? OPERATION_OPTIONS : OPERATIONS
-      return new URL(targetPath, origin).toString()
-    }
-  })
+  try {
+    const redirectTo = await handleRedirectToOperationsUtil(pathname, SYSTEM, {
+      async getAuthToken(): Promise<TokenEntities | null> {
+        const session = await auth()
+        return session?.token ?? null
+      },
+      decodeToken(token: TokenEntities): JwtDecodeDataInterface {
+        return JwtTokenDecodeFactory.create().decode(token.access_token)
+      },
+      async getOperations(
+        token: TokenEntities,
+        ids: number[]
+      ): Promise<OperationInterface[]> {
+        return await GetOperationsFactory.create().execute(token, ids)
+      },
+      createOperation(data): OperationEntities {
+        return OperationFactory.create(data)
+      },
+      saveOperationToCookies(operation): void {
+        const repo = SelectOperationFactory.create(req, response)
+        repo.saveToCookies(operation)
+      },
+      getRedirectUrl(isSingle: boolean): string {
+        const targetPath = isSingle ? OPERATION_OPTIONS : OPERATIONS
+        return new URL(targetPath, origin).toString()
+      }
+    })
 
-  if (redirectTo) {
-    const redirectResponse = NextResponse.redirect(redirectTo)
-    response.cookies
-      .getAll()
-      .forEach((cookie) =>
-        redirectResponse.cookies.set(cookie.name, cookie.value, cookie)
-      )
-    return redirectResponse
+    if (redirectTo) {
+      const redirectResponse = NextResponse.redirect(redirectTo)
+      response.cookies
+        .getAll()
+        .forEach((cookie) =>
+          redirectResponse.cookies.set(cookie.name, cookie.value, cookie)
+        )
+      return redirectResponse
+    }
+  } catch (error) {
+    if (error instanceof HttpResponseError) {
+      if (error.message === HttpStatusCodeEnum.UNAUTHORIZED) req.cookies.clear()
+    }
   }
   return response
 }
