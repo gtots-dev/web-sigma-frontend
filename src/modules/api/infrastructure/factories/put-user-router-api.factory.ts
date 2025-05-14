6645889d6c35f08d17d2b5770f32a79f899c5d32
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-05-14 11:33:48 -0300

    fix: Tipagem de retorno para a fabrica de atualização de usuário

--- src/modules/api/infrastructure/factories/put-user-router-api.factory.ts
+++ src/modules/api/infrastructure/factories/put-user-router-api.factory.ts
@@ -1,9 +1,10 @@
 import { HttpClientFactory } from '@/modules/shared/infrastructure/factories/http-client.factory'
 import { ExecuteRequestFactory } from '@/modules/shared/infrastructure/factories/request.factory'
 import { PutUserRouterApiService } from '../services/put-user-router-api.service'
+import type { PutUserRouterApiServiceInterface } from '../../domain/interfaces/put-user-router-api-service.interface'
 
 export class PutUserRouterApiFactory {
-  static create() {
+  static create(): PutUserRouterApiServiceInterface {
     const httpClient = HttpClientFactory.create('/')
     const executeRequest = ExecuteRequestFactory.create(httpClient)
     return new PutUserRouterApiService(executeRequest)
