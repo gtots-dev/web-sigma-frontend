b3e3f23aac39c40217ac5f4cb873b55a3d668925
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-05-15 10:37:49 -0300

    feat: Componente de tabela de contratos

---
+++ src/modules/contracts/presentation/components/table-contracts/index.ts
@@ -0,0 +1,11 @@
+import { TableContractsBodyComponent } from './table-contract-body.component'
+import { TableContractsHeaderComponent } from './table-contract-header.component'
+import { TableContractsItemComponent } from './table-contract-item.component'
+import { TableContractsRootComponent } from './table-contract-root.component'
+
+export const TableContracts = {
+  Root: TableContractsRootComponent,
+  Body: TableContractsBodyComponent,
+  Header: TableContractsHeaderComponent,
+  Item: TableContractsItemComponent
+}
---
+++ src/modules/contracts/presentation/components/table-contracts/table-contract-root.component.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import React, { ReactNode } from 'react'
+import { Table } from '@/modules/shared/presentation/components/shadcn/table'
+
+interface TableContractsRootComponentProps {
+  children: ReactNode
+}
+
+export function TableContractsRootComponent({
+  children
+}: TableContractsRootComponentProps) {
+  const containerHeight = 69 + 36 + 53 * 10
+  return (
+    <section
+      style={{ height: `${containerHeight}px` }}
+      className="flex flex-1 flex-col w-full"
+    >
+      <Table className="w-full overflow-x-auto">{children}</Table>
+    </section>
+  )
+}
