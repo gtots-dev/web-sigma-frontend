80fd13dcc1f7c7cd9710e52e8c73d927dce1c2a8
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-05-16 10:23:58 -0300

    feat: Injeção do menu de adicionar contratos a pagina de contratos

--- src/app/system/operations/contracts/page.tsx
+++ src/app/system/operations/contracts/page.tsx
@@ -2,16 +2,23 @@ import { HeaderSection } from '@/modules/system/presentation/components/header-s
 import { Separator } from '@/modules/shared/presentation/components/shadcn/separator'
 import { TableContracts } from '@/modules/contracts/presentation/components/table-contracts'
 import { MESSAGES_CONTRACTS } from '@/modules/shared/presentation/messages/contracts'
+import { ActionSection } from '@/modules/system/presentation/components/actions-section'
+import { AddContractMenu } from '@/modules/contracts/presentation/components/add-contract-menu'
+import { AddContractMenuComponent } from '@/modules/contracts/presentation/components/add-contract-menu/add-contract-menu.component'
 
 interface Data {
   title: string
   description: string
+  menuAddContractTitle: string
+  menuAddContractDescription: string
 }
 
 export default async function ContractsPage() {
   const data: Data = {
     title: MESSAGES_CONTRACTS['3.1'],
-    description: MESSAGES_CONTRACTS['3.2']
+    description: MESSAGES_CONTRACTS['3.2'],
+    menuAddContractTitle: MESSAGES_CONTRACTS['3.4'],
+    menuAddContractDescription: MESSAGES_CONTRACTS['3.5']
   }
 
   return (
@@ -23,6 +30,15 @@ export default async function ContractsPage() {
         </HeaderSection.Description>
       </HeaderSection.Root>
       <Separator orientation="horizontal" />
+      <ActionSection.Root>
+        <AddContractMenu.Provider>
+          <AddContractMenu.Trigger />
+          <AddContractMenuComponent
+            title={data.menuAddContractTitle}
+            description={data.menuAddContractDescription}
+          />
+        </AddContractMenu.Provider>
+      </ActionSection.Root>
       <TableContracts.Root>
         <TableContracts.Header />
         <TableContracts.Body>
