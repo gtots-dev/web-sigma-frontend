499881f0b1aeeee1cc2758a08db7fc6a9c79ca93
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-05-20 18:49:07 -0300

    feat: Implementação do trigger para listagem dos arquivos

--- src/modules/users/presentation/components/view-more-user-menu/view-more-user-menu-trigger.component.tsx
+++ src/modules/users/presentation/components/view-more-user-menu/view-more-user-menu-trigger.component.tsx
@@ -2,15 +2,23 @@
 
 import { Button } from '@/modules/shared/presentation/components/shadcn/button'
 import { useDialog } from './view-more-user-menu-provider.component'
+import { useUserFilesStore } from '../../stores/user-files.store'
+import { useTableUser } from '../../contexts/table-user.context'
 
-interface ViewMoreUserMenuTriggerComponentProps {}
+export function ViewMoreUserMenuTriggerComponent() {
+  const { open: openDialog } = useDialog()
+  const { id: userId } = useTableUser()
+  const { getUserFiles } = useUserFilesStore()
 
-export function ViewMoreUserMenuTriggerComponent({}: ViewMoreUserMenuTriggerComponentProps) {
-  const { open } = useDialog()
+  const handleClick = async () => {
+    await getUserFiles(userId)
+    openDialog()
+  }
+
   return (
     <Button
       className="justify-start w-full h-auto cursor-pointer p-1.5 ps-3 rounded-none text-sm disabled:bg-muted-foreground [&>svg]:size-4 [&>svg]:shrink-0 shadow-none"
-      onClick={open}
+      onClick={handleClick}
     >
       Ver Mais
     </Button>
