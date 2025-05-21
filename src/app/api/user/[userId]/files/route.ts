495614a5cf2af2aa30eea26eecf977f49fdbb40b
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-05-21 03:03:10 +0000

    refactor: Alterador o params de "id" para "userId"

--- src/app/api/user/[userId]/files/route.ts
+++ src/app/api/user/[userId]/files/route.ts
@@ -1,17 +1,19 @@
 import { auth } from '@/auth'
 import { HttpStatusCodeEnum } from '@/modules/authentication/domain/enums/status-codes.enum'
 import { HttpResponseError } from '@/modules/shared/infrastructure/errors/http-response.error'
+import type { UserEntity } from '@/modules/users/domain/entities/user.entity'
 import { GetUserFilesFactory } from '@/modules/users/infrastructure/factories/get-user-files.factory'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: Promise<{ userId: UserEntity['id'] }> }
 ): Promise<NextResponse> {
   try {
+    const { userId } = await params
     const { token } = await auth()
     const getUserFilesFactory = GetUserFilesFactory.create()
-    const response = await getUserFilesFactory.execute(token, params.id)
+    const response = await getUserFilesFactory.execute(token, userId)
     return NextResponse.json(response, {
       status: Number(HttpStatusCodeEnum.OK)
     })
