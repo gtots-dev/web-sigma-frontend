f5e8e97c902342cb7b4d14aefc0d9e7ddb925241
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-05-21 00:22:17 +0000

    feat: injeção do componente de item de arquivo

--- src/modules/users/presentation/components/view-more-user-menu/view-more-user-menu.component.tsx
+++ src/modules/users/presentation/components/view-more-user-menu/view-more-user-menu.component.tsx
@@ -28,53 +28,69 @@ export function ViewMoreUserMenuComponent({
     <ViewMoreUserMenu.Root>
       <ViewMoreUserMenu.Content>
         <ViewMoreUserMenu.Header title={title} description={description} />
-        <div className="h-full w-full p-8">
-          <div className="flex flex-col gap-7">
-            <ViewMoreUserMenu.Group>
-              <ViewMoreUserMenu.Item title="Nome" notFoundData="Sem Informação">
-                {name}
-              </ViewMoreUserMenu.Item>
-              <ViewMoreUserMenu.Item
-                title="Email"
-                notFoundData="Sem Informação"
-              >
-                {email}
-              </ViewMoreUserMenu.Item>
-              <ViewMoreUserMenu.Item
-                title="Empresa"
-                notFoundData="Sem Informação"
-              >
-                {company}
-              </ViewMoreUserMenu.Item>
-              <ViewMoreUserMenu.Item
-                title="Posição"
-                notFoundData="Sem Informação"
-              >
-                {position}
-              </ViewMoreUserMenu.Item>
-              <ViewMoreUserMenu.Item
-                title="Nome de usuário"
-                notFoundData="Sem Informação"
-              >
-                {login_name}
-              </ViewMoreUserMenu.Item>
-              <ViewMoreUserMenu.Item
-                title="Habilitado"
-                notFoundData="Sem Informação"
-              >
-                {enabled ? 'Sim' : 'Não'}
-              </ViewMoreUserMenu.Item>
-            </ViewMoreUserMenu.Group>
-            <ViewMoreUserMenu.Group cols={1}>
-              <ViewMoreUserMenu.Item
-                title="Descrição"
-                notFoundData="Sem Informação"
-              >
-                {userDescription}
-              </ViewMoreUserMenu.Item>
-            </ViewMoreUserMenu.Group>
-          </div>
-        </div>
+
+        <main className="flex flex-col flex-1 h-full w-full gap-y-8 overflow-auto p-8">
+          <ViewMoreUserMenu.Group>
+            <ViewMoreUserMenu.Item.data
+              title="Nome"
+              notFoundData="Sem Informação"
+            >
+              {name}
+            </ViewMoreUserMenu.Item.data>
+            <ViewMoreUserMenu.Item.data
+              title="Email"
+              notFoundData="Sem Informação"
+            >
+              {email}
+            </ViewMoreUserMenu.Item.data>
+            <ViewMoreUserMenu.Item.data
+              title="Empresa"
+              notFoundData="Sem Informação"
+            >
+              {company}
+            </ViewMoreUserMenu.Item.data>
+            <ViewMoreUserMenu.Item.data
+              title="Posição"
+              notFoundData="Sem Informação"
+            >
+              {position}
+            </ViewMoreUserMenu.Item.data>
+            <ViewMoreUserMenu.Item.data
+              title="Nome de usuário"
+              notFoundData="Sem Informação"
+            >
+              {login_name}
+            </ViewMoreUserMenu.Item.data>
+            <ViewMoreUserMenu.Item.data
+              title="Habilitado"
+              notFoundData="Sem Informação"
+            >
+              {enabled ? 'Sim' : 'Não'}
+            </ViewMoreUserMenu.Item.data>
+          </ViewMoreUserMenu.Group>
+          <ViewMoreUserMenu.Group cols={1}>
+            <ViewMoreUserMenu.Item.data
+              title="Descrição"
+              notFoundData="Sem Informação"
+            >
+              {userDescription}
+            </ViewMoreUserMenu.Item.data>
+          </ViewMoreUserMenu.Group>
+          <ViewMoreUserMenu.Group cols={1}>
+            <ViewMoreUserMenu.Item.data
+              title="Arquivos anexados"
+              notFoundData="Sem Informação"
+            >
+              <ViewMoreUserMenu.Group>
+                <ViewMoreUserMenu.Item.file
+                  title="Nome"
+                  fileName="simple.pdf"
+                />
+              </ViewMoreUserMenu.Group>
+            </ViewMoreUserMenu.Item.data>
+          </ViewMoreUserMenu.Group>
+        </main>
+
         <ViewMoreUserMenu.Footer>
           <Button
             className="w-full sm:w-[150px]"
