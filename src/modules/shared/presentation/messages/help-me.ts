7a5ca85c269b7bdd42746a4ad60fad493f9ce5a5
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-05-23 13:19:38 +0000

    feat: Mensagem de ajuda para o campo de nova senha

--- src/modules/shared/presentation/messages/help-me.ts
+++ src/modules/shared/presentation/messages/help-me.ts
@@ -2,6 +2,7 @@ type MessagesHelpMeType = {
   username: string
   password: string
   forgotPassword: string
+  newPassword: string
 }
 
 export const MESSAGES_HELP_ME: MessagesHelpMeType = {
@@ -9,6 +10,12 @@ export const MESSAGES_HELP_ME: MessagesHelpMeType = {
   Por favor, forneça o nome de usuário válido.`,
   password: `A senha é obrigatória.
   Ela deve ter no máximo 64 caracteres.`,
+  newPassword: `Senha é obrigatória.
+  Máximo 64 caracteres.
+  Conter pelo menos uma letra maiúscula.
+  Conter pelo menos uma letra minúscula.
+  Conter pelo menos um número.
+  Conter pelo menos um caractere especial (#, @, $, !, %, *, ?, &).`,
   forgotPassword: `Entre em contato com seu gestor para que ele possa solicitar a redefinição de sua senha.
   `
 }
