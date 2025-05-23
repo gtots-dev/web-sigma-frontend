b2fd24dd04ae8615b7cd1653d9b192523a53a56c
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-05-23 13:25:11 +0000

    feat: Componente de estrutura para seção de redefinição de senha

---
+++ src/modules/password-reset/presentation/components/password-reset-card/index.ts
@@ -0,0 +1,9 @@
+import { PasswordResetCardBannerComponent } from './password-reset-card-banner.component'
+import { PasswordResetCardContentComponent } from './password-reset-card-content.component'
+import { PasswordResetCardRootComponent } from './password-reset-card-root.component'
+
+export const PasswordResetCard = {
+  Root: PasswordResetCardRootComponent,
+  Banner: PasswordResetCardBannerComponent,
+  Content: PasswordResetCardContentComponent
+}
