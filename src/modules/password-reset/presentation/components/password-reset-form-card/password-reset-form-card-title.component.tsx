ecf70f03106b776560c2190b30250b8b39e6d277
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-05-23 13:24:47 +0000

    feat: Componente de cartão para o formulário

---
+++ src/modules/password-reset/presentation/components/password-reset-form-card/password-reset-form-card-title.component.tsx
@@ -0,0 +1,17 @@
+import type { ComponentProps } from 'react'
+
+interface PasswordResetFormCardTitleComponentProps
+  extends ComponentProps<'h1'> {
+  title: string
+}
+
+export function PasswordResetFormCardTitleComponent({
+  title,
+  ...props
+}: PasswordResetFormCardTitleComponentProps) {
+  return (
+    <h1 className="text-start text-[36px] font-medium dark:text-zinc-50" {...props}>
+      {title}
+    </h1>
+  )
+}
