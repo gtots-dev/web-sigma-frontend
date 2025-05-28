2e98603bf99e04f10287bde0f8cd0f343ee00e81
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-05-28 19:54:20 +0000

    feat: Mensagem de validação de prazo de expiração da senha

--- src/modules/users/presentation/schemas/add-user-form.schema.ts
+++ src/modules/users/presentation/schemas/add-user-form.schema.ts
@@ -1,44 +1,55 @@
-  import { MESSAGES_USERS } from '@/modules/shared/presentation/messages/users'
-  import { z } from 'zod'
+import { MESSAGES_PASSWORD_RESET } from '@/modules/shared/presentation/messages/password-reset'
+import { MESSAGES_USERS } from '@/modules/shared/presentation/messages/users'
+import { z } from 'zod'
 
-  const VALID_TYPES = ['image/png', 'image/jpg', 'image/jpeg', 'application/pdf']
-  const MAX_SIZE_MB = 10
+const VALID_TYPES = ['image/png', 'image/jpg', 'image/jpeg', 'application/pdf']
+const MAX_SIZE_MB = 10
 
-  export const AddUserFormSchema = z.object({
-    name: z.string().nonempty({
-      message: MESSAGES_USERS['5.6']
+export const AddUserFormSchema = z.object({
+  name: z.string().nonempty({
+    message: MESSAGES_USERS['5.6']
+  }),
+  email: z
+    .string()
+    .nonempty({
+      message: MESSAGES_USERS['5.7']
+    })
+    .email({
+      message: MESSAGES_USERS['5.12']
     }),
-    email: z
-      .string()
-      .nonempty({
-        message: MESSAGES_USERS['5.7']
-      })
-      .email({
-        message: MESSAGES_USERS['5.12']
-      }),
-    company: z.string().nonempty({
-      message: MESSAGES_USERS['5.8']
-    }),
-    position: z.string().nonempty({
-      message: MESSAGES_USERS['5.9']
-    }),
-    login_name: z.string().nonempty({
-      message: MESSAGES_USERS['5.10']
-    }),
-    days_passwd_reg_deadline: z.number().nullable(),
-    files: z
-      .array(z.instanceof(File))
-      .refine((files: Blob[]) => files.every((file: Blob) => VALID_TYPES.includes(file.type)), {
+  company: z.string().nonempty({
+    message: MESSAGES_USERS['5.8']
+  }),
+  position: z.string().nonempty({
+    message: MESSAGES_USERS['5.9']
+  }),
+  login_name: z.string().nonempty({
+    message: MESSAGES_USERS['5.10']
+  }),
+  days_passwd_reg_deadline: z.number().int().gt(0, {
+    message: MESSAGES_PASSWORD_RESET['2.16']
+  }),
+  files: z
+    .array(z.instanceof(File))
+    .refine(
+      (files: Blob[]) =>
+        files.every((file: Blob) => VALID_TYPES.includes(file.type)),
+      {
         message: MESSAGES_USERS['5.20']
-      })
-      .refine(
-        (files: Blob[]) => files.every((file: Blob) => file.size <= MAX_SIZE_MB * 1024 * 1024),
-        { message: MESSAGES_USERS['5.31'] }
-      )
-      .optional(),
-    description: z.string().max(255, {
+      }
+    )
+    .refine(
+      (files: Blob[]) =>
+        files.every((file: Blob) => file.size <= MAX_SIZE_MB * 1024 * 1024),
+      { message: MESSAGES_USERS['5.31'] }
+    )
+    .optional(),
+  description: z
+    .string()
+    .max(255, {
       message: MESSAGES_USERS['5.32']
-    }).optional()
-  })
+    })
+    .optional()
+})
 
-  export type AddUserFormType = z.infer<typeof AddUserFormSchema>
+export type AddUserFormType = z.infer<typeof AddUserFormSchema>
