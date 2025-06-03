75b718ef7b9c6eaf27d2e1a7f47b67ea14065c5f
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-03 16:48:38 +0000

    feat: Menu com formulário de edição de contrato

---
+++ src/modules/contracts/presentation/components/edit-contract-menu/edit-contract-menu-trigger.component.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { Button } from '@/modules/shared/presentation/components/shadcn/button'
+import { useDialog } from './edit-contract-menu-provider.component'
+
+interface EditContractMenuTriggerComponentProps {}
+
+export function EditContractMenuTriggerComponent({}: EditContractMenuTriggerComponentProps) {
+  const { open } = useDialog()
+  return (
+    <Button
+      className="justify-start w-full h-auto cursor-pointer p-1.5 ps-3 rounded-none text-sm disabled:bg-muted-foreground [&>svg]:size-4 [&>svg]:shrink-0 shadow-none"
+      onClick={open}
+    >
+      Editar
+    </Button>
+  )
+}
---
+++ src/modules/contracts/presentation/components/edit-contract-menu/edit-contract-menu.component.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { Button } from '@/modules/shared/presentation/components/shadcn/button'
+import { useDialog } from './edit-contract-menu-provider.component'
+import { EditContractMenu } from '.'
+import type { ContractEntity } from '@/modules/contracts/domain/entities/contract.entity'
+import { EditContractForm } from '../edit-contract-form-provider'
+import { ContractForm } from '../contract-form'
+import { useEditContractSubmit } from '../../hooks/use-edit-contract-submit.hook'
+
+interface EditContractMenuComponentProps {
+  title: string
+  description: string
+}
+
+export function EditContractMenuComponent({
+  title,
+  description
+}: EditContractMenuComponentProps) {
+  const { isOpen, close } = useDialog()
+  const { onAction } = useEditContractSubmit()
+
+  return (
+    <EditContractMenu.Root>
+      <EditContractMenu.Content>
+        <EditContractMenu.Header title={title} description={description} />
+        <EditContractForm.Provider isOpen={isOpen}>
+          <ContractForm.Form>
+            <ContractForm.Input.Name />
+            <ContractForm.Input.Alias />
+            <ContractForm.Input.cfg />
+          </ContractForm.Form>
+
+          <EditContractMenu.Footer>
+            <Button
+              className="w-full sm:w-[150px]"
+              variant="outline"
+              onClick={close}
+            >
+              Cancelar
+            </Button>
+            <ContractForm.Submit
+              onSubmit={(contract: ContractEntity) => onAction(contract, close)}
+            />
+          </EditContractMenu.Footer>
+        </EditContractForm.Provider>
+      </EditContractMenu.Content>
+    </EditContractMenu.Root>
+  )
+}
