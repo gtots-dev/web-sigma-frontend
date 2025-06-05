05668a6ba612a0c82af6e196e20d6aeb2cf9875d
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-05 18:03:12 +0000

    refactor: Removido interfaces não utilizadas

--- src/modules/users/presentation/components/add-user-menu/add-user-menu-trigger.component.tsx
+++ src/modules/users/presentation/components/add-user-menu/add-user-menu-trigger.component.tsx
@@ -4,9 +4,7 @@ import { Button } from '@/modules/shared/presentation/components/shadcn/button'
 import { UserRoundPlus } from 'lucide-react'
 import { useDialog } from './add-user-menu-provider.component'
 
-interface AddUserMenuTriggerComponentProps {}
-
-export function AddUserMenuTriggerComponent({}: AddUserMenuTriggerComponentProps) {
+export function AddUserMenuTriggerComponent() {
   const { open } = useDialog()
   return (
     <Button variant="primary" className="w-full sm:w-auto" onClick={open}>
