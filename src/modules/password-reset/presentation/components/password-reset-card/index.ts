6a2cb333dce7e9481c576d0e8e79b5fb8ead9557
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-05 17:55:50 +0000

    refactor: Removido componente de banner não utilizado

--- src/modules/password-reset/presentation/components/password-reset-card/index.ts
+++ src/modules/password-reset/presentation/components/password-reset-card/index.ts
@@ -1,9 +1,7 @@
-import { PasswordResetCardBannerComponent } from './password-reset-card-banner.component'
 import { PasswordResetCardContentComponent } from './password-reset-card-content.component'
 import { PasswordResetCardRootComponent } from './password-reset-card-root.component'
 
 export const PasswordResetCard = {
   Root: PasswordResetCardRootComponent,
-  Banner: PasswordResetCardBannerComponent,
   Content: PasswordResetCardContentComponent
 }
