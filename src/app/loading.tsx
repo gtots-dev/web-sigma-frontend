06d07e51553eb394a69e6d0e625247fe397915c0
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-05 17:59:28 +0000

    refactor: Alterado o elemento img para Image do next

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,13 @@
+import Image from 'next/image'
+
 export default function Loading() {
   return (
     <main className="grid place-content-center h-svh w-screen">
-      <img
+      <Image
         src="/logos/gtots_principal_hor.svg"
+        alt="Logotipo da empresa Traffic Safe"
         className="h-[100px] aspect-auto animate-pulse duration-1000"
         height={100}
-        width="auto"
         fetchPriority="high"
       />
     </main>
