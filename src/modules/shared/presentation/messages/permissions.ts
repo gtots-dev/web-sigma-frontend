2f261d50241508ea346a0cf2461888fefdb23438
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-12 18:32:12 +0000

    feat: Mensagens da seção de permissões baseadas no documento de mensageiria

---
+++ src/modules/shared/presentation/messages/permissions.ts
@@ -0,0 +1,7 @@
+type MessageKeys = '6.1' | '6.2' | '6.3'
+
+export const MESSAGES_PERMISSIONS: Record<MessageKeys, string> = {
+  '6.1': 'Permissões',
+  '6.2': 'Navegue e gerencie os perfis de permissões disponíveis',
+  '6.3': 'Nenhum perfil encontrado.'
+}
