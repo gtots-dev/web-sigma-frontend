903213bbe9c59a69581d467c46a50adb5f8f0372
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-18 16:28:05 +0000

    feat: Mensagens de titulo e descrição para o menu de adicionar perfil de permissão

--- src/modules/shared/presentation/messages/permissions.ts
+++ src/modules/shared/presentation/messages/permissions.ts
@@ -1,7 +1,9 @@
-type MessageKeys = '6.1' | '6.2' | '6.3'
+type MessageKeys = '6.1' | '6.2' | '6.3' | '6.4' | '6.5'
 
 export const MESSAGES_PERMISSIONS: Record<MessageKeys, string> = {
   '6.1': 'Permissões',
   '6.2': 'Navegue e gerencie os perfis de permissões disponíveis',
-  '6.3': 'Nenhum perfil encontrado.'
+  '6.3': 'Nenhum perfil encontrado.',
+  '6.4': 'Novo perfil de permissão',
+  '6.5': 'Preencha os dados obrigatórios para adicionar um novo perfil.'
 }
