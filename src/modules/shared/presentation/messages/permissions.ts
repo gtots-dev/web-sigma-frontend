6bf08e05696036b58fb64d8367cefe9db5a9407c
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-23 17:51:26 +0000

    feat: Mensagens para o formulário de permissão

--- src/modules/shared/presentation/messages/permissions.ts
+++ src/modules/shared/presentation/messages/permissions.ts
@@ -1,9 +1,10 @@
-type MessageKeys = '6.1' | '6.2' | '6.3' | '6.4' | '6.5'
+type MessageKeys = '6.1' | '6.2' | '6.3' | '6.4' | '6.5' | '6.6'
 
 export const MESSAGES_PERMISSIONS: Record<MessageKeys, string> = {
   '6.1': 'Permissões',
   '6.2': 'Navegue e gerencie os perfis de permissões disponíveis',
   '6.3': 'Nenhum perfil encontrado.',
   '6.4': 'Novo perfil de permissão',
-  '6.5': 'Preencha os dados obrigatórios para adicionar um novo perfil.'
+  '6.5': 'Preencha os dados obrigatórios para adicionar um novo perfil.',
+  '6.6': 'Nenhuma permissão encontrada.'
 }
