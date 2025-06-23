4ccfd3e699fe9dbf0380ce6a91d693bee8e2b2df
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-23 18:03:25 +0000

    feat: Provedor de formulário de adição de perfil de permissão

---
+++ src/modules/permissions/presentation/components/add-permission-profile-form-provider/add-permission-profile-form-provider.component.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { useForm, FormProvider } from 'react-hook-form'
+import { useEffect, useMemo } from 'react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import type { ReactNode } from 'react'
+import {
+  AddPermissionProfilesFormSchema,
+  type AddPermissionProfileFormType
+} from '../../schemas/add-permission-profile-form.schema'
+import type { PermissionProfilesInterface } from '@/modules/permissions/domain/interfaces/permission-profiles.interface'
+
+interface AddPermissionProfileFormContextProviderComponentProps {
+  children: ReactNode
+  isOpen: boolean
+}
+
+export function AddPermissionProfileFormContextProviderComponent({
+  children,
+  isOpen
+}: AddPermissionProfileFormContextProviderComponentProps) {
+  const defaultValues = useMemo<PermissionProfilesInterface>(
+    () => ({
+      name: ''
+    }),
+    []
+  )
+
+  const methods = useForm<AddPermissionProfileFormType>({
+    resolver: zodResolver(AddPermissionProfilesFormSchema),
+    defaultValues
+  })
+
+  useEffect(() => {
+    if (isOpen) {
+      methods.reset(defaultValues)
+    }
+  }, [isOpen, defaultValues, methods])
+
+  return <FormProvider {...methods}>{children}</FormProvider>
+}
