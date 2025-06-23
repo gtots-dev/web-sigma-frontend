78fb8be631758e992f60559b763c6e1a53a4d080
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-23 18:00:45 +0000

    feat: Configurações de permissões estáticas

---
+++ src/modules/permissions/infrastructure/configs/features.config.ts
@@ -0,0 +1,10 @@
+export const FEATURES = [
+  {
+    name: 'Usuários',
+    features: [
+      { id: 1, name: 'Adicionar' },
+      { id: 2, name: 'Desabilitar' },
+      { id: 3, name: 'Visualizar' }
+    ]
+  }
+]
