077178d506da457c3d45c032f782c7a10e8dbbc1
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-24 13:58:14 +0000

    feat: Feature de redefinição de senha

--- src/modules/permissions/infrastructure/configs/features.config.ts
+++ src/modules/permissions/infrastructure/configs/features.config.ts
@@ -2,9 +2,10 @@ export const FEATURES = [
   {
     name: 'Usuários',
     features: [
+      { id: 3, name: 'Visualizar' },
       { id: 1, name: 'Adicionar' },
       { id: 2, name: 'Desabilitar' },
-      { id: 3, name: 'Visualizar' }
+      { id: 5, name: 'Redefinição de Senha' }
     ]
   }
 ]
