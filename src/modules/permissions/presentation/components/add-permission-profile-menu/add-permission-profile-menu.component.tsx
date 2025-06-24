9e1874d7232a394f24638f94269d14cdc27ef2ec
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-24 13:56:02 +0000

    feat: Inserção do hook de ação de envio de dados do formulário

--- src/modules/permissions/presentation/components/add-permission-profile-menu/add-permission-profile-menu.component.tsx
+++ src/modules/permissions/presentation/components/add-permission-profile-menu/add-permission-profile-menu.component.tsx
@@ -6,6 +6,8 @@ import { PermissionProfileForm } from '../permission-profile-form'
 import { AddPermissionProfileForm } from '../add-permission-profile-form-provider'
 import { useDialog } from '@/modules/permissions/presentation/components/add-permission-profile-menu/add-permission-profile-menu-provider.component'
 import { FEATURES } from '@/modules/permissions/infrastructure/configs/features.config'
+import { useAddPermissionProfileSubmit } from '../../hooks/use-add-permission-profile-submit.hook'
+import type { PermissionProfilesInterface } from '@/modules/permissions/domain/interfaces/permission-profiles.interface'
 
 interface AddPermissionProfileMenuComponentProps {
   title: string
@@ -17,6 +19,7 @@ export function AddPermissionProfileMenuComponent({
   description
 }: AddPermissionProfileMenuComponentProps) {
   const { isOpen, close } = useDialog()
+  const { onAction } = useAddPermissionProfileSubmit()
 
   return (
     <AddPermissionProfileMenu.Root>
@@ -42,7 +45,11 @@ export function AddPermissionProfileMenuComponent({
             >
               Cancelar
             </Button>
-            <PermissionProfileForm.Submit onSubmit={() => {}} />
+            <PermissionProfileForm.Submit
+              onSubmit={(permissionProfile: PermissionProfilesInterface) =>
+                onAction(permissionProfile, close)
+              }
+            />
           </AddPermissionProfileMenu.Footer>
         </AddPermissionProfileForm.Provider>
       </AddPermissionProfileMenu.Content>
