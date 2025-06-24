3f483804e5eb0b8b3b0382fc4583f12b4cf49114
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-24 13:56:21 +0000

    feat: Validação do zod para o atributo de features

--- src/modules/permissions/presentation/schemas/add-permission-profile-form.schema.ts
+++ src/modules/permissions/presentation/schemas/add-permission-profile-form.schema.ts
@@ -4,7 +4,8 @@ import { z } from 'zod'
 export const AddPermissionProfilesFormSchema = z.object({
   name: z.string().nonempty({
     message: MESSAGES_PERMISSIONS[6.7]
-  })
+  }),
+  features: z.array(z.number()).optional()
 })
 
 export type AddPermissionProfileFormType = z.infer<
