142146396a42cb2c0997db7f8d5792a5aa64e83b
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-26 17:56:30 +0000

    feat: Componente de descrição

--- src/modules/permissions/presentation/components/permission-profile-form/index.ts
+++ src/modules/permissions/presentation/components/permission-profile-form/index.ts
@@ -1,17 +1,17 @@
 'use client'
 
 import { PermissionProfileFormInputNameComponent } from './permission-profile-form-input-name.component'
-import { PermissionProfileFormInputEnabledComponent } from './permission-profile-form-input-enabled.component'
 import { PermissionProfileFormSubmitComponent } from './permission-profile-form-submit.component'
 import { PermissionProfileFormComponent } from './permission-profile-form.component'
 import { PermissionProfileFormInputFeaturesComponent } from './permission-profile-form-input-features.component'
+import { PermissionProfileFormInputDescriptionComponent } from './permission-profile-form-input-description.component'
 
 export const PermissionProfileForm = {
   Form: PermissionProfileFormComponent,
   Submit: PermissionProfileFormSubmitComponent,
   Input: {
     Name: PermissionProfileFormInputNameComponent,
-    Features: PermissionProfileFormInputFeaturesComponent,
-    Enabled: PermissionProfileFormInputEnabledComponent
+    Description: PermissionProfileFormInputDescriptionComponent,
+    Features: PermissionProfileFormInputFeaturesComponent
   }
 }
