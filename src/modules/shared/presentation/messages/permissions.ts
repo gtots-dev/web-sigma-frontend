392d6db6241acf32048136334515b1fdd17cab95
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-26 17:51:21 +0000

    feat: Mensagens relacionadas ao campo de descrição

--- src/modules/shared/presentation/messages/permissions.ts
+++ src/modules/shared/presentation/messages/permissions.ts
@@ -1,4 +1,13 @@
-type MessageKeys = '6.1' | '6.2' | '6.3' | '6.4' | '6.5' | '6.6' | '6.7'
+type MessageKeys =
+  | '6.1'
+  | '6.2'
+  | '6.3'
+  | '6.4'
+  | '6.5'
+  | '6.6'
+  | '6.7'
+  | '6.8'
+  | '6.9'
 
 export const MESSAGES_PERMISSIONS: Record<MessageKeys, string> = {
   '6.1': 'Permissões',
@@ -7,5 +16,7 @@ export const MESSAGES_PERMISSIONS: Record<MessageKeys, string> = {
   '6.4': 'Novo perfil de permissão',
   '6.5': 'Preencha os dados obrigatórios para adicionar um novo perfil.',
   '6.6': 'Nenhuma permissão encontrada.',
-  '6.7': 'O nome do perfil é obrigatório.'
+  '6.7': 'O nome do perfil é obrigatório.',
+  '6.8': 'A descrição do perfil é obrigatória.',
+  '6.9': 'A descrição deve ter no máximo 255 caracteres.'
 }
