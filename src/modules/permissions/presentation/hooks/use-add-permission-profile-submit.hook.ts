d8d925f007b8dec40ad656419a5cd37bb1a03c1d
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-26 21:35:51 +0000

    feat: Injeção das requisições das stores de permissões

--- src/modules/permissions/presentation/hooks/use-add-permission-profile-submit.hook.ts
+++ src/modules/permissions/presentation/hooks/use-add-permission-profile-submit.hook.ts
@@ -4,14 +4,24 @@ import { useCallback } from 'react'
 import { toast } from '@/modules/shared/presentation/components/hooks/use-toast'
 import { HttpResponseError } from '@/modules/shared/infrastructure/errors/http-response.error'
 import type { PermissionProfileInterface } from '../../domain/interfaces/permission-profiles.interface'
+import { usePermissionProfileStore } from '../stores/permission-profile.store'
+import { useOperationStore } from '@/modules/system/presentation/store/operation.store'
 
 export function useAddPermissionProfileSubmit() {
+  const { operation } = useOperationStore()
+  const { addPermissionProfile, getPermissionProfiles } =
+    usePermissionProfileStore()
   const onAction = useCallback(
     async (
       permissionProfile: PermissionProfileInterface,
       onSuccess: VoidFunction
     ): Promise<void> => {
       try {
+        await addPermissionProfile({
+          ...permissionProfile,
+          operation_id: Number(operation.id)
+        })
+        await getPermissionProfiles()
         toast({
           title: 'Perfil de permissão adicionado com sucesso!',
           description: JSON.stringify(permissionProfile),
@@ -29,7 +39,7 @@ export function useAddPermissionProfileSubmit() {
         }
       }
     },
-    []
+    [addPermissionProfile, getPermissionProfiles]
   )
 
   return { onAction }
