1dd5d71913c76cc249778fa7a3447fa6ba2e1b9b
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-27 07:36:07 +0000

    refactor: Parâmetros nativos e síncrono

--- src/app/api/permission/[permissionProfileId]/feature/[featureId]/route.ts
+++ src/app/api/permission/[permissionProfileId]/feature/[featureId]/route.ts
@@ -3,20 +3,25 @@ import { HttpStatusCodeEnum } from '@/modules/authentication/domain/enums/status
 import { HttpResponseError } from '@/modules/shared/infrastructure/errors/http-response.error'
 import { DeleteFeatureFactory } from '@/modules/permissions/infrastructure/factories/delete-feature.factory'
 import { NextResponse, type NextRequest } from 'next/server'
-import type { GetStaticPropsContext } from 'next'
+import type { PermissionProfileInterface } from '@/modules/permissions/domain/interfaces/permission-profiles.interface'
+import type { FeaturesInterface } from '@/modules/permissions/domain/interfaces/features.interface'
 
 export async function DELETE(
   req: NextRequest,
-  context: GetStaticPropsContext<{
-    permissionProfileId: string
-    featureId: string
-  }>
+  {
+    params
+  }: {
+    params: Promise<{
+      featureId: FeaturesInterface['id']
+      permissionProfileId: PermissionProfileInterface['id']
+    }>
+  }
 ): Promise<NextResponse> {
-  const { featureId, permissionProfileId } = context.params
   try {
+    const { permissionProfileId, featureId } = await params
     const { token } = await auth()
     const deleteFeatureFactory = DeleteFeatureFactory.create()
-    deleteFeatureFactory.execute(
+    await deleteFeatureFactory.execute(
       token,
       Number(featureId),
       Number(permissionProfileId)
