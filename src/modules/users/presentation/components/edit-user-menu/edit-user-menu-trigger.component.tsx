4143f6303acf3a4ef8b37a74d02a4e654a92fe3f
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-27 06:17:17 +0000

    fix: Abertura e fechamento do Menu de edição de usuários

--- src/modules/users/presentation/components/edit-user-menu/edit-user-menu-trigger.component.tsx
+++ src/modules/users/presentation/components/edit-user-menu/edit-user-menu-trigger.component.tsx
@@ -4,11 +4,11 @@ import { Button } from '@/modules/shared/presentation/components/shadcn/button'
 import { useDialog } from './edit-user-menu-provider.component'
 
 export function EditUserMenuTriggerComponent() {
-  const { open } = useDialog()
+  const { open: openDialog } = useDialog()
   return (
     <Button
+      onClick={() => queueMicrotask(() => openDialog())}
       className="justify-start w-full h-auto cursor-pointer p-1.5 ps-3 rounded-none text-sm disabled:bg-muted-foreground [&>svg]:size-4 [&>svg]:shrink-0 shadow-none"
-      onClick={open}
     >
       Editar
     </Button>
