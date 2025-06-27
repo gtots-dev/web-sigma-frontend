5eb20d1f254ea88aba3e1c5d62ad04d5090ec482
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-27 05:57:26 +0000

    feat: Exportação da tipagem da extensão do perfil de permissão com features

--- src/modules/permissions/presentation/hooks/use-add-permission-profile-submit.hook.ts
+++ src/modules/permissions/presentation/hooks/use-add-permission-profile-submit.hook.ts
@@ -7,7 +7,7 @@ import type { PermissionProfileInterface } from '../../domain/interfaces/permiss
 import { usePermissionProfileStore } from '../stores/permission-profile.store'
 import { useOperationStore } from '@/modules/system/presentation/store/operation.store'
 
-type ExtendedPermissionProfile = PermissionProfileInterface & {
+export type ExtendedPermissionProfile = PermissionProfileInterface & {
   features: number[]
 }
 
