03bf5952b5dbba0780f03b122ef3b88d7f51a7b0
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-27 03:22:50 +0000

    feat: Provedor do perfil de permissão para o formulário

---
+++ src/modules/permissions/presentation/components/edit-permission-profile-form-provider/index.ts
@@ -0,0 +1,5 @@
+import { EditPermissionProfileFormContextProviderComponent } from './edit-permission-profile-form-provider.component'
+
+export const EditPermissionProfileForm = {
+  Provider: EditPermissionProfileFormContextProviderComponent
+}
