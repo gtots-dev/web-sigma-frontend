39bbc091eb74994e0c85a2d04cc2f4c9f8c5fbdd
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-27 05:02:06 +0000

    feat: Gancho para listar as features por meio do botão de abrir o menu de editar perfil

--- src/modules/permissions/presentation/components/edit-permission-profile-menu/edit-permission-profile-menu-trigger.component.tsx
+++ src/modules/permissions/presentation/components/edit-permission-profile-menu/edit-permission-profile-menu-trigger.component.tsx
@@ -2,13 +2,23 @@
 
 import { Button } from '@/modules/shared/presentation/components/shadcn/button'
 import { useDialog } from './edit-permission-profile-menu-provider.component'
+import { usePermissionProfileStore } from '../../stores/permission-profile.store'
+import { useTablePermissionProfile } from '../../contexts/table-permission-profiles.context'
 
 export function EditPermissionProfileMenuTriggerComponent() {
-  const { open } = useDialog()
+  const { open: openDialog } = useDialog()
+  const { id: permissionProfileId } = useTablePermissionProfile()
+  const { getFeatures } = usePermissionProfileStore()
+
+  const handleClick = async () => {
+    await getFeatures(permissionProfileId)
+    openDialog()
+  }
+
   return (
     <Button
       className="justify-start w-full h-auto cursor-pointer p-1.5 ps-3 rounded-none text-sm disabled:bg-muted-foreground [&>svg]:size-4 [&>svg]:shrink-0 shadow-none"
-      onClick={open}
+      onClick={handleClick}
     >
       Editar
     </Button>
