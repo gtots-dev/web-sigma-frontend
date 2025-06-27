f05a0d86d7a012e71caf968f7e1974aafd39a0d8
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-27 14:14:21 +0000

    debug: Log de autenticação

--- src/modules/authentication/infrastructure/auth/logger-error.auth.ts
+++ src/modules/authentication/infrastructure/auth/logger-error.auth.ts
@@ -3,5 +3,6 @@ import { CredentialsSignin } from 'next-auth'
 export function LoggerErrorAuth(error: Error): void {
   if (error instanceof CredentialsSignin) {
     console.log('[Auth] Credenciais inválidas.')
+    console.log(error)
   }
 }
