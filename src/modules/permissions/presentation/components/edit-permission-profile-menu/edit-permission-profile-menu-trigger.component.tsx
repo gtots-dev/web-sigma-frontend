27be311e8b417ec3626febf858d1d849c5135967
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-30 11:47:09 +0000

    fix: Abrir/Fechar menu de "editar" do perfil de permissão

--- src/modules/permissions/presentation/components/edit-permission-profile-menu/edit-permission-profile-menu-trigger.component.tsx
+++ src/modules/permissions/presentation/components/edit-permission-profile-menu/edit-permission-profile-menu-trigger.component.tsx
@@ -1,24 +1,16 @@
 'use client'
 
 import { Button } from '@/modules/shared/presentation/components/shadcn/button'
-import { useDialog } from './edit-permission-profile-menu-provider.component'
-import { usePermissionProfileStore } from '../../stores/permission-profile.store'
-import { useTablePermissionProfile } from '../../contexts/table-permission-profiles.context'
+import { useEditPermissionProfileMenuTrigger } from '../../hooks/use-edit-permission-profile-menu-trigger.hook'
 
 export function EditPermissionProfileMenuTriggerComponent() {
-  const { open: openDialog } = useDialog()
-  const { id: permissionProfileId } = useTablePermissionProfile()
-  const { getFeatures } = usePermissionProfileStore()
-
-  const handleClick = async () => {
-    await getFeatures(permissionProfileId)
-    openDialog()
-  }
+  const { loadPermissionProfileEditOpenDialog } =
+    useEditPermissionProfileMenuTrigger()
 
   return (
     <Button
       className="justify-start w-full h-auto cursor-pointer p-1.5 ps-3 rounded-none text-sm disabled:bg-muted-foreground [&>svg]:size-4 [&>svg]:shrink-0 shadow-none"
-      onClick={handleClick}
+      onClick={loadPermissionProfileEditOpenDialog}
     >
       Editar
     </Button>
