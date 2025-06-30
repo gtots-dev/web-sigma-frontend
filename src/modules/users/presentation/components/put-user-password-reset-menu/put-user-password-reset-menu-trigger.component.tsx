5a0f90b79a6b4582d526899fa09b5c4c314f8a0e
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-30 11:45:57 +0000

    fix: Abrir/Fechar menu de "redefinir senha" do usuário

--- src/modules/users/presentation/components/put-user-password-reset-menu/put-user-password-reset-menu-trigger.component.tsx
+++ src/modules/users/presentation/components/put-user-password-reset-menu/put-user-password-reset-menu-trigger.component.tsx
@@ -1,14 +1,14 @@
 'use client'
 
 import { Button } from '@/modules/shared/presentation/components/shadcn/button'
-import { useDialog } from './put-user-password-reset-menu-provider.component'
+import { usePasswordResetUserMenuTrigger } from '../../hooks/use-password-reset-user-menu-trigger.hook'
 
 export function PutUserPasswordResetMenuTriggerComponent() {
-  const { open } = useDialog()
+  const { loadUserPasswordResetOpenDialog } = usePasswordResetUserMenuTrigger()
   return (
     <Button
       className="justify-start w-full h-auto cursor-pointer p-1.5 ps-3 rounded-none text-sm disabled:bg-muted-foreground [&>svg]:size-4 [&>svg]:shrink-0 shadow-none"
-      onClick={open}
+      onClick={loadUserPasswordResetOpenDialog}
     >
       Redefinir Senha
     </Button>
