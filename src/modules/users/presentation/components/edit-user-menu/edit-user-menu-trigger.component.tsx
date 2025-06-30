7d13144d3864ff65923d8fee61e6a30938ff785f
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-06-30 11:46:13 +0000

    fix: Abrir/Fechar menu de "editar" do usuário

--- src/modules/users/presentation/components/edit-user-menu/edit-user-menu-trigger.component.tsx
+++ src/modules/users/presentation/components/edit-user-menu/edit-user-menu-trigger.component.tsx
@@ -1,13 +1,13 @@
 'use client'
 
 import { Button } from '@/modules/shared/presentation/components/shadcn/button'
-import { useDialog } from './edit-user-menu-provider.component'
+import { useEditUserMenuTrigger } from '../../hooks/use-edit-user-menu-trigger.hook'
 
 export function EditUserMenuTriggerComponent() {
-  const { open: openDialog } = useDialog()
+  const { loadUserEditOpenDialog } = useEditUserMenuTrigger()
   return (
     <Button
-      onClick={() => queueMicrotask(() => openDialog())}
+      onClick={loadUserEditOpenDialog}
       className="justify-start w-full h-auto cursor-pointer p-1.5 ps-3 rounded-none text-sm disabled:bg-muted-foreground [&>svg]:size-4 [&>svg]:shrink-0 shadow-none"
     >
       Editar
