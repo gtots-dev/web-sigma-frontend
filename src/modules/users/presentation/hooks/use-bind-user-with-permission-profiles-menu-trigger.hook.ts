bfb7cb962cfebf01b83d31187282cf1c9aba7bd5
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-07-01 21:05:04 +0000

    feat: Injeção das requisições dos listagem de perfis de permissão e perfil de permissões do usuário

--- src/modules/users/presentation/hooks/use-bind-user-with-permission-profiles-menu-trigger.hook.ts
+++ src/modules/users/presentation/hooks/use-bind-user-with-permission-profiles-menu-trigger.hook.ts
@@ -1,12 +1,22 @@
 import { toast } from '@/modules/shared/presentation/components/hooks/use-toast'
 import { useDialog } from '../components/bind-user-with-permission-profiles-menu/bind-user-with-permission-profiles-menu-provider.component'
+import { usePermissionProfileWithUserStore } from '../stores/user-permission-profile.store'
+import { useTableUser } from '../contexts/table-user.context'
+import { usePermissionProfileStore } from '@/modules/permissions/presentation/stores/permission-profile.store'
 
 export function useBindUserWithPermissionProfilesMenuTrigger() {
   const { open: openDialog } = useDialog()
+  const { id: userId } = useTableUser()
+  const { getUserWithPermissionProfiles } = usePermissionProfileWithUserStore()
+  const { getPermissionProfiles } = usePermissionProfileStore()
 
   const loadUserWithPermissionProfileBindOpenDialog = () => {
     queueMicrotask(async () => {
       try {
+        await Promise.all([
+          getUserWithPermissionProfiles(userId),
+          getPermissionProfiles()
+        ])
         openDialog()
       } catch {
         toast({
