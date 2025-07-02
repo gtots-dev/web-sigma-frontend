49bc4f3656e09a037a2a696a51bb1a7a8796cc23
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-07-02 16:22:55 +0000

    feat: Rota de vincular perfil de permissão ao usuário (Router Api)

--- src/app/api/user/[userId]/permission-profile/route.ts
+++ src/app/api/user/[userId]/permission-profile/route.ts
@@ -1,9 +1,42 @@
 import { auth } from '@/auth'
 import { HttpStatusCodeEnum } from '@/modules/authentication/domain/enums/status-codes.enum'
+import type { PermissionProfileEntity } from '@/modules/permissions/domain/entities/permission-profile.entity'
 import { HttpResponseError } from '@/modules/shared/infrastructure/errors/http-response.error'
 import type { UserEntity } from '@/modules/users/domain/entities/user.entity'
 import { GetUserWithPermissionProfileFactory } from '@/modules/users/infrastructure/factories/get-user-with-permission-profile.factory'
+import { PostBindUserWithPermissionProfileFactory } from '@/modules/users/infrastructure/factories/post-bind-user-with-permission-profile.factory'
 import { NextResponse, type NextRequest } from 'next/server'
+
+export async function POST(
+  req: NextRequest,
+  { params }: { params: Promise<{ userId: UserEntity['id'] }> }
+): Promise<NextResponse> {
+  try {
+    const { userId } = await params
+    const permissionProfileIds = await req.json()
+    const { token } = await auth()
+    const postBindUserWithPermissionProfileFactory =
+      PostBindUserWithPermissionProfileFactory.create()
+    await postBindUserWithPermissionProfileFactory.execute(
+      token,
+      permissionProfileIds,
+      userId
+    )
+    return NextResponse.json({
+      status: Number(HttpStatusCodeEnum.OK)
+    })
+  } catch (error) {
+    if (error instanceof HttpResponseError) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: error.message
+        },
+        { status: Number(HttpStatusCodeEnum.BAD_REQUEST) }
+      )
+    }
+  }
+}
 
 export async function GET(
   req: NextRequest,
