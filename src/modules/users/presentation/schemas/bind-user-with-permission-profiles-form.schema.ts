1bab1f1ff4a16a19e72738d5ed1a953720a4a184
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-07-02 16:32:07 +0000

    refactor: Removido validação de sem perfil selecionado, adicionado id do usuário

--- src/modules/users/presentation/schemas/bind-user-with-permission-profiles-form.schema.ts
+++ src/modules/users/presentation/schemas/bind-user-with-permission-profiles-form.schema.ts
@@ -1,10 +1,8 @@
-import { MESSAGES_PERMISSIONS } from '@/modules/shared/presentation/messages/permissions'
 import { z } from 'zod'
 
 export const BindUserWithPermissionProfileFormSchema = z.object({
-  perm_profile_id: z.array(z.number()).min(1, {
-    message: MESSAGES_PERMISSIONS[6.12]
-  })
+  user_id: z.number(),
+  perm_profile_id: z.array(z.number())
 })
 
 export type BindUserWithPermissionProfileFormType = z.infer<
