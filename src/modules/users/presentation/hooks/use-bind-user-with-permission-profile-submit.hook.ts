668372324b93d946bb932f066a892dfcfcd32061
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-07-15 14:31:32 +0000

    feat: hook de envio de solicitação para o backend

--- src/modules/users/presentation/hooks/use-bind-user-with-permission-profile-submit.hook.ts
+++ src/modules/users/presentation/hooks/use-bind-user-with-permission-profile-submit.hook.ts
@@ -1,18 +1,47 @@
 import { useCallback } from 'react'
 import { toast } from '@/modules/shared/presentation/components/hooks/use-toast'
 import { HttpResponseError } from '@/modules/shared/infrastructure/errors/http-response.error'
-import type { PermissionProfileEntity } from '@/modules/permissions/domain/entities/permission-profile.entity'
+import { usePermissionProfileWithUserStore } from '../stores/user-permission-profile.store'
+import type { PermissionProfileWithUserInterface } from '@/modules/permissions/domain/interfaces/permission-profile-with-user.interface'
+import type { PermissionsProfileIdsWithUserIdInterface } from '../../domain/interfaces/permissions-profile-ids-with-user-id.interface'
 
 export function useBindUserWithPermissionProfileSubmit() {
+  const {
+    bindUserWithPermissionProfile,
+    deleteBindUserWithPermissionProfile,
+    userWithPermissionProfiles
+  } = usePermissionProfileWithUserStore()
+
   const onAction = useCallback(
     async (
-      permissionProfiles: PermissionProfileEntity['id'][],
+      {
+        user_id: userId,
+        perm_profile_id: permissionProfileIds
+      }: PermissionsProfileIdsWithUserIdInterface,
       onSuccess: VoidFunction
     ): Promise<void> => {
       try {
+        const currentProfileIds = userWithPermissionProfiles.map(
+          ({ id }: PermissionProfileWithUserInterface) => id
+        )
+        const toDelete = currentProfileIds.filter(
+          (id) => !permissionProfileIds.includes(id)
+        )
+        const toAdd = permissionProfileIds.filter(
+          (id) => !currentProfileIds.includes(id)
+        )
+        await Promise.all([
+          ...toDelete.map((id) =>
+            deleteBindUserWithPermissionProfile(id, userId)
+          ),
+          toAdd.length > 0
+            ? bindUserWithPermissionProfile(toAdd, userId)
+            : Promise.resolve()
+        ])
+
         toast({
           title: 'Perfis de permissão vinculados com sucesso!',
-          description: `Os perfis selecionados foram atribuídos ao usuário. ${JSON.stringify(permissionProfiles)}`,
+          description: `Os perfis selecionados foram atribuídos ao usuário.`,
           variant: 'success'
         })
         onSuccess?.()
@@ -27,7 +56,11 @@ export function useBindUserWithPermissionProfileSubmit() {
         }
       }
     },
-    []
+    [
+      bindUserWithPermissionProfile,
+      deleteBindUserWithPermissionProfile,
+      userWithPermissionProfiles
+    ]
   )
 
   return { onAction }
