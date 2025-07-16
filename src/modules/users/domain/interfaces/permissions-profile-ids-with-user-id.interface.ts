e34a3d3581dd0c8955e85a7b92982e7b0fa43e1e
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-07-16 15:45:50 +0000

    feat: Atributo de id de contrato para interface de permissões e usuários

--- src/modules/users/domain/interfaces/permissions-profile-ids-with-user-id.interface.ts
+++ src/modules/users/domain/interfaces/permissions-profile-ids-with-user-id.interface.ts
@@ -1,7 +1,9 @@
 import type { PermissionProfileEntity } from '@/modules/permissions/domain/entities/permission-profile.entity'
 import type { UserEntity } from '../entities/user.entity'
+import type { ContractEntity } from '@/modules/contracts/domain/entities/contract.entity'
 
 export interface PermissionsProfileIdsWithUserIdInterface {
   perm_profile_id: PermissionProfileEntity['id'][]
   user_id: UserEntity['id']
+  contract_id: ContractEntity['id'][]
 }
