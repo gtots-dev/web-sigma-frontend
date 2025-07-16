fae0c88dac3b62fd0c1ce9149487fe8980715fe4
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-07-16 15:44:02 +0000

    feat: Atributo de ids de contrato no esquema de validação do zod

--- src/modules/users/presentation/schemas/bind-user-with-permission-profiles-form.schema.ts
+++ src/modules/users/presentation/schemas/bind-user-with-permission-profiles-form.schema.ts
@@ -2,7 +2,8 @@ import { z } from 'zod'
 
 export const BindUserWithPermissionProfileFormSchema = z.object({
   user_id: z.number(),
-  perm_profile_id: z.array(z.number())
+  perm_profile_id: z.array(z.number()),
+  contract_id: z.array(z.number())
 })
 
 export type BindUserWithPermissionProfileFormType = z.infer<
