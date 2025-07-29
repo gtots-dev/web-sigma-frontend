04dcb0c8fdfd3795d3c80989f8d258f51bad3c38
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-07-29 13:37:33 +0000

    refactor: Lista de verificação de permissões

---
+++ src/modules/system/domain/enums/permissions.enum.ts
@@ -0,0 +1,10 @@
+export enum PermissionEnum {
+  NOT_REQUIRED = 'not_access_permission',
+  CONTRACTS_VIEW = 'contracts_view',
+  PROCESSING_UNITS_VIEW = 'processing_units_view',
+  LANES_VIEW = 'lanes_view',
+  POINTS_VIEW = 'points_view',
+  OPERATORS_VIEW = 'operators_view',
+  USERS_VIEW = 'users_view',
+  PERMISSIONS_VIEW = 'permission_view'
+}
