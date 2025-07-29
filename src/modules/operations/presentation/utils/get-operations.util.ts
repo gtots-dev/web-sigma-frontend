fb338818ebbe12ca5e511af6ce8e3a24c101c5c3
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-07-29 13:34:55 +0000

    refactor: Desacoplamento do JWT do utilitário de operações

--- src/modules/operations/presentation/utils/get-operations.util.ts
+++ src/modules/operations/presentation/utils/get-operations.util.ts
@@ -1,9 +1,10 @@
-import { auth } from '@/auth'
 import { GetOperationsFactory } from '../../infrastructure/factories/get-operations.factory'
+import type { TokenEntities } from '@/modules/authentication/domain/entities/token.entity'
 import type { OperationEntity } from '../../domain/entities/operation.entity'
 
-export async function getOperations(): Promise<OperationEntity[]> {
-  const { token } = await auth()
+export async function getOperations(
+  JWT: TokenEntities
+): Promise<OperationEntity[]> {
   const getOperations = GetOperationsFactory.create()
-  return await getOperations.execute(token)
+  return await getOperations.execute(JWT)
 }
