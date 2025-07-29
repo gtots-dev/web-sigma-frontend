25a7c65c86ab2a53e1f4e640c2ba5ed79d8ec8aa
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-07-29 18:33:33 +0000

    refactor: Alterado local de aplicação de seleção, redirecionado ao botão checkbox

--- src/modules/shared/presentation/components/group-item-selector/group-item-selector-check-checkbox.component.tsx
+++ src/modules/shared/presentation/components/group-item-selector/group-item-selector-check-checkbox.component.tsx
@@ -1,22 +1,30 @@
+'use client'
+
 import { Check, Square } from 'lucide-react'
 import { cn } from '../../lib/utils'
+import { useGroupItemSelectorContext } from '../../contexts/group-item-selector.context'
+import type { BaseItem } from './group-item-selector-list.component'
 
-interface GroupItemSelectorCheckCheckboxComponentProps {
+interface GroupItemSelectorCheckCheckboxComponentProps<Item extends BaseItem> {
   selected: boolean
   className?: string
+  item: Item
 }
 
-export function GroupItemSelectorCheckCheckboxComponent({
+export function GroupItemSelectorCheckCheckboxComponent<Item extends BaseItem>({
   selected,
-  className
-}: GroupItemSelectorCheckCheckboxComponentProps) {
+  className,
+  item
+}: GroupItemSelectorCheckCheckboxComponentProps<Item>) {
+  const { toggleItem } = useGroupItemSelectorContext<Item>()
+
   return (
-    <>
+    <button onClick={() => item && toggleItem(item)} type="button">
       {selected ? (
         <Check className={cn('!w-3.5 !h-3.5', className)} />
       ) : (
         <Square className={cn('!w-3.5 !h-3.5', className)} />
       )}
-    </>
+    </button>
   )
 }
