197ba6630e8966010c7ee2c203de1b9559c1cd7a
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-08-13 21:59:11 +0000

    feat: Componente de menu de habilitar/Desabilitar (Permissões)

---
+++ src/modules/permissions/presentation/components/put-permission-profile-status-menu/put-permission-profile-status-menu-trigger.component.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { Button } from '@/modules/shared/presentation/components/shadcn/button'
+import { usePutPermissionProfileStatusMenuTrigger } from '../../hooks/use-put-permission-profile-status-menu-trigger.hook'
+
+export function PutPermissionProfileStatusMenuTriggerComponent() {
+  const { loadUserStatusOpenDialog } = usePutPermissionProfileStatusMenuTrigger()
+  return (
+    <Button
+      className="justify-start w-full h-auto cursor-pointer p-1.5 ps-3 rounded-none text-sm disabled:bg-muted-foreground [&>svg]:size-4 [&>svg]:shrink-0 shadow-none"
+      onClick={loadUserStatusOpenDialog}
+    >
+     Habilitar/Desabilitar
+    </Button>
+  )
+}
