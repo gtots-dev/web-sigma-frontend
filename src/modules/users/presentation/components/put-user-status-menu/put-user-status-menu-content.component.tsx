61366c4e5d981fba2f6f0c76e12df01fa8d206c6
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-08-13 20:18:27 +0000

    feat: Componente de menu de habilitar/Desabilitar (Usuário)

---
+++ src/modules/users/presentation/components/put-user-status-menu/put-user-status-menu-content.component.tsx
@@ -0,0 +1,18 @@
+import { DrawerDialog } from '@/modules/shared/presentation/components/dialog-with-drawer'
+import { type ReactNode } from 'react'
+
+interface PutUserStatusMenuContentComponentProps {
+  children: ReactNode
+  className?: string
+}
+
+export function PutUserStatusMenuContentComponent({
+  children,
+  className
+}: PutUserStatusMenuContentComponentProps) {
+  return (
+    <DrawerDialog.Content className={className}>
+      {children}
+    </DrawerDialog.Content>
+  )
+}
---
+++ src/modules/users/presentation/components/put-user-status-menu/put-user-status-menu.component.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { UserForm } from '../user-form'
+import { Button } from '@/modules/shared/presentation/components/shadcn/button'
+import { PutUserStatusMenu } from '.'
+import { useTableUser } from '../../contexts/table-user.context'
+import { PutUserStatusForm } from '../put-user-status-form-provider'
+import { useDialog } from './put-user-status-menu-provider.component'
+import { usePutUserStatusSubmit } from '../../hooks/use-update-user-status-submit.hook'
+
+interface PutUserStatusMenuComponentProps {
+  title: string
+  description: string
+}
+
+export function PutUserStatusMenuComponent({
+  title,
+  description
+}: PutUserStatusMenuComponentProps) {
+  const { isOpen, close } = useDialog()
+  const { onAction } = usePutUserStatusSubmit()
+  const user = useTableUser()
+
+  return (
+    <PutUserStatusMenu.Root>
+      <PutUserStatusMenu.Content className="lg:!w-[600px] lg:!h-auto">
+        <PutUserStatusMenu.Header title={title} description={description} />
+
+        <PutUserStatusForm.Provider user={user} isOpen={isOpen}>
+          <UserForm.Form>
+            <UserForm.Input.Enabled />
+          </UserForm.Form>
+
+          <PutUserStatusMenu.Footer>
+            <Button
+              className="w-full sm:w-[150px]"
+              variant="outline"
+              onClick={close}
+            >
+              Cancelar
+            </Button>
+            <UserForm.Submit
+              onSubmit={(userStatus: { enabled: boolean; userId: number }) =>
+                onAction(userStatus, close)
+              }
+            />
+          </PutUserStatusMenu.Footer>
+        </PutUserStatusForm.Provider>
+      </PutUserStatusMenu.Content>
+    </PutUserStatusMenu.Root>
+  )
+}
