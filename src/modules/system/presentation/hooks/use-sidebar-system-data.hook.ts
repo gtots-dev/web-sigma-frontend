e1f9ac79d52d25f284dbaca8957c7c676592cfe3
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-08-13 13:27:51 +0000

    refactor: Hook de retorno dos elementos da lista do menu lateral

--- src/modules/system/presentation/hooks/use-sidebar-system-data.hook.ts
+++ src/modules/system/presentation/hooks/use-sidebar-system-data.hook.ts
@@ -1,25 +1,25 @@
 'use client'
 
 import { usePathname } from 'next/navigation'
-import { useEffect, useMemo, useState } from 'react'
+import { useMemo } from 'react'
 import { getSidebarData } from '@/modules/system/infrastructure/configs/sidebar.config'
 import { extractOperationId } from '../utils/export-operation-id.util'
 import { filterSidebarByPermissions } from '../utils/filter-sidebar-by-permissions.util'
 import type { UserPermissionsInterface } from '@/modules/users/domain/interfaces/user-permissions.interface'
 
-export function useSidebarSystemData(permissions: UserPermissionsInterface) {
+export function useSidebarSystemData(
+  permissions: UserPermissionsInterface,
+  isAdmin?: boolean
+) {
   const pathname = usePathname()
-  const [operationId, setOperationId] = useState('')
-
-  useEffect(() => {
-    const extractedId = extractOperationId(pathname)
-    setOperationId(extractedId)
-  }, [pathname])
+  const operationId = useMemo(() => extractOperationId(pathname), [pathname])
 
   const sidebarData = useMemo(() => {
     const rawData = getSidebarData(Number(operationId))
-    return filterSidebarByPermissions(rawData, permissions, operationId)
-  }, [operationId, permissions])
+    return isAdmin
+      ? rawData
+      : filterSidebarByPermissions(rawData, permissions, operationId)
+  }, [operationId, permissions, isAdmin])
 
   return {
     pathname,
