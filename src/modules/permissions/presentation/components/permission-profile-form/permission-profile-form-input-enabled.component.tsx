24f7c43e7236655453ee91621ca5ed149a2f902b
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-08-13 22:01:14 +0000

    feat: Componente de provedor do formulário de habilitar/Desabilitar (Permissão)

--- src/modules/permissions/presentation/components/permission-profile-form/permission-profile-form-input-enabled.component.tsx
+++ src/modules/permissions/presentation/components/permission-profile-form/permission-profile-form-input-enabled.component.tsx
@@ -8,6 +8,7 @@ import {
 } from '@/modules/shared/presentation/components/shadcn/form'
 import { Switch } from '@/modules/shared/presentation/components/shadcn/switch'
 import { useFormContext } from 'react-hook-form'
+import { useTablePermissionProfile } from '../../contexts/table-permission-profiles.context'
 
 interface PermissionProfileFormInputEnabledComponentProps {
   require?: boolean
@@ -19,6 +20,7 @@ export function PermissionProfileFormInputEnabledComponent({
   description
 }: PermissionProfileFormInputEnabledComponentProps) {
   const { control } = useFormContext()
+  const permissionProfile = useTablePermissionProfile()
 
   return (
     <FormField
@@ -28,24 +30,30 @@ export function PermissionProfileFormInputEnabledComponent({
         <FormItem>
           <FormLabel
             className="text-sm flex items-center gap-x-1.5 dark:text-zinc-50"
-            htmlFor="enabled-user"
+            htmlFor="enabled-permission"
           >
-            Habilitado{require ? ': *' : ':'}
             <HelpMeButtonComponent description={description} />
           </FormLabel>
 
           <FormControl>
-            <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
+            <div className="flex flex-row items-center justify-between rounded-lg gap-x-2 border p-3 shadow-sm">
               <div className="space-y-0.5">
-                <FormLabel>Habilitar usuário?</FormLabel>
+                <FormLabel>
+                  Permissão {field.value ? 'Habilitada' : 'Desabilitada'}
+                </FormLabel>
                 <FormDescription>
-                  Caso a opção correspondente esteja desabilitada, o usuário não
-                  poderá utilizar os recursos da aplicação.
+                  Permissão{' '}
+                  <b className="text-white/80">{permissionProfile.name}</b> será{' '}
+                  <b className="text-white/80">
+                    {' '}
+                    {!field.value ? 'Habilitada' : 'Desabilitada'}{' '}
+                  </b>{' '}
+                  caso o botão seja alterado.
                 </FormDescription>
               </div>
               <Switch
                 className="data-[state=checked]:bg-primary-600"
-                id="enabled-user"
+                id="enabled-permission"
                 checked={field.value}
                 onCheckedChange={field.onChange}
               />
