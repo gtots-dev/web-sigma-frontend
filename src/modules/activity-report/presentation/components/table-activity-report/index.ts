dd24a82cc6abf0315309131b21a1965ebe4ca0ec
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-09-11 16:29:48 +0000

    feat: Componente de tabela de logs

---
+++ src/modules/activity-report/presentation/components/table-activity-report/index.ts
@@ -0,0 +1,11 @@
+import { TableActivityReportBodyComponent } from './table-activity-report-body.component'
+import { TableActivityReportHeaderComponent } from './table-activity-report-header.component'
+import { TableActivityReportItemComponent } from './table-activity-report-item.component'
+import { TableActivityReportRootComponent } from './table-activity-report-root.component'
+
+export const TableActivityReport = {
+  Root: TableActivityReportRootComponent,
+  Body: TableActivityReportBodyComponent,
+  Header: TableActivityReportHeaderComponent,
+  Item: TableActivityReportItemComponent
+}
