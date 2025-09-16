070a77505fc690e8736031500a4cb0ec507468d8
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-09-16 13:30:57 +0000

    feat: Serviço de listagem de perfis de permissões com contratos

---
+++ src/modules/users/infrastructure/services/get-user-permission-profiles-contract.service.ts
@@ -0,0 +1,43 @@
+import type { ExecuteRequest } from '@/modules/shared/infrastructure/services/execute-request.service'
+import type { HttpRequestConfig } from '@/modules/shared/domain/interfaces/http-request-config.interface'
+import type { HttpResponse } from '@/modules/shared/domain/interfaces/http-response.interface'
+import type { TokenEntities } from '@/modules/authentication/domain/entities/token.entity'
+import type { UserPermissionProfileContractInterface } from '../../domain/interfaces/user-permission-profile-contract.interface'
+import type { GetUserPermissionProfileContractServiceInterface } from '../../domain/interfaces/get-user-permission-profiles-contract-service.interface'
+import type { UserEntity } from '../../domain/entities/user.entity'
+import type { PermissionProfileInterface } from '@/modules/permissions/domain/interfaces/permission-profiles.interface'
+
+export class GetUserPermissionProfileContractService
+  implements GetUserPermissionProfileContractServiceInterface
+{
+  constructor(private readonly httpRequest: ExecuteRequest) {}
+
+  getHttpRequestConfig(
+    token: TokenEntities,
+    userId: UserEntity['id'],
+    userPermissionProfileId: PermissionProfileInterface['id']
+  ): HttpRequestConfig {
+    return {
+      method: 'GET',
+      url: `/users/${userId}/perm-profiles/${userPermissionProfileId}/contracts`,
+      headers: token.access_token && {
+        Authorization: `${token.token_type} ${token.access_token}`
+      }
+    }
+  }
+
+  async execute(
+    token: TokenEntities,
+    userId: UserEntity['id'],
+    userPermissionProfileId: PermissionProfileInterface['id']
+  ): Promise<UserPermissionProfileContractInterface[]> {
+    const settingsAuthHTTP = this.getHttpRequestConfig(
+      token,
+      userId,
+      userPermissionProfileId
+    )
+    const { data }: HttpResponse<UserPermissionProfileContractInterface[]> =
+      await this.httpRequest.execute(settingsAuthHTTP)
+    return data
+  }
+}
