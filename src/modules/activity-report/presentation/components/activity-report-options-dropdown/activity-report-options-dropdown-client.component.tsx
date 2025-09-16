f1f544292979a3073ea78a2c125338276cd2f858
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-09-16 18:56:59 +0000

    feat: Injeção do componente de menu de ver mais

--- src/modules/activity-report/presentation/components/activity-report-options-dropdown/activity-report-options-dropdown-client.component.tsx
+++ src/modules/activity-report/presentation/components/activity-report-options-dropdown/activity-report-options-dropdown-client.component.tsx
@@ -1,23 +1,24 @@
 'use client'
 
-import type { ReactNode } from 'react'
 import { ActivityReportOptionsDropdown } from '.'
+import { ViewMoreActivityReportMenu } from '../view-more-user-menu'
+import { ViewMoreActivityReportMenuComponent } from '../view-more-user-menu/view-more-activity-report-menu.component'
 
-interface ActivityReportOptionsDropdownClientProps {
-  children: ReactNode
-}
+interface ActivityReportOptionsDropdownClientProps {}
 
-export function ActivityReportOptionsDropdownClient({
-  children
-}: ActivityReportOptionsDropdownClientProps) {
+export function ActivityReportOptionsDropdownClient({}: ActivityReportOptionsDropdownClientProps) {
   return (
-    <ActivityReportOptionsDropdown.Root>
-      <ActivityReportOptionsDropdown.Trigger />
-      <ActivityReportOptionsDropdown.Menu>
-        <ActivityReportOptionsDropdown.Item>
-          {children}
-        </ActivityReportOptionsDropdown.Item>
-      </ActivityReportOptionsDropdown.Menu>
-    </ActivityReportOptionsDropdown.Root>
+    <ViewMoreActivityReportMenu.Provider>
+      <ActivityReportOptionsDropdown.Root>
+        <ActivityReportOptionsDropdown.Trigger />
+        <ActivityReportOptionsDropdown.Menu>
+          <ActivityReportOptionsDropdown.Item>
+            <ViewMoreActivityReportMenu.Trigger />
+          </ActivityReportOptionsDropdown.Item>
+        </ActivityReportOptionsDropdown.Menu>
+      </ActivityReportOptionsDropdown.Root>
+
+      <ViewMoreActivityReportMenuComponent title="Relatório" />
+    </ViewMoreActivityReportMenu.Provider>
   )
 }
