ce3ab1e783ce9cecd2e169afdf60a9c69903adee
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-09-17 19:50:18 +0000

    feat: Hook de envio de dados do formulário de filtros de logs

---
+++ src/modules/activity-report/presentation/hooks/use-activity-submit.hook.ts
@@ -0,0 +1,48 @@
+'use client'
+
+import { useCallback } from 'react'
+import type { ActivityReportSchemaType } from './use-activity-schema.hook'
+import { useActivityReportStore } from '../stores/activity-report.store'
+
+function removeUndefined<T extends object>(obj: T): Partial<T> {
+  return Object.fromEntries(
+    Object.entries(obj).filter(([_, v]) => v !== undefined)
+  ) as Partial<T>
+}
+
+export function useActivityReportSubmit() {
+  const { getActivityReport } = useActivityReportStore()
+
+  const handleSubmit = useCallback(
+    async ({
+      actions,
+      contract_ids,
+      date_range,
+      operation_ids,
+      page,
+      per_page,
+      user_ids
+    }: ActivityReportSchemaType): Promise<void> => {
+      try {
+        await getActivityReport({
+          filters: removeUndefined({
+            actions,
+            contract_ids,
+            operation_ids,
+            user_ids,
+            date_range
+          }),
+          pagination: removeUndefined({
+            per_page,
+            page
+          })
+        })
+      } catch (err: unknown) {
+        console.log(err)
+      }
+    },
+    [getActivityReport]
+  )
+
+  return { handleSubmit }
+}
