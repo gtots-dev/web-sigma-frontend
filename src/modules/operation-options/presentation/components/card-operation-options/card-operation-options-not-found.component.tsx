e0ee8fead5b00a6bcda6c422bebc6a0a3873e8b3
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-09-17 17:10:04 +0000

    fix: Cor de fundo da mensagem de nenhuma opção disponível da operação

--- src/modules/operation-options/presentation/components/card-operation-options/card-operation-options-not-found.component.tsx
+++ src/modules/operation-options/presentation/components/card-operation-options/card-operation-options-not-found.component.tsx
@@ -6,7 +6,7 @@ export function CardOperationOptionsNotFoundComponent({
   message
 }: CardOperationOptionsNotFoundComponentProps) {
   return (
-    <div className="flex justify-center items-center w-full px-4 py-10 bg-zinc-950/50 rounded-lg border">
+    <div className="flex justify-center items-center w-full px-4 py-10 bg-zinc-100 dark:bg-zinc-950/50 rounded-lg border">
       <p className="text-sm text-muted-foreground">{message}</p>
     </div>
   )
