bed9636dd3b35340ce1bee2ff9a4d3d77e720a20
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-09-23 13:23:27 +0000

    feat: Componente de menu supenos de filtros

---
+++ src/modules/activity-report/presentation/components/activity-report-filters-dropdown/activity-report-filters-dropdown-client.component.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { ActivityReportFiltersDropdown } from '.'
+import { ActivityReportForm } from '../activity-report-form'
+
+export function ActivityReportFiltersDropdownClient() {
+  return (
+    <ActivityReportFiltersDropdown.Root>
+      <ActivityReportFiltersDropdown.Trigger />
+
+      <ActivityReportFiltersDropdown.Menu className="flex flex-col w-[400px] h-[450px]">
+        <div className="p-4 flex flex-col flex-1 gap-y-5 overflow-auto">
+          <ActivityReportForm.Inputs.Date />
+          <ActivityReportForm.Inputs.Time />
+          <ActivityReportForm.Inputs.Contracts />
+          <ActivityReportForm.Inputs.Users />
+          <ActivityReportForm.Inputs.Operations />
+
+          <ActivityReportForm.Submit className="mt-auto w-full">
+            Filtrar
+          </ActivityReportForm.Submit>
+        </div>
+      </ActivityReportFiltersDropdown.Menu>
+    </ActivityReportFiltersDropdown.Root>
+  )
+}
