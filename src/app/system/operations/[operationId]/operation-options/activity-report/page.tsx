4579c8ff0f717098ed8ff8d16f6ad3c59877f11e
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-09-23 13:37:59 +0000

    feat: Pagina de relatório de atividades

--- src/app/system/operations/[operationId]/operation-options/activity-report/page.tsx
+++ src/app/system/operations/[operationId]/operation-options/activity-report/page.tsx
@@ -1,7 +1,88 @@
+import { subMonths } from 'date-fns'
+import { ActivityReportForm } from '@/modules/activity-report/presentation/components/activity-report-form'
+import { TableActivityReport } from '@/modules/activity-report/presentation/components/table-activity-report'
+import { ActivityReportOptionsDropdown } from '@/modules/activity-report/presentation/components/activity-report-options-dropdown'
+import { ActivityReportFiltersDropdown } from '@/modules/activity-report/presentation/components/activity-report-filters-dropdown'
+import { Separator } from '@/modules/shared/presentation/components/shadcn/separator'
+import { SystemPagination } from '@/modules/shared/presentation/components/system-pagination'
+import { MESSAGES_ACTIVITY_REPORT } from '@/modules/shared/presentation/messages/activity-report'
+
+import { ActionSection } from '@/modules/system/presentation/components/actions-section'
+import { HeaderSection } from '@/modules/system/presentation/components/header-section'
+import {
+  formatDateOnly,
+  formatTimeOnly
+} from '@/modules/shared/presentation/utils/formatted.utils'
+
 interface ActivityReportPageProps {
   params: Promise<{ operationId: string }>
 }
 
-export default async function ActivityReportPage({}: ActivityReportPageProps) {
-  return <></>
+export default async function ActivityReportPage({
+  params
+}: ActivityReportPageProps) {
+  const { operationId: rawOperationId } = await params
+
+  const data = {
+    title: MESSAGES_ACTIVITY_REPORT['15.1'],
+    description: MESSAGES_ACTIVITY_REPORT['15.2']
+  }
+
+  const today = new Date()
+  const oneMonthAgo = subMonths(today, 1)
+
+  const initialSettingsLogs = {
+    filters: {
+      operation_ids: [Number(rawOperationId)],
+      date_range: {
+        start: formatDateOnly(oneMonthAgo),
+        end: formatDateOnly(today)
+      },
+      time_range: {
+        start: formatTimeOnly(0, 0),
+        end: formatTimeOnly(23, 59)
+      }
+    },
+    pagination: {
+      per_page: 50,
+      page: 1
+    }
+  }
+
+  return (
+    <ActivityReportForm.Form initSettings={initialSettingsLogs}>
+      <main className="flex flex-col flex-1 h-full w-full p-8 sm:p-10 sm:pb-0 gap-5">
+        <HeaderSection.Root>
+          <HeaderSection.Title>{data.title}</HeaderSection.Title>
+          <HeaderSection.Description>
+            {data.description}
+          </HeaderSection.Description>
+        </HeaderSection.Root>
+
+        <Separator orientation="horizontal" />
+
+        <ActionSection.Root>
+          <ActivityReportForm.Inputs.Search />
+          <ActivityReportFiltersDropdown.Client />
+        </ActionSection.Root>
+
+        <div className="flex flex-col h-full w-full">
+          <TableActivityReport.Root>
+            <TableActivityReport.Header />
+            <TableActivityReport.Body initSettings={initialSettingsLogs}>
+              <TableActivityReport.Item>
+                <ActivityReportOptionsDropdown.Client />
+              </TableActivityReport.Item>
+            </TableActivityReport.Body>
+          </TableActivityReport.Root>
+
+          <Separator orientation="horizontal" />
+
+          <SystemPagination.Root>
+            <SystemPagination.Controls />
+          </SystemPagination.Root>
+        </div>
+      </main>
+    </ActivityReportForm.Form>
+  )
 }
