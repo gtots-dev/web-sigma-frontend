6ea510701bdbf0711f18a52167389010680de062
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-06 19:51:14 +0000

    fix: Removido interface não utilizada no gatilho de abri menu de adicionar contrato

--- src/modules/contracts/presentation/components/add-contract-menu/add-contract-menu-trigger.component.tsx
+++ src/modules/contracts/presentation/components/add-contract-menu/add-contract-menu-trigger.component.tsx
@@ -4,9 +4,7 @@ import { Button } from '@/modules/shared/presentation/components/shadcn/button'
 import { FilePlus2 } from 'lucide-react'
 import { useDialog } from './add-contract-menu-provider.component'
 
-interface AddContractMenuTriggerComponentProps {}
-
-export function AddContractMenuTriggerComponent({}: AddContractMenuTriggerComponentProps) {
+export function AddContractMenuTriggerComponent() {
   const { open } = useDialog()
   return (
     <Button variant="primary" className="w-full sm:w-auto" onClick={open}>
