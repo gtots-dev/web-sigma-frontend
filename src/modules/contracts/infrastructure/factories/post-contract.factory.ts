ba147f64e3076e847c0fdab7ba5c89d0ad4d2166
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-06 20:27:24 +0000

    refactor: Removido a utilização de "decoder"

--- src/modules/contracts/infrastructure/factories/post-contract.factory.ts
+++ src/modules/contracts/infrastructure/factories/post-contract.factory.ts
@@ -3,14 +3,12 @@ import { ExecuteRequestFactory } from '@/modules/shared/infrastructure/factories
 import { PostContractService } from '../services/post-contract.service'
 import type { PostContractServiceInterface } from '../../domain/interfaces/post-contract-service.interface'
 import { FormDataConverterFactory } from '@/modules/shared/infrastructure/factories/form-data-converter.factory'
-import { JwtTokenDecodeFactory } from '@/modules/shared/infrastructure/factories/jwt-decode.factory'
 
 export class PostContractFactory {
   static create(): PostContractServiceInterface {
     const httpClient = HttpClientFactory.create(process.env.HOST_API)
     const executeRequest = ExecuteRequestFactory.create(httpClient)
     const formDataConvert = FormDataConverterFactory.create()
-    const jwtDecoder = JwtTokenDecodeFactory.create()
-    return new PostContractService(executeRequest, jwtDecoder, formDataConvert)
+    return new PostContractService(executeRequest, formDataConvert)
   }
 }
--- src/modules/contracts/infrastructure/services/post-contract.service.ts
+++ src/modules/contracts/infrastructure/services/post-contract.service.ts
@@ -5,14 +5,11 @@ import type { TokenEntities } from '@/modules/authentication/domain/entities/tok
 import { HttpResponseContractsValidator } from '../../domain/validators/http-response-contracts.validator'
 import type { PostContractServiceInterface } from '../../domain/interfaces/post-contract-service.interface'
 import type { ConvertJsonToFormData } from '@/modules/shared/infrastructure/services/convert-json-to-form-data.service'
-import type { ContractWithOperationId } from '../../domain/interfaces/contract-with-operation-id.interface'
 import type { ContractEntity } from '../../domain/entities/contract.entity'
-import type { JwtTokenDecode } from '@/modules/shared/infrastructure/jwt/jwt-decode'
 
 export class PostContractService implements PostContractServiceInterface {
   constructor(
     private readonly executeRequest: ExecuteRequest,
-    private readonly jwtDecoder: JwtTokenDecode,
     private readonly convertFormData: ConvertJsonToFormData
   ) {}
 
@@ -22,7 +19,7 @@ export class PostContractService implements PostContractServiceInterface {
   ): HttpRequestConfig<FormData> {
     return {
       method: 'POST',
-      url: `/contracts/`,
+      url: `/contracts`,
       data: contractFormData,
       headers: token.access_token && {
         Authorization: `${token.token_type} ${token.access_token}`
@@ -31,13 +28,9 @@ export class PostContractService implements PostContractServiceInterface {
   }
 
   async execute(token: TokenEntities, contract: ContractEntity): Promise<void> {
-    const operationId = String(this.jwtDecoder.decode(token.access_token).id)
-    const enrichedContract: ContractWithOperationId = {
-      ...contract,
-      operation_id: operationId
-    }
-    const contractFormDataConverted =
-      this.convertFormData.execute(enrichedContract)
+    const contractFormDataConverted = this.convertFormData.execute({
+      ...contract
+    })
     const settingsAuthHTTP = this.getHttpRequestConfig(
       token,
       contractFormDataConverted
