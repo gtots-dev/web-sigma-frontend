b664f6771bcf3e5496890953e319a612bb146636
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-06 21:02:42 +0000

    fix: Removido interface de contrato que extende de outro interface com operation_id

--- src/modules/contracts/domain/interfaces/contract-with-operation-id.interface.ts
+++
@@ -1,8 +0,0 @@
-import type { OperationIdInterface } from '@/modules/operations/domain/interfaces/operation-id.interface'
-import type { ContractInterface } from './contract.interface'
-
-export interface ContractWithOperationId
-  extends ContractInterface,
-    OperationIdInterface {
-  [key: string]: unknown
-}
