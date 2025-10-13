23157c99d35092d8d00691aa1b1da4d782348c1a
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-13 17:02:23 +0000

    feat: Função de atualizar status

--- src/modules/contracts/presentation/stores/contract.store.ts
+++ src/modules/contracts/presentation/stores/contract.store.ts
@@ -4,6 +4,7 @@ import type { ContractEntity } from '../../domain/entities/contract.entity'
 import { HttpResponseError } from '@/modules/shared/infrastructure/errors/http-response.error'
 import { PostContractRouterApiFactory } from '@/modules/api/infrastructure/factories/post-contract-router-api.factory'
 import { PutContractRouterApiFactory } from '@/modules/api/infrastructure/factories/put-contract-router-api.factory'
+import { PutContractStatusRouterApiFactory } from '@/modules/api/infrastructure/factories/put-contract-status-router-api.factory'
 
 type ContractState = {
   contracts: ContractEntity[]
@@ -11,6 +12,7 @@ type ContractState = {
   getContracts: () => Promise<void>
   addContract: (contract: ContractEntity) => Promise<void>
   updateContract: (contract: ContractEntity) => Promise<void>
+  updateStatus: (contract: ContractEntity) => Promise<void>
   setContract: (contract: ContractEntity) => Promise<void>
 }
 
@@ -51,6 +53,18 @@ export const useContractStore = create<ContractState>((set) => ({
     try {
       const putContractsRouterApiFactory = PutContractRouterApiFactory.create()
       await putContractsRouterApiFactory.execute(contract)
+    } catch (error) {
+      if (error instanceof HttpResponseError) {
+        throw error
+      }
+    }
+  },
+
+  updateStatus: async (contract: ContractEntity) => {
+    try {
+      const putContractStatusRouterApiFactory =
+        PutContractStatusRouterApiFactory.create()
+      await putContractStatusRouterApiFactory.execute(contract)
     } catch (error) {
       if (error instanceof HttpResponseError) {
         throw error
