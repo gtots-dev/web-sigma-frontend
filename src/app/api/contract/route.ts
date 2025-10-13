7463abe1b7cdca99e4abbb9a6578f54316967405
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-13 14:42:09 +0000

    feat: Rota de atualização de contrato (Router Api)

--- src/app/api/contract/route.ts
+++ src/app/api/contract/route.ts
@@ -4,6 +4,7 @@ import { HttpStatusCodeEnum } from '@/modules/authentication/domain/enums/status
 import { GetContractsFactory } from '@/modules/contracts/infrastructure/factories/get-contracts.factory'
 import { HttpResponseError } from '@/modules/shared/infrastructure/errors/http-response.error'
 import { PostContractFactory } from '@/modules/contracts/infrastructure/factories/post-contract.factory'
+import { PutContractFactory } from '@/modules/contracts/infrastructure/factories/put-contract.factory'
 
 export async function GET(): Promise<NextResponse> {
   try {
@@ -33,6 +34,42 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
     const contract = await req.json()
     const postContract = PostContractFactory.create()
     await postContract.execute(token, contract)
+    return NextResponse.json(
+      {
+        success: true
+      },
+      { status: Number(HttpStatusCodeEnum.OK) }
+    )
+  } catch (error) {
+    if (error instanceof HttpResponseError) {
+      return NextResponse.json(
+        {
+          success: false,
+          data: null,
+          message: error.message
+        },
+        { status: Number(HttpStatusCodeEnum.BAD_REQUEST) }
+      )
+    }
+    return NextResponse.json(
+      {
+        success: false,
+        data: null,
+        message: 'Erro inesperado'
+      },
+      { status: Number(HttpStatusCodeEnum.INTERNAL_SERVER_ERROR) }
+    )
+  }
+}
+
+export async function PUT(req: NextRequest): Promise<NextResponse> {
+  try {
+    const { token } = await auth()
+    const contract = await req.json()
+    console.log('aqui')
+
+    const putContract = PutContractFactory.create()
+    await putContract.execute(token, contract)
     return NextResponse.json(
       {
         success: true
