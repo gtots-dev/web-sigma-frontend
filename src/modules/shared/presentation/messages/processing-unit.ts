a65ad399712e605ef32d1405b28c76dc7486ed3d
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-14 18:04:11 +0000

    feat: Mensagens da seção de unidades de processamento

---
+++ src/modules/shared/presentation/messages/processing-unit.ts
@@ -0,0 +1,11 @@
+type MessageKeys = '7.1' | '7.2' | '7.3' | '7.4' | '7.5' | '7.6'
+
+export const MESSAGES_PROCESSING_UNIT: Record<MessageKeys, string> = {
+  '7.1': 'Unidade de Processamento',
+  '7.2': 'Navegue pelas unidades de processamento disponíveis no sistema.',
+  '7.3': 'Nenhuma unidade de processamento encontrada',
+  '7.4': 'Nova unidade de contrato',
+  '7.5':
+    'Preencha os dados obrigatórios para adicionar uma nova unidade de processamento.',
+  '7.6': 'O nome da unidade de processamento é obrigatório.'
+}
