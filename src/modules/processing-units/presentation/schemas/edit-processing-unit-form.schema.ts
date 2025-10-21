ae062fcdcba885158758db60c7d93831b1634686
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-21 04:10:17 +0000

    feat: Esquema de edição de unidade de processamento

---
+++ src/modules/processing-units/presentation/schemas/edit-processing-unit-form.schema.ts
@@ -0,0 +1,17 @@
+import { MESSAGES_PROCESSING_UNIT } from '@/modules/shared/presentation/messages/processing-unit'
+import { z } from 'zod'
+
+export const EditProcessingUnitFormSchema = z.object({
+  id: z.number(),
+  name: z.string().nonempty({
+    message: MESSAGES_PROCESSING_UNIT['7.6']
+  }),
+  cfg: z.string().nullable().optional(),
+  processingUnit_id: z.number(),
+  operation_id: z.number(),
+  enabled: z.boolean()
+})
+
+export type EditProcessingUnitFormType = z.infer<
+  typeof EditProcessingUnitFormSchema
+>
