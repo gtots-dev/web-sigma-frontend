99b38b94818e3274b4618cd10a7287da585b351d
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-21 14:55:16 +0000

    feat: Componente de menu suspenso para opções da faixa

---
+++ src/modules/lanes/presentation/components/lane-options-dropdown/index.tsx
@@ -0,0 +1,11 @@
+import { LaneOptionsDropdownItemComponent } from './lane-options-dropdown-item.component'
+import { LaneOptionsDropdownMenuComponent } from './lane-options-dropdown-menu.component'
+import { LaneOptionsDropdownRootComponent } from './lane-options-dropdown-root.component'
+import { LaneOptionsDropdownTriggerComponent } from './lane-options-dropdown-trigger.component'
+
+export const LaneOptionsDropdown = {
+  Root: LaneOptionsDropdownRootComponent,
+  Menu: LaneOptionsDropdownMenuComponent,
+  Item: LaneOptionsDropdownItemComponent,
+  Trigger: LaneOptionsDropdownTriggerComponent
+}
