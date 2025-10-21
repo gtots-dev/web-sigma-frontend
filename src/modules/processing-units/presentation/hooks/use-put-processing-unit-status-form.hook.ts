21c028b175252e3f5f2d4f0d6a68e32381b0143b
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-21 04:11:10 +0000

    feat: Hooks para o menu de atualização de status de unidade de processamento

---
+++ src/modules/processing-units/presentation/hooks/use-put-processing-unit-status-form.hook.ts
@@ -0,0 +1,27 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { useMemo } from 'react'
+import { PutProcessingUnitStatusFormSchema, type PutProcessingUnitStatusFormType } from '../schemas/put-processing-unit-status-form.schema'
+import type { ProcessingUnitEntity } from '../../domain/entities/processing-unit.entity'
+
+export function usePutProcessingUnitStatusForm(processingUnit: ProcessingUnitEntity) {
+  const defaultValues = useMemo<Partial<ProcessingUnitEntity>>(
+    () => ({
+      id: processingUnit.id,
+      enabled: processingUnit.enabled
+    }),
+    [processingUnit]
+  )
+
+  const methods = useForm<PutProcessingUnitStatusFormType>({
+    resolver: zodResolver(PutProcessingUnitStatusFormSchema),
+    defaultValues
+  })
+
+  return {
+    defaultValues,
+    methods
+  }
+}
