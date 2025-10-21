2104cfd6a914f8b260c8326905fa7811c03f6e69
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-21 14:57:00 +0000

    feat: Componente de tabela de faixas

---
+++ src/modules/lanes/presentation/components/table-lanes/index.ts
@@ -0,0 +1,11 @@
+import { TableLanesBodyComponent } from './table-lanes-body.component'
+import { TableLanesHeaderComponent } from './table-lanes-header.component'
+import { TableLanesItemComponent } from './table-lanes-item.component'
+import { TableLanesRootComponent } from './table-lanes-root.component'
+
+export const TableLanes = {
+  Root: TableLanesRootComponent,
+  Body: TableLanesBodyComponent,
+  Header: TableLanesHeaderComponent,
+  Item: TableLanesItemComponent
+}
