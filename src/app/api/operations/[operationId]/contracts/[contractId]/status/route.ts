e493b0c6d95bbb548624027485b0e8bdbf09a279
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-28 18:43:52 +0000

    refactor: Injeção de parâmetros dinâmico para as rotas de contrato/status

--- src/app/api/operations/[operationId]/contracts/[contractId]/status/route.ts
+++ src/app/api/operations/[operationId]/contracts/[contractId]/status/route.ts
@@ -1,39 +1,16 @@
-import { NextResponse, type NextRequest } from 'next/server'
-import { auth } from '@/auth'
 import { HttpStatusCodeEnum } from '@/modules/authentication/domain/enums/status-codes.enum'
-import { HttpResponseError } from '@/modules/shared/infrastructure/errors/http-response.error'
 import { PutContractStatusFactory } from '@/modules/contracts/infrastructure/factories/put-contract-status.factory'
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
+import { RouterApiFactory } from '@/modules/api/infrastructure/factories/router-service-api.factory'
 
-export async function PUT(req: NextRequest): Promise<NextResponse> {
-  try {
-    const { token } = await auth()
-    const contract = await req.json()
-    const putContractStatus = PutContractStatusFactory.create()
-    await putContractStatus.execute(token, contract)
-    return NextResponse.json(
-      {
-        success: true
-      },
-      { status: Number(HttpStatusCodeEnum.OK) }
-    )
-  } catch (error) {
-    if (error instanceof HttpResponseError) {
-      return NextResponse.json(
-        {
-          success: false,
-          data: null,
-          message: error.message
-        },
-        { status: Number(HttpStatusCodeEnum.BAD_REQUEST) }
-      )
-    }
-    return NextResponse.json(
-      {
-        success: false,
-        data: null,
-        message: 'Erro inesperado'
-      },
-      { status: Number(HttpStatusCodeEnum.INTERNAL_SERVER_ERROR) }
-    )
+const routerApi = RouterApiFactory.create()
+
+export const PUT = routerApi.PUT<UrlParams>(async ({ operationId }, req) => {
+  const contract = await req?.json()
+  const putContract = PutContractStatusFactory.create({ operationId })
+  await putContract.execute(contract)
+  return {
+    data: { success: true },
+    status: HttpStatusCodeEnum.OK
   }
-}
+})
