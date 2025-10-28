54261af8036b746e54c4ca557057098df03795ff
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-28 18:42:00 +0000

    refactor: Adaptação das fabricas para construção do novos modelos de serviços

--- src/modules/api/infrastructure/factories/put-contract-status-router-api.factory.ts
+++ src/modules/api/infrastructure/factories/put-contract-status-router-api.factory.ts
@@ -2,11 +2,12 @@ import { HttpClientFactory } from '@/modules/shared/infrastructure/factories/htt
 import { ExecuteRequestFactory } from '@/modules/shared/infrastructure/factories/request.factory'
 import type { PutContractStatusRouterApiServiceInterface } from '../../domain/interfaces/put-contract-status-router-api-service.interface'
 import { PutContractStatusRouterApiService } from '../services/put-contract-status-router-api.service'
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
 
 export class PutContractStatusRouterApiFactory {
-  static create(): PutContractStatusRouterApiServiceInterface {
+  static create(params: UrlParams): PutContractStatusRouterApiServiceInterface {
     const httpClient = HttpClientFactory.create('/')
     const executeRequest = ExecuteRequestFactory.create(httpClient)
-    return new PutContractStatusRouterApiService(executeRequest)
+    return new PutContractStatusRouterApiService(executeRequest, params)
   }
 }
