d1695009e21039acfe4d4fda585ddcd5fcbd8194
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-28 14:50:57 +0000

    feat: Interface do serviço de gerador para router api do next

---
+++ src/modules/api/domain/interfaces/router-api-service.interface.ts
@@ -0,0 +1,39 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { HttpStatusCodeEnum } from '@/modules/authentication/domain/enums/status-codes.enum'
+
+export interface RouterApiResponse<R = unknown> {
+  data: R
+  status: HttpStatusCodeEnum
+}
+
+export interface ErrorResponse {
+  success: false
+  message: string
+}
+
+export type HandlerCallback<P = void, R = unknown> = (
+  params: P,
+  req?: NextRequest
+) => Promise<void | R | RouterApiResponse<R | ErrorResponse>>
+
+export interface RouterApiServiceInterface {
+  GET<P = void, R = unknown>(
+    callback: HandlerCallback<P, R>
+  ): (req: NextRequest, context?: { params: P }) => Promise<NextResponse>
+
+  POST<P = void, R = unknown>(
+    callback: HandlerCallback<P, R>
+  ): (req: NextRequest, context?: { params: P }) => Promise<NextResponse>
+
+  PUT<P = void, R = unknown>(
+    callback: HandlerCallback<P, R>
+  ): (req: NextRequest, context?: { params: P }) => Promise<NextResponse>
+
+  PATCH<P = void, R = unknown>(
+    callback: HandlerCallback<P, R>
+  ): (req: NextRequest, context?: { params: P }) => Promise<NextResponse>
+
+  DELETE<P = void, R = unknown>(
+    callback: HandlerCallback<P, R>
+  ): (req: NextRequest, context?: { params: P }) => Promise<NextResponse>
+}
