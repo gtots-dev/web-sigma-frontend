d1c049d27e8f562548fa70ce464c2476d13e299f
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-28 18:42:00 +0000

    refactor: Adaptação das fabricas para construção do novos modelos de serviços

--- src/modules/api/infrastructure/factories/patch-contract-router-api.factory.ts
+++ src/modules/api/infrastructure/factories/patch-contract-router-api.factory.ts
@@ -1,13 +1,13 @@
 import { HttpClientFactory } from '@/modules/shared/infrastructure/factories/http-client.factory'
 import { ExecuteRequestFactory } from '@/modules/shared/infrastructure/factories/request.factory'
-import { PostContractRouterApiService } from '../services/post-contract-router-api.service'
-import type { PostContractRouterApiServiceInterface } from '../../domain/interfaces/post-contract-router-api-service.interface'
+import { PatchContractRouterApiService } from '../services/patch-contract-router-api.service'
+import type { PatchContractRouterApiServiceInterface } from '../../domain/interfaces/put-contract-router-api-service.interface'
 import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
 
-export class PostContractRouterApiFactory {
-  static create(params: UrlParams): PostContractRouterApiServiceInterface {
+export class PatchContractRouterApiFactory {
+  static create(params: UrlParams): PatchContractRouterApiServiceInterface {
     const httpClient = HttpClientFactory.create('/')
     const executeRequest = ExecuteRequestFactory.create(httpClient)
-    return new PostContractRouterApiService(executeRequest, params)
+    return new PatchContractRouterApiService(executeRequest, params)
   }
 }
