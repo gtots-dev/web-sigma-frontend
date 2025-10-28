25e4f3402cecea4823ad0300dab5a26849556414
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-28 15:20:03 +0000

    refactor: Construção das rota com os novos parametros

--- src/app/api/operations/[operationId]/contracts/route.ts
+++ src/app/api/operations/[operationId]/contracts/route.ts
@@ -1,112 +1,39 @@
-import { NextResponse, type NextRequest } from 'next/server'
-import { auth } from '@/auth'
-import { HttpStatusCodeEnum } from '@/modules/authentication/domain/enums/status-codes.enum'
 import { GetContractsFactory } from '@/modules/contracts/infrastructure/factories/get-contracts.factory'
-import { HttpResponseError } from '@/modules/shared/infrastructure/errors/http-response.error'
 import { PostContractFactory } from '@/modules/contracts/infrastructure/factories/post-contract.factory'
-import { PutContractFactory } from '@/modules/contracts/infrastructure/factories/put-contract.factory'
+import { PatchContractFactory } from '@/modules/contracts/infrastructure/factories/patch-contract.factory'
+import { HttpStatusCodeEnum } from '@/modules/authentication/domain/enums/status-codes.enum'
+import { RouterApiFactory } from '@/modules/api/infrastructure/factories/router-service-api.factory'
+import type { ContractEntity } from '@/modules/contracts/domain/entities/contract.entity'
 import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: Promise<UrlParams> }
-): Promise<NextResponse> {
-  try {
-    const [authResult, { operationId }] = await Promise.all([auth(), params])
-    const { token } = authResult
+const routerApi = RouterApiFactory.create()
 
-    if (!operationId) {
-      return NextResponse.json(
-        { success: false, message: 'operationId é obrigatório' },
-        { status: Number(HttpStatusCodeEnum.BAD_REQUEST) }
-      )
-    }
-
-    const getContractsFactory = GetContractsFactory.create()
-    const response = await getContractsFactory.execute(token, { operationId })
-
-    return NextResponse.json(response, {
-      status: Number(HttpStatusCodeEnum.OK)
-    })
-  } catch (error) {
-    if (error instanceof HttpResponseError) {
-      return NextResponse.json(
-        { success: false, data: null, message: error.message },
-        { status: Number(HttpStatusCodeEnum.BAD_REQUEST) }
-      )
-    }
-
-    return NextResponse.json(
-      { success: false, data: null, message: 'Erro inesperado' },
-      { status: Number(HttpStatusCodeEnum.INTERNAL_SERVER_ERROR) }
-    )
+export const GET = routerApi.GET<UrlParams, ContractEntity[]>(
+  async ({ operationId }) => {
+    const getContracts = GetContractsFactory.create({ operationId })
+    const response = await getContracts.execute()
+    return { data: response, status: HttpStatusCodeEnum.OK }
   }
-}
+)
 
-export async function POST(req: NextRequest): Promise<NextResponse> {
-  try {
-    const { token } = await auth()
-    const contract = await req.json()
-    const postContract = PostContractFactory.create()
-    await postContract.execute(token, contract)
-    return NextResponse.json(
-      {
-        success: true
-      },
-      { status: Number(HttpStatusCodeEnum.OK) }
-    )
-  } catch (error) {
-    if (error instanceof HttpResponseError) {
-      return NextResponse.json(
-        {
-          success: false,
-          data: null,
-          message: error.message
-        },
-        { status: Number(HttpStatusCodeEnum.BAD_REQUEST) }
-      )
-    }
-    return NextResponse.json(
-      {
-        success: false,
-        data: null,
-        message: 'Erro inesperado'
-      },
-      { status: Number(HttpStatusCodeEnum.INTERNAL_SERVER_ERROR) }
-    )
+export const POST = routerApi.POST<UrlParams>(async ({ operationId }, req) => {
+  const contract = await req?.json()
+  const postContract = PostContractFactory.create({ operationId })
+  await postContract.execute(contract)
+  return {
+    data: { success: true },
+    status: HttpStatusCodeEnum.OK
   }
-}
+})
 
-export async function PUT(req: NextRequest): Promise<NextResponse> {
-  try {
-    const { token } = await auth()
-    const contract = await req.json()
-    const putContract = PutContractFactory.create()
-    await putContract.execute(token, contract)
-    return NextResponse.json(
-      {
-        success: true
-      },
-      { status: Number(HttpStatusCodeEnum.OK) }
-    )
-  } catch (error) {
-    if (error instanceof HttpResponseError) {
-      return NextResponse.json(
-        {
-          success: false,
-          data: null,
-          message: error.message
-        },
-        { status: Number(HttpStatusCodeEnum.BAD_REQUEST) }
-      )
+export const PATCH = routerApi.PATCH<UrlParams>(
+  async ({ operationId }, req) => {
+    const contract = await req?.json()
+    const patchContract = PatchContractFactory.create({ operationId })
+    await patchContract.execute(contract)
+    return {
+      data: { success: true },
+      status: HttpStatusCodeEnum.OK
     }
-    return NextResponse.json(
-      {
-        success: false,
-        data: null,
-        message: 'Erro inesperado'
-      },
-      { status: Number(HttpStatusCodeEnum.INTERNAL_SERVER_ERROR) }
-    )
   }
-}
+)
