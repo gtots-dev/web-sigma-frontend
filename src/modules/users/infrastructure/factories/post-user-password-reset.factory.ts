caf2ce64dd590357af404ae32004db4da75ed1e3
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-30 19:01:24 +0000

    refactor: Adaptação das fabricas para construção do novos modelos de serviços (Backend)

--- src/modules/users/infrastructure/factories/post-user-password-reset.factory.ts
+++ src/modules/users/infrastructure/factories/post-user-password-reset.factory.ts
@@ -3,12 +3,19 @@ import { ExecuteRequestFactory } from '@/modules/shared/infrastructure/factories
 import type { PostUserPasswordResetServiceInterface } from '../../domain/interfaces/post-user-password-reset-service.interface'
 import { PostUserPasswordResetService } from '../services/post-user-password-reset.service'
 import { FormDataConverterFactory } from '@/modules/shared/infrastructure/factories/form-data-converter.factory'
-
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
+import { AuthTokenFactory } from '@/modules/api/infrastructure/factories/auth-token.factory'
 export class PostUserPasswordResetFactory {
-  static create(): PostUserPasswordResetServiceInterface {
+  static create(params: UrlParams): PostUserPasswordResetServiceInterface {
     const httpClient = HttpClientFactory.create(process.env.HOST_API)
     const formDataConvert = FormDataConverterFactory.create()
     const executeRequest = ExecuteRequestFactory.create(httpClient)
-    return new PostUserPasswordResetService(executeRequest, formDataConvert)
+    const authToken = AuthTokenFactory.create()
+    return new PostUserPasswordResetService(
+      executeRequest,
+      formDataConvert,
+      authToken,
+      params
+    )
   }
 }
