8a7bbbcd7b6d23760ce333b066b7322397eeaef8
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-30 18:55:52 +0000

    feat: Interfaces de complemento de perfis de permissão

---
+++ src/modules/users/domain/interfaces/user-permission-profile-with-features-and-contracts.interface.ts
@@ -0,0 +1,5 @@
+import type { UserPermissionProfilesWithContracts } from './user-permission-profile-id-with-contracts.interface'
+
+export interface UserPermissionProfileWithFeaturesAndContractsInterface {
+  profiles: UserPermissionProfilesWithContracts[]
+}
