1c5a14f8bd33c27c6a3e1da6a57fd4125c895051
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-30 14:07:34 +0000

    refactor: Adaptação para novo modelo de serviço com parâmetros dinâmicos

--- src/modules/api/domain/interfaces/get-user-files-router-api-service.interface.ts
+++ src/modules/api/domain/interfaces/get-user-files-router-api-service.interface.ts
@@ -1,6 +1,5 @@
-import type { UserEntity } from '@/modules/users/domain/entities/user.entity'
 import type { UserFileInterface } from '@/modules/users/domain/interfaces/user-file.interface'
 
 export interface GetUserFilesRouterApiServiceInterface {
-  execute(userId: UserEntity['id']): Promise<UserFileInterface[]>
+  execute(): Promise<UserFileInterface[]>
 }
--- src/modules/api/infrastructure/factories/get-user-file-router-api.factory.ts
+++ src/modules/api/infrastructure/factories/get-user-file-router-api.factory.ts
@@ -2,11 +2,12 @@ import { HttpClientFactory } from '@/modules/shared/infrastructure/factories/htt
 import { ExecuteRequestFactory } from '@/modules/shared/infrastructure/factories/request.factory'
 import { GetUserFileRouterApiService } from '../services/get-user-file-router-api.service'
 import type { GetUserFileRouterApiServiceInterface } from '../../domain/interfaces/get-user-file-router-api-service.interface'
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
 
 export class GetUserFileRouterApiFactory {
-  static create(): GetUserFileRouterApiServiceInterface {
+  static create(params: UrlParams): GetUserFileRouterApiServiceInterface {
     const httpClient = HttpClientFactory.create('/')
     const executeRequest = ExecuteRequestFactory.create(httpClient)
-    return new GetUserFileRouterApiService(executeRequest)
+    return new GetUserFileRouterApiService(executeRequest, params)
   }
 }
