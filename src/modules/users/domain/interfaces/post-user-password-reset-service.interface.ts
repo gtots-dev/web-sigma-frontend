8150b6119a7830f1461b198419a6630371c7da60
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-30 18:53:39 +0000

    refactor: Removido parâmetro de token e id para interface de serviço de senha

--- src/modules/users/domain/interfaces/post-user-password-reset-service.interface.ts
+++ src/modules/users/domain/interfaces/post-user-password-reset-service.interface.ts
@@ -1,9 +1,5 @@
-import type { TokenEntities } from '@/modules/authentication/domain/entities/token.entity'
 import type { UserPasswordResetInterface } from './user-password-reset.interface'
 
 export interface PostUserPasswordResetServiceInterface {
-  execute(
-    token: TokenEntities,
-    userPasswordReset: UserPasswordResetInterface
-  ): Promise<void>
+  execute(userPasswordReset: UserPasswordResetInterface): Promise<void>
 }
