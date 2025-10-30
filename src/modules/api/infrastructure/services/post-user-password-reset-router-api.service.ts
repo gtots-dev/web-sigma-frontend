ff7ee278f60fd02ac1d99c73ff0431b6dc6864e6
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-30 18:57:07 +0000

    refactor: Adaptação para novo modelo de serviço (Router Api)

--- src/modules/api/infrastructure/services/post-user-password-reset-router-api.service.ts
+++ src/modules/api/infrastructure/services/post-user-password-reset-router-api.service.ts
@@ -3,24 +3,32 @@ import type { HttpRequestConfig } from '@/modules/shared/domain/interfaces/http-
 import type { PostUserPasswordResetRouterApiServiceInterface } from '../../domain/interfaces/post-user-password-reset-router-api-service.interface'
 import type { UserPasswordResetInterface } from '@/modules/users/domain/interfaces/user-password-reset.interface'
 import { HttpResponseUserPasswordResetValidator } from '@/modules/users/domain/validators/http-response-user-password-reset.validator'
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
 
 export class PostUserPasswordResetRouterApiService
   implements PostUserPasswordResetRouterApiServiceInterface
 {
-  constructor(private readonly executeRequest: ExecuteRequest) {}
+  constructor(
+    private readonly executeRequest: ExecuteRequest,
+    private readonly params: UrlParams
+  ) {}
 
   getHttpRequestConfig(
+    { operationId }: UrlParams,
     userPasswordReset: UserPasswordResetInterface
   ): HttpRequestConfig<UserPasswordResetInterface> {
     return {
       method: 'POST',
       data: userPasswordReset,
-      url: 'api/user/password-reset'
+      url: `api/operations/${operationId}/users/${userPasswordReset.userId}/passwords`
     }
   }
 
   async execute(userPasswordReset: UserPasswordResetInterface): Promise<void> {
-    const settingsAuthHTTP = this.getHttpRequestConfig(userPasswordReset)
+    const settingsAuthHTTP = this.getHttpRequestConfig(
+      this.params,
+      userPasswordReset
+    )
     const { success, status } =
       await this.executeRequest.execute<null>(settingsAuthHTTP)
     HttpResponseUserPasswordResetValidator.validate(success, status)
