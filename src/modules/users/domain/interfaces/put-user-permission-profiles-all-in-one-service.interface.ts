9e8450a8de5fce710f4c23a6b115ffc534d7e82a
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-30 18:56:10 +0000

    refactor: Interface de própria para profiles

--- src/modules/users/domain/interfaces/put-user-permission-profiles-all-in-one-service.interface.ts
+++ src/modules/users/domain/interfaces/put-user-permission-profiles-all-in-one-service.interface.ts
@@ -1,17 +1,7 @@
-import type { TokenEntities } from '@/modules/authentication/domain/entities/token.entity'
-import type { UserEntity } from '../entities/user.entity'
-import type { PermissionProfileEntity } from '@/modules/permissions/domain/entities/permission-profile.entity'
-import type { ContractEntity } from '@/modules/contracts/domain/entities/contract.entity'
+import type { UserPermissionProfileWithFeaturesAndContractsInterface } from './user-permission-profile-with-features-and-contracts.interface'
 
 export interface PutUserPermissionProfileAllInOneServiceInterface {
   execute(
-    token: TokenEntities,
-    userId: UserEntity['id'],
-    profiles: {
-      profiles: {
-        perm_profile_id: PermissionProfileEntity['id']
-        contract_ids: ContractEntity['id'][]
-      }[]
-    }
+    profiles: UserPermissionProfileWithFeaturesAndContractsInterface
   ): Promise<void>
 }
