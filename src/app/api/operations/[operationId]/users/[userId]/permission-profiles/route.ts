ad9df782feb0c265293bfaf0b00cccfe92d3e0ee
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-30 19:02:41 +0000

    refactor: Diretório de pasta com parâmetro operationId obrigatório

--- src/app/api/operations/[operationId]/users/[userId]/permission-profiles/route.ts
+++ src/app/api/operations/[operationId]/users/[userId]/permission-profiles/route.ts
@@ -1,10 +1,15 @@
 import { auth } from '@/auth'
+import { RouterApiFactory } from '@/modules/api/infrastructure/factories/router-service-api.factory'
 import { HttpStatusCodeEnum } from '@/modules/authentication/domain/enums/status-codes.enum'
+import type { PermissionProfileWithUserInterface } from '@/modules/permissions/domain/interfaces/permission-profile-with-user.interface'
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
 import { HttpResponseError } from '@/modules/shared/infrastructure/errors/http-response.error'
 import type { UserEntity } from '@/modules/users/domain/entities/user.entity'
 import { GetUserWithPermissionProfileFactory } from '@/modules/users/infrastructure/factories/get-user-with-permission-profile.factory'
 import { PostBindUserWithPermissionProfileFactory } from '@/modules/users/infrastructure/factories/post-bind-user-with-permission-profile.factory'
 import { NextResponse, type NextRequest } from 'next/server'
+
+const routerApi = RouterApiFactory.create()
 
 export async function POST(
   req: NextRequest,
@@ -37,31 +42,15 @@ export async function POST(
   }
 }
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: Promise<{ userId: UserEntity['id'] }> }
-): Promise<NextResponse> {
-  try {
-    const { userId } = await params
-    const { token } = await auth()
-    const getUserWithPermissionProfileFactory =
-      GetUserWithPermissionProfileFactory.create()
-    const response = await getUserWithPermissionProfileFactory.execute(
-      token,
+export const GET = routerApi.GET<
+  UrlParams,
+  PermissionProfileWithUserInterface[]
+>(async ({ operationId, userId }) => {
+  const getUserWithPermissionProfile =
+    GetUserWithPermissionProfileFactory.create({
+      operationId,
       userId
-    )
-    return NextResponse.json(response, {
-      status: Number(HttpStatusCodeEnum.OK)
     })
-  } catch (error) {
-    if (error instanceof HttpResponseError) {
-      return NextResponse.json(
-        {
-          success: false,
-          message: error.message
-        },
-        { status: Number(HttpStatusCodeEnum.BAD_REQUEST) }
-      )
-    }
-  }
-}
+  const response = await getUserWithPermissionProfile.execute()
+  return { data: response, status: HttpStatusCodeEnum.OK }
+})
