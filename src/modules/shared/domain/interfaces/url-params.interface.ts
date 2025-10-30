24a178c4593088fdc16e483b64450bddb702da4b
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-30 14:08:29 +0000

    feat: Parâmetros de url (userId, fileId)

--- src/modules/shared/domain/interfaces/url-params.interface.ts
+++ src/modules/shared/domain/interfaces/url-params.interface.ts
@@ -1,9 +1,13 @@
 import type { ContractEntity } from '@/modules/contracts/domain/entities/contract.entity'
 import type { OperationEntity } from '@/modules/operations/domain/entities/operation.entity'
 import type { ProcessingUnitEntity } from '@/modules/processing-units/domain/entities/processing-unit.entity'
+import type { UserEntity } from '@/modules/users/domain/entities/user.entity'
+import type { UserFileInterface } from '@/modules/users/domain/interfaces/user-file.interface'
 
 export interface UrlParams {
   operationId?: OperationEntity['id']
   contractId?: ContractEntity['id']
   processingUnitId?: ProcessingUnitEntity['id']
+  userId?: UserEntity['id']
+  fileId?: UserFileInterface['id']
 }
