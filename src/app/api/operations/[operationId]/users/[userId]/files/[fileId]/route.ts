8e9c8718ae69c7011c46ad19b3faf3a326b37f6b
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-10-30 14:22:46 +0000

    refactor: Adaptação para novo modelo de solicitação de requisição, parâmetros dinâmicos de URL

--- src/app/api/operations/[operationId]/users/[userId]/files/[fileId]/route.ts
+++ src/app/api/operations/[operationId]/users/[userId]/files/[fileId]/route.ts
@@ -1,40 +1,24 @@
-import { auth } from '@/auth'
 import { HttpStatusCodeEnum } from '@/modules/authentication/domain/enums/status-codes.enum'
-import { HttpResponseError } from '@/modules/shared/infrastructure/errors/http-response.error'
 import { GetUserFileFactory } from '@/modules/users/infrastructure/factories/get-user-file.factory'
-import { NextResponse, type NextRequest } from 'next/server'
-import type { GetStaticPropsContext } from 'next'
+import { RouterApiFactory } from '@/modules/api/infrastructure/factories/router-service-api.factory'
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
+const routerApi = RouterApiFactory.create()
 
-export async function GET(
-  req: NextRequest,
-  context: GetStaticPropsContext<{ userId: string; fileId: string }>
-): Promise<Response> {
-  try {
-    const { fileId, userId } = context.params
-    const { token } = await auth()
-    const getUserFileFactory = GetUserFileFactory.create()
-    const fileBlob = await getUserFileFactory.execute(
-      token,
-      Number(userId),
-      Number(fileId)
-    )
-
-    return new Response(fileBlob, {
-      status: Number(HttpStatusCodeEnum.OK),
+export const GET = routerApi.GET<UrlParams, File>(
+  async ({ operationId, userId, fileId }) => {
+    const getUserFileFactory = GetUserFileFactory.create({
+      operationId,
+      userId,
+      fileId
+    })
+    const fileBlob = await getUserFileFactory.execute()
+    return {
+      data: fileBlob,
+      status: HttpStatusCodeEnum.OK,
       headers: {
         'Content-Type': fileBlob.type || 'application/octet-stream',
         'Content-Disposition': `attachment; filename="${fileId}"`
       }
-    })
-  } catch (error) {
-    if (error instanceof HttpResponseError) {
-      return NextResponse.json(
-        {
-          success: false,
-          message: error.message
-        },
-        { status: Number(HttpStatusCodeEnum.BAD_REQUEST) }
-      )
     }
   }
-}
+)
--- src/app/api/operations/[operationId]/users/[userId]/files/route.ts
+++ src/app/api/operations/[operationId]/users/[userId]/files/route.ts
@@ -1,31 +1,18 @@
-import { auth } from '@/auth'
+import { RouterApiFactory } from '@/modules/api/infrastructure/factories/router-service-api.factory'
 import { HttpStatusCodeEnum } from '@/modules/authentication/domain/enums/status-codes.enum'
-import { HttpResponseError } from '@/modules/shared/infrastructure/errors/http-response.error'
-import type { UserEntity } from '@/modules/users/domain/entities/user.entity'
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
+import type { UserFileInterface } from '@/modules/users/domain/interfaces/user-file.interface'
 import { GetUserFilesFactory } from '@/modules/users/infrastructure/factories/get-user-files.factory'
-import { NextResponse, type NextRequest } from 'next/server'
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: Promise<{ userId: UserEntity['id'] }> }
-): Promise<NextResponse> {
-  try {
-    const { userId } = await params
-    const { token } = await auth()
-    const getUserFilesFactory = GetUserFilesFactory.create()
-    const response = await getUserFilesFactory.execute(token, userId)
-    return NextResponse.json(response, {
-      status: Number(HttpStatusCodeEnum.OK)
+const routerApi = RouterApiFactory.create()
+
+export const GET = routerApi.GET<UrlParams, UserFileInterface[]>(
+  async ({ operationId, userId }) => {
+    const getUserFilesFactory = GetUserFilesFactory.create({
+      operationId,
+      userId
     })
-  } catch (error) {
-    if (error instanceof HttpResponseError) {
-      return NextResponse.json(
-        {
-          success: false,
-          message: error.message
-        },
-        { status: Number(HttpStatusCodeEnum.BAD_REQUEST) }
-      )
-    }
+    const response = await getUserFilesFactory.execute()
+    return { data: response, status: HttpStatusCodeEnum.OK }
   }
-}
+)
