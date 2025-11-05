039c9828f576aa2c0e27ec26af1401ced802bd2a
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-11-05 13:58:55 +0000

    refactor: Adaptação das fabricas para construção do novos modelos de serviços (Permissões - Backend)

--- src/modules/permissions/infrastructure/factories/post-feature.factory.ts
+++ src/modules/permissions/infrastructure/factories/post-feature.factory.ts
@@ -2,11 +2,14 @@ import { HttpClientFactory } from '@/modules/shared/infrastructure/factories/htt
 import { ExecuteRequestFactory } from '@/modules/shared/infrastructure/factories/request.factory'
 import type { PostFeatureServiceInterface } from '../../domain/interfaces/post-feature-service.interface'
 import { PostFeatureService } from '../services/post-feature.service'
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
+import { AuthTokenFactory } from '@/modules/api/infrastructure/factories/auth-token.factory'
 
 export class PostFeatureFactory {
-  static create(): PostFeatureServiceInterface {
+  static create(params: UrlParams): PostFeatureServiceInterface {
     const httpClient = HttpClientFactory.create(process.env.HOST_API)
     const executeRequest = ExecuteRequestFactory.create(httpClient)
-    return new PostFeatureService(executeRequest)
+    const authToken = AuthTokenFactory.create()
+    return new PostFeatureService(executeRequest, authToken, params)
   }
 }
