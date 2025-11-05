7bbf506f34d5106f8ce9238ca6b87ed6f069980b
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-11-05 14:30:13 +0000

    refactor: Adaptação dos novos modelos de serviços (Permissões - Router API)

--- src/modules/api/infrastructure/services/post-feature-router-api.service.ts
+++ src/modules/api/infrastructure/services/post-feature-router-api.service.ts
@@ -1,33 +1,30 @@
 import type { ExecuteRequest } from '@/modules/shared/infrastructure/services/execute-request.service'
 import type { HttpRequestConfig } from '@/modules/shared/domain/interfaces/http-request-config.interface'
 import type { HttpResponse } from '@/modules/shared/domain/interfaces/http-response.interface'
-import type { PermissionProfileInterface } from '@/modules/permissions/domain/interfaces/permission-profiles.interface'
 import type { PostFeatureRouterApiServiceInterface } from '../../domain/interfaces/post-feature-router-api-service.interface'
 import type { PermissionProfileEntity } from '@/modules/permissions/domain/entities/permission-profile.entity'
 import { HttpResponsePermissionProfileValidator } from '@/modules/permissions/domain/validators/http-response-permission-profile.validator'
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
 
 export class PostFeatureRouterApiService
   implements PostFeatureRouterApiServiceInterface
 {
-  constructor(private readonly httpRequest: ExecuteRequest) {}
+  constructor(
+    private readonly httpRequest: ExecuteRequest,
+    private readonly params: UrlParams
+  ) {}
   getHttpRequestConfig(
     features: number[],
-    PermissionProfileId: PermissionProfileInterface['id']
+    { permissionProfileId, operationId }: UrlParams
   ): HttpRequestConfig<number[]> {
     return {
       method: 'POST',
       data: features,
-      url: `api/permission/${PermissionProfileId}/feature`
+      url: `api/operations/${operationId}/permissions/${permissionProfileId}/features`
     }
   }
-  async execute(
-    features: number[],
-    PermissionProfileId: PermissionProfileInterface['id']
-  ): Promise<void> {
-    const settingsAuthHTTP = this.getHttpRequestConfig(
-      features,
-      PermissionProfileId
-    )
+  async execute(features: number[]): Promise<void> {
+    const settingsAuthHTTP = this.getHttpRequestConfig(features, this.params)
     const { success, status }: HttpResponse<PermissionProfileEntity> =
       await this.httpRequest.execute(settingsAuthHTTP)
     HttpResponsePermissionProfileValidator.validate(success, status)
--- src/modules/api/infrastructure/services/post-permission-profile-and-features-router-api.service.ts
+++ src/modules/api/infrastructure/services/post-permission-profile-and-features-router-api.service.ts
@@ -4,24 +4,30 @@ import type { HttpResponse } from '@/modules/shared/domain/interfaces/http-respo
 import type { PostPermissionProfileAndFeaturesRouterApiServiceInterface } from '../../domain/interfaces/post-permission-profile-and-features-router-api-service.interface'
 import { HttpResponsePermissionProfileValidator } from '@/modules/permissions/domain/validators/http-response-permission-profile.validator'
 import type { PermissionProfileAndFeaturesInterface } from '@/modules/permissions/domain/interfaces/permission-profile-and-features'
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
 
 export class PostPermissionProfileAndFeaturesRouterApiService
   implements PostPermissionProfileAndFeaturesRouterApiServiceInterface
 {
-  constructor(private readonly httpRequest: ExecuteRequest) {}
+  constructor(
+    private readonly httpRequest: ExecuteRequest,
+    private readonly params: UrlParams
+  ) {}
   getHttpRequestConfig(
+    { operationId }: UrlParams,
     permissionProfileAndFeatures: PermissionProfileAndFeaturesInterface
   ): HttpRequestConfig<PermissionProfileAndFeaturesInterface> {
     return {
       method: 'POST',
       data: permissionProfileAndFeatures,
-      url: `api/permission/all-in-one`
+      url: `api/operations/${operationId}/permissions/all-in-one`
     }
   }
   async execute(
     permissionProfileAndFeatures: PermissionProfileAndFeaturesInterface
   ): Promise<void> {
     const settingsAuthHTTP = this.getHttpRequestConfig(
+      this.params,
       permissionProfileAndFeatures
     )
     const {
