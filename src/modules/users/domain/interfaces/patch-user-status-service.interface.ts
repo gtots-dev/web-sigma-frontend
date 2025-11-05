4f3c86d29bfb182c023d7b38946d607ca3349899
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-11-05 14:18:45 +0000

    refactor: Interface de atualização  de status

--- src/modules/users/domain/interfaces/patch-user-status-service.interface.ts
+++ src/modules/users/domain/interfaces/patch-user-status-service.interface.ts
@@ -1,5 +1,3 @@
-import type { TokenEntities } from '@/modules/authentication/domain/entities/token.entity'
-
 export interface PatchUserStatusServiceInterface {
-  execute(token: TokenEntities, userEnableAndDisable: FormData): Promise<void>
+  execute(userEnableAndDisable: FormData): Promise<void>
 }
