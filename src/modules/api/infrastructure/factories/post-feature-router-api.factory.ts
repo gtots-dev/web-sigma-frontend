6750838de483b11ab29a66671fbd6ae034a79d95
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-11-05 14:30:44 +0000

    refactor: Adaptação das fabricas para construção do novos modelos de serviços (Permissões - Router API)

--- src/modules/api/infrastructure/factories/post-feature-router-api.factory.ts
+++ src/modules/api/infrastructure/factories/post-feature-router-api.factory.ts
@@ -2,11 +2,12 @@ import { HttpClientFactory } from '@/modules/shared/infrastructure/factories/htt
 import { ExecuteRequestFactory } from '@/modules/shared/infrastructure/factories/request.factory'
 import { PostFeatureRouterApiService } from '../services/post-feature-router-api.service'
 import type { PostFeatureRouterApiServiceInterface } from '../../domain/interfaces/post-feature-router-api-service.interface'
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
 
 export class PostFeatureRouterApiFactory {
-  static create(): PostFeatureRouterApiServiceInterface {
+  static create(params: UrlParams): PostFeatureRouterApiServiceInterface {
     const httpClient = HttpClientFactory.create('/')
     const executeRequest = ExecuteRequestFactory.create(httpClient)
-    return new PostFeatureRouterApiService(executeRequest)
+    return new PostFeatureRouterApiService(executeRequest, params)
   }
 }
--- src/modules/api/infrastructure/factories/post-permission-profile-router-api.factory.ts
+++ src/modules/api/infrastructure/factories/post-permission-profile-router-api.factory.ts
@@ -2,11 +2,14 @@ import { HttpClientFactory } from '@/modules/shared/infrastructure/factories/htt
 import { ExecuteRequestFactory } from '@/modules/shared/infrastructure/factories/request.factory'
 import { PostPermissionProfileRouterApiService } from '../services/post-permission-profile-router-api.service'
 import type { PostPermissionProfileRouterApiServiceInterface } from '../../domain/interfaces/post-permission-profile-router-api-service.interface'
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
 
 export class PostPermissionProfileRouterApiFactory {
-  static create(): PostPermissionProfileRouterApiServiceInterface {
+  static create(
+    params: UrlParams
+  ): PostPermissionProfileRouterApiServiceInterface {
     const httpClient = HttpClientFactory.create('/')
     const executeRequest = ExecuteRequestFactory.create(httpClient)
-    return new PostPermissionProfileRouterApiService(executeRequest)
+    return new PostPermissionProfileRouterApiService(executeRequest, params)
   }
 }
