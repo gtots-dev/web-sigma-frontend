ac7728192b879d9e781219570cfcd009420c866c
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-11-05 14:28:32 +0000

    refactor: Alterado nomenclatura de PUT para PATCH

--- src/modules/permissions/presentation/schemas/put-user-permission-profile-form.schema.ts
+++ src/modules/permissions/presentation/schemas/put-user-permission-profile-form.schema.ts
@@ -1,10 +1,10 @@
 import { z } from 'zod'
 
-export const PutPermissionProfileStatusFormSchema = z.object({
+export const PatchPermissionProfileStatusFormSchema = z.object({
   id: z.number(),
   enabled: z.boolean().nullable()
 })
 
-export type PutPermissionProfileStatusFormType = z.infer<
-  typeof PutPermissionProfileStatusFormSchema
+export type PatchPermissionProfileStatusFormType = z.infer<
+  typeof PatchPermissionProfileStatusFormSchema
 >
