09596e16465f8ed2efe7ba9501b5aae337f63f7e
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-11-05 14:30:13 +0000

    refactor: Adaptação dos novos modelos de serviços (Permissões - Router API)

--- src/modules/api/infrastructure/services/post-permission-profile-router-api.service.ts
+++ src/modules/api/infrastructure/services/post-permission-profile-router-api.service.ts
@@ -5,24 +5,32 @@ import type { PostPermissionProfileRouterApiServiceInterface } from '../../domai
 import type { PermissionProfileInterface } from '@/modules/permissions/domain/interfaces/permission-profiles.interface'
 import type { PermissionProfileEntity } from '@/modules/permissions/domain/entities/permission-profile.entity'
 import { HttpResponsePermissionProfileValidator } from '@/modules/permissions/domain/validators/http-response-permission-profile.validator'
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
 
 export class PostPermissionProfileRouterApiService
   implements PostPermissionProfileRouterApiServiceInterface
 {
-  constructor(private readonly httpRequest: ExecuteRequest) {}
+  constructor(
+    private readonly httpRequest: ExecuteRequest,
+    private readonly params: UrlParams
+  ) {}
   getHttpRequestConfig(
+    { operationId }: UrlParams,
     permissionProfile: PermissionProfileInterface
   ): HttpRequestConfig<PermissionProfileInterface> {
     return {
       method: 'POST',
       data: permissionProfile,
-      url: `api/permission`
+      url: `api/operations/${operationId}/permissions`
     }
   }
   async execute(
     permissionProfile: PermissionProfileInterface
   ): Promise<PermissionProfileEntity> {
-    const settingsAuthHTTP = this.getHttpRequestConfig(permissionProfile)
+    const settingsAuthHTTP = this.getHttpRequestConfig(
+      this.params,
+      permissionProfile
+    )
     const { success, data, status }: HttpResponse<PermissionProfileEntity> =
       await this.httpRequest.execute(settingsAuthHTTP)
     HttpResponsePermissionProfileValidator.validate(success, status)
