cfd5ecf990612104dbc889051c6e42fd210fbe31
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-11-05 14:00:47 +0000

    refactor: Adaptação das interfaces de implementação para construção do novos modelos de serviços (Permissões - Backend)

--- src/modules/permissions/domain/interfaces/post-permission-profile-service.interface.ts
+++ src/modules/permissions/domain/interfaces/post-permission-profile-service.interface.ts
@@ -1,9 +1,7 @@
-import type { TokenEntities } from '@/modules/authentication/domain/entities/token.entity'
 import type { PermissionProfileInterface } from './permission-profiles.interface'
 
 export interface PostPermissionProfileServiceInterface {
   execute(
-    token: TokenEntities,
     permissionProfile: PermissionProfileInterface
   ): Promise<PermissionProfileInterface>
 }
--- src/modules/permissions/domain/interfaces/put-permission-profile-status-service.interface.ts
+++ src/modules/permissions/domain/interfaces/put-permission-profile-status-service.interface.ts
@@ -1,10 +1,6 @@
-import type { TokenEntities } from '@/modules/authentication/domain/entities/token.entity'
-import type { PermissionProfileEntity } from '../entities/permission-profile.entity'
 import type { PermissionProfileEnableAndDisableInterface } from './permission-profile-enable-and-disable.interface'
-export interface PutPermissionProfileStatusServiceInterface {
+export interface PatchPermissionProfileStatusServiceInterface {
   execute(
-    token: TokenEntities,
-    permissionProfileId: PermissionProfileEntity['id'],
     permissionProfileEnableAndDisable: PermissionProfileEnableAndDisableInterface
   ): Promise<void>
 }
