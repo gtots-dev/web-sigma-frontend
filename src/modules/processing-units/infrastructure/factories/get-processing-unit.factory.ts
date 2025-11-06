07d69d8f11af228d7b79a33a0d1b0972c274bfd8
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-11-06 21:12:58 +0000

    refactor: Adaptação das fabricas para construção do novos modelos de serviços (Unidade de processamento - Backend)

--- src/modules/processing-units/infrastructure/factories/get-processing-unit.factory.ts
+++ src/modules/processing-units/infrastructure/factories/get-processing-unit.factory.ts
@@ -1,15 +1,15 @@
 import { HttpClientFactory } from '@/modules/shared/infrastructure/factories/http-client.factory'
-import type { PostProcessingUnitServiceInterface } from '../../domain/interfaces/post-processing-unit-service.interface'
 import { ExecuteRequestFactory } from '@/modules/shared/infrastructure/factories/request.factory'
-import { PostProcessingUnitService } from '../services/post-processing-unit.service'
 import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
 import { AuthTokenFactory } from '@/modules/api/infrastructure/factories/auth-token.factory'
+import { GetProcessingUnitsService } from '../services/get-processing-unit.service'
+import type { GetProcessingUnitsServiceInterface } from '../../domain/interfaces/get-processing-unit-service.interface'
 
-export class PostProcessingUnitFactory {
-  static create(params: UrlParams): PostProcessingUnitServiceInterface {
+export class GetProcessingUnitsFactory {
+  static create(params: UrlParams): GetProcessingUnitsServiceInterface {
     const httpClient = HttpClientFactory.create(process.env.HOST_API)
     const executeRequest = ExecuteRequestFactory.create(httpClient)
     const authToken = AuthTokenFactory.create()
-    return new PostProcessingUnitService(executeRequest, authToken, params)
+    return new GetProcessingUnitsService(executeRequest, authToken, params)
   }
 }
