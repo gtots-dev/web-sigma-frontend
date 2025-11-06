8000b4c04b89bd872224d34ee284060d162abca9
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-11-06 21:08:38 +0000

    refactor: Adaptação dos novos modelos de serviços (Unidades de Processamento - Router API)

---
+++ src/modules/api/infrastructure/services/patch-processing-unit-status-router-api.service.ts
@@ -0,0 +1,38 @@
+import type { ExecuteRequest } from '@/modules/shared/infrastructure/services/execute-request.service'
+import type { HttpRequestConfig } from '@/modules/shared/domain/interfaces/http-request-config.interface'
+import { HttpResponseProcessingUnitValidator } from '@/modules/processing-units/domain/validators/http-response-processing-unit.validator'
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
+import type { PatchProcessingUnitStatusRouterApiServiceInterface } from '../../domain/interfaces/patch-processing-unit-status-router-api-service.interface'
+import type { ProcessingUnitEnableAndDisableInterface } from '@/modules/processing-units/domain/interfaces/processing-unit-enable-and-disable.interface'
+
+export class PatchProcessingUnitStatusRouterApiService
+  implements PatchProcessingUnitStatusRouterApiServiceInterface
+{
+  constructor(
+    private readonly executeRequest: ExecuteRequest,
+    private readonly params: UrlParams
+  ) {}
+
+  getHttpRequestConfig(
+    { operationId, contractId, processingUnitId }: UrlParams,
+    processingUnitEnableAndDisable: ProcessingUnitEnableAndDisableInterface
+  ): HttpRequestConfig<ProcessingUnitEnableAndDisableInterface> {
+    return {
+      method: 'PATCH',
+      data: processingUnitEnableAndDisable,
+      url: `api/operations/${operationId}/contracts/${contractId}/processing-unit/${processingUnitId}/status`
+    }
+  }
+
+  async execute(
+    processingUnitEnableAndDisable: ProcessingUnitEnableAndDisableInterface
+  ): Promise<void> {
+    const settingsAuthHTTP = this.getHttpRequestConfig(
+      this.params,
+      processingUnitEnableAndDisable
+    )
+    const { success, status } =
+      await this.executeRequest.execute<null>(settingsAuthHTTP)
+    HttpResponseProcessingUnitValidator.validate(success, status)
+  }
+}
