5e504128c3c08b678037d32233b2706e18cc6ab9
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-11-10 19:00:35 +0000

    refactor: Adaptação das fabricas para construção do novos modelos de serviços (Faixas - Router API)

--- src/modules/api/infrastructure/factories/patch-lane-router-api.factory.ts
+++ src/modules/api/infrastructure/factories/patch-lane-router-api.factory.ts
@@ -1,13 +1,13 @@
 import { HttpClientFactory } from '@/modules/shared/infrastructure/factories/http-client.factory'
 import { ExecuteRequestFactory } from '@/modules/shared/infrastructure/factories/request.factory'
-import type { PostLaneRouterApiServiceInterface } from '../../domain/interfaces/post-lane-router-api-service.interface'
-import { PostLaneRouterApiService } from '../services/post-lane-router-api.service'
 import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
+import type { PatchLaneRouterApiServiceInterface } from '../../domain/interfaces/patch-lane-router-api-service.interface'
+import { PatchLaneRouterApiService } from '../services/patch-lane-router-api.service'
 
-export class PostLaneRouterApiFactory {
-  static create(params: UrlParams): PostLaneRouterApiServiceInterface {
+export class PatchLaneRouterApiFactory {
+  static create(params: UrlParams): PatchLaneRouterApiServiceInterface {
     const httpClient = HttpClientFactory.create('/')
     const executeRequest = ExecuteRequestFactory.create(httpClient)
-    return new PostLaneRouterApiService(executeRequest, params)
+    return new PatchLaneRouterApiService(executeRequest, params)
   }
 }
