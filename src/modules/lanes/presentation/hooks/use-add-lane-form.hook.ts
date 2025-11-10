90178d151b8bfba42dafab62412083410020b502
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-11-10 19:10:50 +0000

    refactor: Adaptação de Hooks de faixas para novos modelos e serviços

--- src/modules/lanes/presentation/hooks/use-add-lane-form.hook.ts
+++ src/modules/lanes/presentation/hooks/use-add-lane-form.hook.ts
@@ -7,20 +7,15 @@ import {
   AddLaneFormSchema,
   type AddLaneFormType
 } from '../schemas/add-lane-form.schema'
-import { useParams } from 'next/navigation'
 import type { LaneEntity } from '../../domain/entities/lane.entity'
 
 export function useAddLaneForm() {
-  const { operationId, contractId, processingUnitId } = useParams()
   const defaultValues = useMemo<LaneEntity>(
     () => ({
       name: '',
-      cfg: '',
-      up_id: Number(processingUnitId),
-      contract_id: Number(contractId),
-      operation_id: Number(operationId)
+      cfg: ''
     }),
-    [operationId, contractId, processingUnitId]
+    []
   )
 
   const methods = useForm<AddLaneFormType>({
--- src/modules/lanes/presentation/hooks/use-add-lane-submit.hook.ts
+++ src/modules/lanes/presentation/hooks/use-add-lane-submit.hook.ts
@@ -1,37 +1,39 @@
+'use client'
+
 import { useCallback } from 'react'
 import { toast } from '@/modules/shared/presentation/components/hooks/use-toast'
 import { HttpResponseError } from '@/modules/shared/infrastructure/errors/http-response.error'
 import type { LaneEntity } from '../../domain/entities/lane.entity'
 import { useLaneStore } from '../stores/lanes.store'
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
+import { useParams } from 'next/navigation'
 
 export function useAddLaneSubmit() {
+  const { operationId, contractId, processingUnitId }: UrlParams = useParams()
   const { getLanes, addLane } = useLaneStore()
 
   const onAction = useCallback(
-    async (
-      data: LaneEntity,
-      onSuccess: VoidFunction
-    ): Promise<void> => {
+    async (lane: LaneEntity, onSuccess: VoidFunction): Promise<void> => {
       try {
-        await addLane(data)
+        await addLane({ operationId, contractId, processingUnitId }, lane)
         toast({
-          title: 'Contrato adicionado com sucesso!',
+          title: 'Faixa adicionada com sucesso!',
           variant: 'success'
         })
-        await getLanes()
+        await getLanes({ operationId, contractId, processingUnitId })
         onSuccess?.()
       } catch (error) {
         if (error instanceof HttpResponseError) {
           toast({
-            title: 'Erro ao cadastrar a Faixa',
+            title: 'Erro ao cadastrar a faixa',
             description:
-              'Ocorreu um problema ao tentar cadastrar a Faixa. Verifique e tente novamente',
+              'Ocorreu um problema ao tentar cadastrar a faixa. Verifique e tente novamente',
             variant: 'destructive'
           })
         }
       }
     },
-    [addLane, getLanes]
+    [addLane, getLanes, operationId, contractId, processingUnitId]
   )
 
   return { onAction }
