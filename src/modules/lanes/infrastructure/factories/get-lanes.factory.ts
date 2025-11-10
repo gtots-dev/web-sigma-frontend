6b10c5eaec9209719d7a90eeda188d2498e22161
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-11-10 19:09:51 +0000

    feat: Adaptação das fabricas para construção do novos modelos de serviços (Faixas - Backend)

--- src/modules/lanes/infrastructure/factories/get-lanes.factory.ts
+++ src/modules/lanes/infrastructure/factories/get-lanes.factory.ts
@@ -1,15 +1,16 @@
 import { HttpClientFactory } from '@/modules/shared/infrastructure/factories/http-client.factory'
-import type { PostLaneServiceInterface } from '../../domain/interfaces/post-lane-service.interface'
 import { ExecuteRequestFactory } from '@/modules/shared/infrastructure/factories/request.factory'
-import { PostLaneService } from '../services/post-lane.service'
 import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
 import { AuthTokenFactory } from '@/modules/api/infrastructure/factories/auth-token.factory'
+import type { GetLanesServiceInterface } from '../../domain/interfaces/get-lanes-service.interfaces'
+import { GetLanesService } from '../services/get-lanes.service'
 
-export class PostLaneFactory {
-  static create(params: UrlParams): PostLaneServiceInterface {
+
+export class GetLanesFactory {
+  static create(params: UrlParams): GetLanesServiceInterface {
     const httpClient = HttpClientFactory.create(process.env.HOST_API)
     const executeRequest = ExecuteRequestFactory.create(httpClient)
     const authToken = AuthTokenFactory.create()
-    return new PostLaneService(executeRequest, authToken, params)
+    return new GetLanesService(executeRequest, authToken, params)
   }
 }
