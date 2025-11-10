90ef84ba3ac664220259308c795673019df3541a
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-11-10 21:03:04 +0000

    refactor: Nomenclaturas de interfaces para "Gateways" para se encaixar melhor ao "Pattern"

--- src/modules/processing-units/domain/gateways/patch-processing-unit-status-service.interface.ts
+++
@@ -1,7 +0,0 @@
-import type { ProcessingUnitEnableAndDisableInterface } from '../interfaces/processing-unit-enable-and-disable.interface'
-
-export interface PatchProcessingUnitStatusGateway {
-  execute(
-    processingUnitEnableAndDisable: ProcessingUnitEnableAndDisableInterface
-  ): Promise<void>
-}
