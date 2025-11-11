d3c4fa4ced54bc6a0b856b82797ecc275be8013c
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-11-11 02:20:54 +0000

    refactor: Removido utilitário de listagem de usuários

--- src/modules/users/presentation/utils/get-users.util.ts
+++
@@ -1,9 +0,0 @@
-import { auth } from '@/auth'
-import { GetUsersFactory } from '../../infrastructure/factories/get-users.factory'
-import type { UserInterface } from '../../domain/interfaces/user.interface'
-
-export async function getUsers(): Promise<UserInterface[]> {
-  const { token } = await auth()
-  const getUsersFactory = GetUsersFactory.create()
-  return await getUsersFactory.execute(token)
-}
