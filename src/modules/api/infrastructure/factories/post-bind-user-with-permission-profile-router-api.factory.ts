2c3d166994d169f09b1cdfaac92edbe719eb21d2
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-11-12 19:29:00 +0000

    feat: Adaptação das fabricas para construção do novos modelos de serviços (Permissões - Router API)

--- src/modules/api/infrastructure/factories/post-bind-user-with-permission-profile-router-api.factory.ts
+++ src/modules/api/infrastructure/factories/post-bind-user-with-permission-profile-router-api.factory.ts
@@ -2,11 +2,17 @@ import { HttpClientFactory } from '@/modules/shared/infrastructure/factories/htt
 import { ExecuteRequestFactory } from '@/modules/shared/infrastructure/factories/request.factory'
 import type { PostBindUserWithPermissionProfileRouterApiGateway } from '../../domain/gateways/post-bind-user-with-permission-profile-router-api.gateway'
 import { PostBindUserWithPermissionProfileRouterApiService } from '../services/post-bind-user-with-permission-profile-router-api.service'
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
 
 export class PostBindUserWithPermissionProfileRouterApiFactory {
-  static create(): PostBindUserWithPermissionProfileRouterApiGateway {
+  static create(
+    params: UrlParams
+  ): PostBindUserWithPermissionProfileRouterApiGateway {
     const httpClient = HttpClientFactory.create('/')
     const executeRequest = ExecuteRequestFactory.create(httpClient)
-    return new PostBindUserWithPermissionProfileRouterApiService(executeRequest)
+    return new PostBindUserWithPermissionProfileRouterApiService(
+      executeRequest,
+      params
+    )
   }
 }
