cffdbaeee661437f4f3d5359749cadfadc953ada
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-11-12 19:31:17 +0000

    feat: Adaptação das fabricas para construção do novos modelos de serviços (Permissões - Backend)

--- src/modules/users/infrastructure/factories/delete-bind-user-with-permission-profile.factory.ts
+++ src/modules/users/infrastructure/factories/delete-bind-user-with-permission-profile.factory.ts
@@ -2,11 +2,18 @@ import { HttpClientFactory } from '@/modules/shared/infrastructure/factories/htt
 import { ExecuteRequestFactory } from '@/modules/shared/infrastructure/factories/request.factory'
 import type { DeleteBindUserWithPermissionProfileGateway } from '../../domain/gateways/delete-bind-user-with-permission-profile.gateway'
 import { DeleteBindUserWithPermissionProfileService } from '../services/delete-bind-user-with-permission-profile.service'
+import { AuthTokenFactory } from '@/modules/api/infrastructure/factories/auth-token.factory'
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
 
 export class DeleteBindUserWithPermissionProfileFactory {
-  static create(): DeleteBindUserWithPermissionProfileGateway {
+  static create(params: UrlParams): DeleteBindUserWithPermissionProfileGateway {
     const httpClient = HttpClientFactory.create(process.env.HOST_API)
     const executeRequest = ExecuteRequestFactory.create(httpClient)
-    return new DeleteBindUserWithPermissionProfileService(executeRequest)
+    const authToken = AuthTokenFactory.create()
+    return new DeleteBindUserWithPermissionProfileService(
+      executeRequest,
+      authToken,
+      params
+    )
   }
 }
