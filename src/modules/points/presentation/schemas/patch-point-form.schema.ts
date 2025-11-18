f2fdd0c074d21e8d44b35eab5184732f49797734
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-11-18 17:03:16 +0000

    feat: injeção das mensagens as esquemas

--- src/modules/points/presentation/schemas/patch-point-form.schema.ts
+++ src/modules/points/presentation/schemas/patch-point-form.schema.ts
@@ -1,9 +1,10 @@
+import { MESSAGES_POINT } from '@/modules/shared/presentation/messages/points'
 import { z } from 'zod'
 
 export const PatchPointFormSchema = z.object({
   id: z.number(),
   name: z.string().nonempty({
-    message: ''
+    message: MESSAGES_POINT['14.4']
   }),
   description: z.string(),
   cfg: z.string()
--- src/modules/points/presentation/schemas/post-point-form.schema.ts
+++ src/modules/points/presentation/schemas/post-point-form.schema.ts
@@ -1,8 +1,9 @@
+import { MESSAGES_POINT } from '@/modules/shared/presentation/messages/points'
 import { z } from 'zod'
 
 export const PostPointFormSchema = z.object({
   name: z.string().nonempty({
-    message: ''
+    message: MESSAGES_POINT['14.6']
   }),
   description: z.string(),
   cfg: z.string()
