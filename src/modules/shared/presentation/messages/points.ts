a6659c4664fe6e239d2c568299925fbe3e79ea8a
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-11-18 17:03:01 +0000

    feat: Mensagens da seção de pontos

---
+++ src/modules/shared/presentation/messages/points.ts
@@ -0,0 +1,25 @@
+type MessageKeys =
+  | '14.1'
+  | '14.2'
+  | '14.3'
+  | '14.4'
+  | '14.5'
+  | '14.6'
+  | '14.7'
+  | '14.8'
+  | '14.9'
+  | '14.10'
+
+export const MESSAGES_POINT: Record<MessageKeys, string> = {
+  '14.1': 'Pontos',
+  '14.2': 'Navegue e gerencie seus pontos com suas faixas disponíveis.',
+  '14.3': 'Nenhum ponto encontrado',
+  '14.4': 'Novo Ponto',
+  '14.5': 'Preencha os dados obrigatórios para adicionar um novo ponto.',
+  '14.6': 'O nome do ponto é obrigatório.',
+  '14.7': 'Editar ponto',
+  '14.8': 'Altere os dados desejados para editar o ponto.',
+  '14.9': 'Habilitar/Desabilitar',
+  '14.10':
+    'Caso a opção correspondente esteja desabilitada, o ponto não estará mais disponível na lista.'
+}
