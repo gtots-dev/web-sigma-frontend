0c26bcf4344497ae5d9e97a109325f6de5ae5cec
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-11-18 16:18:44 +0000

    feat: Componentes de formulário/Inputs

---
+++ src/modules/points/presentation/components/point-form/index.ts
@@ -0,0 +1,17 @@
+import { PointFormInputCfgComponent } from './point-form-input-cfg.component'
+import { PointFormInputEnabledComponent } from './point-form-input-enabled.component'
+import { PointFormInputNameComponent } from './point-form-input-name.component'
+import { PointFormSubmitComponent } from './point-form-submit.component'
+import { PointFormComponent } from './point-form.component'
+import { PointFormInputDescriptionComponent } from './point-form-input-description.component'
+
+export const PointForm = {
+  Form: PointFormComponent,
+  Submit: PointFormSubmitComponent,
+  Input: {
+    Name: PointFormInputNameComponent,
+    cfg: PointFormInputCfgComponent,
+    Description: PointFormInputDescriptionComponent,
+    Enabled: PointFormInputEnabledComponent
+  }
+}
