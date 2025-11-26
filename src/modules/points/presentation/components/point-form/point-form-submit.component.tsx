efffb83cf392d750f0c12eb0467a9b29365c848f
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-11-26 18:09:32 +0000

    feat: Bloqueio do botão durante o envio do formulário

--- src/modules/points/presentation/components/point-form/point-form-submit.component.tsx
+++ src/modules/points/presentation/components/point-form/point-form-submit.component.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { LoadingSpinComponent } from '@/modules/shared/presentation/components/loading-spin/loading-spin.component'
 import { Button } from '@/modules/shared/presentation/components/shadcn/button'
 import { useFormContext } from 'react-hook-form'
 
@@ -10,16 +11,18 @@ interface PointFormSubmitComponentProps<T> {
 export function PointFormSubmitComponent<T>({
   onSubmit
 }: PointFormSubmitComponentProps<T>) {
-  const { handleSubmit } = useFormContext<T>()
+  const { handleSubmit, formState } = useFormContext<T>()
+  const { isSubmitting, isSubmitSuccessful } = formState
 
   return (
     <Button
       type="submit"
       className="w-full sm:w-[150px]"
       variant="primary"
+      disabled={isSubmitting || isSubmitSuccessful}
       onClick={handleSubmit(onSubmit)}
     >
-      Confirmar
+      Confirmar <LoadingSpinComponent loading={isSubmitting} />
     </Button>
   )
 }
