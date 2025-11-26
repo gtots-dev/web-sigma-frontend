57f4dbd6eb77cd7e2691940a44739630f23b8a1a
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-11-26 17:41:23 +0000

    feat: Validação de máximo de caracteres

--- src/modules/points/presentation/schemas/patch-point-form.schema.ts
+++ src/modules/points/presentation/schemas/patch-point-form.schema.ts
@@ -3,10 +3,17 @@ import { z } from 'zod'
 
 export const PatchPointFormSchema = z.object({
   id: z.number(),
-  name: z.string().nonempty({
-    message: MESSAGES_POINT['14.4']
+  name: z
+    .string()
+    .nonempty({
+      message: MESSAGES_POINT['14.6']
+    })
+    .max(150, {
+      message: MESSAGES_POINT['14.13']
+    }),
+  description: z.string().max(150, {
+    message: MESSAGES_POINT['14.14']
   }),
-  description: z.string(),
   cfg: z.string()
 })
 
--- src/modules/points/presentation/schemas/post-point-form.schema.ts
+++ src/modules/points/presentation/schemas/post-point-form.schema.ts
@@ -2,10 +2,17 @@ import { MESSAGES_POINT } from '@/modules/shared/presentation/messages/points'
 import { z } from 'zod'
 
 export const PostPointFormSchema = z.object({
-  name: z.string().nonempty({
-    message: MESSAGES_POINT['14.6']
+  name: z
+    .string()
+    .nonempty({
+      message: MESSAGES_POINT['14.6']
+    })
+    .max(150, {
+      message: MESSAGES_POINT['14.13']
+    }),
+  description: z.string().max(150, {
+    message: MESSAGES_POINT['14.14']
   }),
-  description: z.string(),
   cfg: z.string()
 })
 
