4d6e7a24fe31d1d5f0352e06e7bb9aa55c4eb67f
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-12-09 13:46:59 +0000

    feat: Esquema de validação do zod para formulário de adição de grupo

---
+++ src/modules/groups/presentation/schemas/post-group-form.schema.ts
@@ -0,0 +1,24 @@
+import { MESSAGES_GROUP } from '@/modules/shared/presentation/messages/groups'
+import { z } from 'zod'
+
+export const PostGroupFormSchema = z.object({
+  name: z
+    .string()
+    .nonempty({
+      message: MESSAGES_GROUP['14.6']
+    })
+    .max(150, {
+      message: MESSAGES_GROUP['14.13']
+    }),
+  description: z
+    .string()
+    .max(150, {
+      message: MESSAGES_GROUP['14.14']
+    })
+    .optional()
+    .nullable()
+    .transform((val) => (!val || val.trim() === '' ? null : val)),
+  cfg: z.string()
+})
+
+export type PostGroupFormType = z.infer<typeof PostGroupFormSchema>
