a29cc85ad99293b2f7480c4179f306d8b0fde48a
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-12-09 17:44:04 +0000

    feat: Rotas de adicionar/remover pontos

---
+++ src/app/api/operations/[operationId]/contracts/[contractId]/groups/[groupId]/points/[pointId]/route.ts
@@ -0,0 +1,22 @@
+import { HttpStatusCodeEnum } from '@/modules/authentication/domain/enums/status-codes.enum'
+import { RouterApiFactory } from '@/modules/api/infrastructure/factories/router-service-api.factory'
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
+import { DeleteGroupPointFactory } from '@/modules/groups/infrastructure/factories/delete-group-point.factory'
+
+const routerApi = RouterApiFactory.create()
+
+export const DELETE = routerApi.DELETE<UrlParams>(
+  async ({ operationId, contractId, groupId, pointId }) => {
+    const deleteGroupPoint = DeleteGroupPointFactory.create({
+      operationId,
+      contractId,
+      groupId,
+      pointId
+    })
+    await deleteGroupPoint.execute()
+    return {
+      data: { success: true },
+      status: HttpStatusCodeEnum.NO_CONTENT
+    }
+  }
+)
