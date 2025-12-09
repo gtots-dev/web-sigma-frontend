5b5e38dfd1765b558b6684ad55ce815c7872a468
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-12-09 13:40:44 +0000

    feat: Tabela de listagem de Grupos

---
+++ src/modules/groups/presentation/components/table-groups/table-groups-item.component.tsx
@@ -0,0 +1,71 @@
+'use client'
+
+import type { ReactNode } from 'react'
+import { Skeleton } from '@/modules/shared/presentation/components/shadcn/skeleton'
+import {
+  TableCell,
+  TableRow
+} from '@/modules/shared/presentation/components/shadcn/table'
+import { useMediaQuery } from '@/modules/shared/presentation/hooks/use-media-query'
+import { AvailabilityStatusComponent } from '@/modules/shared/presentation/components/availability-status/availability-status.component'
+import { useTableGroup } from '../../contexts/table-group.context'
+
+interface TableGroupsItemComponentProps {
+  children?: ReactNode
+}
+
+const truncateText =
+  'h-auto md:h-[52.5px] sm:max-w-full truncate whitespace-nowrap overflow-hidden'
+const baseCell = 'ps-5 sm:ps-10 text-zinc-700 dark:text-zinc-50'
+
+export function TableGroupsItemComponent({
+  children
+}: TableGroupsItemComponentProps) {
+  const { group } = useTableGroup()
+  const isLarge = useMediaQuery('(min-width: 1024px)')
+  const isExtraLarge = useMediaQuery('(min-width: 1230px)')
+
+  const renderSkeleton = () => (
+    <TableRow>
+      <TableCell className={`${baseCell} ${truncateText}`} colSpan={6}>
+        <Skeleton className="w-full !h-[10px] rounded-full" />
+      </TableCell>
+    </TableRow>
+  )
+
+  const renderCompactView = () => (
+    <>
+      <TableCell className={`${baseCell} flex flex-col gap-y-0.5`}>
+        <span title={group.name} className={`${truncateText} !h-auto`}>
+          {group.name}
+        </span>
+      </TableCell>
+    </>
+  )
+
+  const renderExpandedView = () => (
+    <>
+      <TableCell colSpan={2} className={`${baseCell} ${truncateText} w-[30%]`}>
+        {group.name}
+      </TableCell>
+      {isExtraLarge && (
+        <TableCell className={`${baseCell} ${truncateText}`}>
+          <AvailabilityStatusComponent enabled={group.enabled} />
+        </TableCell>
+      )}
+    </>
+  )
+
+  if (isExtraLarge === undefined || isLarge === undefined) {
+    return renderSkeleton()
+  }
+
+  return (
+    <TableRow>
+      {isLarge ? renderExpandedView() : renderCompactView()}
+      <TableCell className="pe-5 sm:pe-10 text-right" colSpan={1}>
+        {children}
+      </TableCell>
+    </TableRow>
+  )
+}
