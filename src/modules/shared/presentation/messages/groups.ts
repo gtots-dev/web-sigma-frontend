f4462a6f9afd58100473f55132582b3e08fed86b
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-12-09 14:48:33 +0000

    feat: Mensagens da seção de grupos

---
+++ src/modules/shared/presentation/messages/groups.ts
@@ -0,0 +1,37 @@
+type MessageKeys =
+  | '14.1'
+  | '14.2'
+  | '14.3'
+  | '14.4'
+  | '14.5'
+  | '14.6'
+  | '14.7'
+  | '14.8'
+  | '14.9'
+  | '14.10'
+  | '14.11'
+  | '14.12'
+  | '14.13'
+  | '14.14'
+  | '14.15'
+  | '14.16'
+
+export const MESSAGES_GROUP: Record<MessageKeys, string> = {
+  '14.1': 'Grupos',
+  '14.2': 'Navegue e gerencie seus grupos com suas faixas disponíveis.',
+  '14.3': 'Nenhum grupo encontrado',
+  '14.4': 'Novo Grupo',
+  '14.5': 'Preencha os dados obrigatórios para adicionar um novo grupo.',
+  '14.6': 'O nome do grupo é obrigatório.',
+  '14.7': 'Editar grupo',
+  '14.8': 'Altere os dados desejados para editar o grupo.',
+  '14.9': 'Habilitar/Desabilitar',
+  '14.10':
+    'Caso a opção correspondente esteja desabilitada, o grupo não estará mais disponível na lista.',
+  '14.11': 'Vincular Faixas',
+  '14.12': 'Selecione as faixas desejadas para o vinculo do grupo.',
+  '14.13': 'O nome deve ter no máximo 150 caracteres.',
+  '14.14': 'A descrição deve ter no máximo 150 caracteres.',
+  '14.15': 'Informações do Grupo',
+  '14.16': 'Veja mais informações sobre o grupo selecionado'
+}
