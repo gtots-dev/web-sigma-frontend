6108c1df215182471dfd4e02b120758021fb17de
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-12-09 17:11:30 +0000

    feat: Chamada da lista de pontos para população do ver mais

--- src/modules/groups/presentation/hooks/use-view-more-group-menu-trigger.hook.ts
+++ src/modules/groups/presentation/hooks/use-view-more-group-menu-trigger.hook.ts
@@ -3,16 +3,21 @@ import { useViewMoreGroupMenuContext } from '../contexts/view-more-group-menu.co
 import { useLaneStore } from '@/modules/lanes/presentation/stores/lanes.store'
 import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
 import { useParams } from 'next/navigation'
+import { usePointStore } from '@/modules/points/presentation/stores/point.store'
 
 export function useViewMoreGroupMenuTrigger() {
   const { open: openDialog } = useViewMoreGroupMenuContext()
   const { getContractLanes } = useLaneStore()
+  const { getPoints } = usePointStore()
   const { operationId, contractId }: UrlParams = useParams()
 
   const loadViewMoreGroupOpenDialog = () => {
     queueMicrotask(async () => {
       try {
-        await getContractLanes({ operationId, contractId })
+        await Promise.all([
+          getContractLanes({ operationId, contractId }),
+          getPoints({ operationId, contractId })
+        ])
         openDialog()
       } catch {
         toast({
