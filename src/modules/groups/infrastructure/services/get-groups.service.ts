324940f873bfdd33399100272e4aaf5ae78291e5
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-12-09 14:26:04 +0000

    feat: Serviço de listagem de grupos (Backend)

---
+++ src/modules/groups/infrastructure/services/get-groups.service.ts
@@ -0,0 +1,39 @@
+import type { ExecuteRequest } from '@/modules/shared/infrastructure/services/execute-request.service'
+import type { HttpRequestConfig } from '@/modules/shared/domain/interfaces/http-request-config.interface'
+import type { HttpResponse } from '@/modules/shared/domain/interfaces/http-response.interface'
+import type { TokenEntities } from '@/modules/authentication/domain/entities/token.entity'
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
+import type { AuthTokenProvider } from '@/modules/api/infrastructure/providers/token.provider'
+import { HttpResponseGroupValidator } from '../../domain/validators/http-response-group.validator'
+import type { GetGroupsGateway } from '../../domain/gateways/get-groups.gateway'
+import type { GroupWithGroupInterface } from '../../domain/interfaces/group-with-group.interface'
+
+export class GetGroupsService implements GetGroupsGateway {
+  constructor(
+    private readonly executeRequest: ExecuteRequest,
+    private readonly auth: AuthTokenProvider,
+    private readonly params: UrlParams
+  ) {}
+
+  getHttpRequestConfig(
+    token: TokenEntities,
+    { operationId, contractId }: UrlParams
+  ): HttpRequestConfig<null> {
+    return {
+      method: 'GET',
+      url: `/operations/${operationId}/contracts/${contractId}/groups`,
+      headers: token.access_token && {
+        Authorization: `${token.token_type} ${token.access_token}`
+      }
+    }
+  }
+
+  async execute(): Promise<GroupWithGroupInterface[]> {
+    const token = await this.auth.getToken()
+    const settingsAuthHTTP = this.getHttpRequestConfig(token, this.params)
+    const { success, data, status }: HttpResponse<GroupWithGroupInterface[]> =
+      await this.executeRequest.execute(settingsAuthHTTP)
+    HttpResponseGroupValidator.validate(success, status)
+    return data
+  }
+}
