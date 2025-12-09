cef0627247a23ace2659c255cc52283bc2f49fb9
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-12-09 13:34:22 +0000

    feat: Serviço de edição de grupo (Router API)

---
+++ src/modules/api/infrastructure/services/patch-group-router-api.service.ts
@@ -0,0 +1,31 @@
+import type { ExecuteRequest } from '@/modules/shared/infrastructure/services/execute-request.service'
+import type { HttpRequestConfig } from '@/modules/shared/domain/interfaces/http-request-config.interface'
+import type { GroupEntity } from '@/modules/groups/domain/entities/group.entity'
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
+import { HttpResponseGroupValidator } from '@/modules/groups/domain/validators/http-response-group.validator'
+import type { PatchGroupRouterApiGateway } from '../../domain/gateways/patch-group-router-api.gateway'
+
+export class PatchGroupRouterApiService implements PatchGroupRouterApiGateway {
+  constructor(
+    private readonly executeRequest: ExecuteRequest,
+    private readonly params: UrlParams
+  ) {}
+
+  getHttpRequestConfig(
+    { operationId, contractId }: UrlParams,
+    group: GroupEntity
+  ): HttpRequestConfig<GroupEntity> {
+    return {
+      method: 'PATCH',
+      data: group,
+      url: `api/operations/${operationId}/contracts/${contractId}/groups`
+    }
+  }
+
+  async execute(group: GroupEntity): Promise<void> {
+    const settingsAuthHTTP = this.getHttpRequestConfig(this.params, group)
+    const { success, status } =
+      await this.executeRequest.execute<null>(settingsAuthHTTP)
+    HttpResponseGroupValidator.validate(success, status)
+  }
+}
