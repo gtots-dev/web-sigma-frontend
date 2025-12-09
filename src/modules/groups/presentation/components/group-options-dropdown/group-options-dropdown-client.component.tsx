f251d2f9f03f3c7187e5062e7ffa2ea047419d64
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-12-09 17:14:08 +0000

    feat: Opção de vinculação de ponto no menu suspenso

--- src/modules/groups/presentation/components/group-options-dropdown/group-options-dropdown-client.component.tsx
+++ src/modules/groups/presentation/components/group-options-dropdown/group-options-dropdown-client.component.tsx
@@ -10,6 +10,8 @@ import { PostGroupLaneMenu } from '../post-group-lane-menu'
 import { PostGroupLaneMenuComponent } from '../post-group-lane-menu/post-group-lane-menu.component'
 import { ViewMoreGroupMenu } from '../view-more-group-menu'
 import { ViewMoreGroupMenuComponent } from '../view-more-group-menu/view-more-group-menu.component'
+import { PostGroupPointMenuComponent } from '../post-group-point-menu/post-group-point-menu.component'
+import { PostGroupPointMenu } from '../post-group-point-menu'
 
 export function GroupOptionsDropdownClient({
   isAdmin,
@@ -20,6 +22,8 @@ export function GroupOptionsDropdownClient({
   patchStatusDescription,
   postGroupLaneTitle,
   postGroupLaneDescription,
+  postGroupPointTitle,
+  postGroupPointDescription,
   viewMoreGroupLaneTitle,
   ViewMoreGroupLaneDescription
 }: {
@@ -30,73 +34,86 @@ export function GroupOptionsDropdownClient({
   patchStatusDescription: string
   postGroupLaneTitle: string
   postGroupLaneDescription: string
+  postGroupPointTitle: string
+  postGroupPointDescription: string
   viewMoreGroupLaneTitle: string
   ViewMoreGroupLaneDescription: string
   permissions: Set<PermissionEnum>
 }) {
   return (
     <ViewMoreGroupMenu.Provider>
-      <PostGroupLaneMenu.Provider>
-        <PatchGroupMenu.Provider>
-          <PatchGroupStatusMenu.Provider>
-            <GroupOptionsDropdown.Root>
-              <GroupOptionsDropdown.Trigger />
+      <PostGroupPointMenu.Provider>
+        <PostGroupLaneMenu.Provider>
+          <PatchGroupMenu.Provider>
+            <PatchGroupStatusMenu.Provider>
+              <GroupOptionsDropdown.Root>
+                <GroupOptionsDropdown.Trigger />
 
-              <GroupOptionsDropdown.Menu>
-                <GroupOptionsDropdown.Item>
-                  <ViewMoreGroupMenu.Trigger />
-                </GroupOptionsDropdown.Item>
-
-                <GroupOptionsDropdown.Item>
-                  <PostGroupLaneMenu.Trigger />
-                </GroupOptionsDropdown.Item>
-
-                {(isAdmin || permissions.has(PermissionEnum.GROUPS_EDIT)) && (
+                <GroupOptionsDropdown.Menu>
                   <GroupOptionsDropdown.Item>
-                    <PatchGroupMenu.Trigger />
+                    <ViewMoreGroupMenu.Trigger />
                   </GroupOptionsDropdown.Item>
-                )}
 
-                {(isAdmin ||
-                  permissions.has(
-                    PermissionEnum.GROUPS_ENABLE_AND_DISABLE
-                  )) && (
                   <GroupOptionsDropdown.Item>
-                    <PatchGroupStatusMenu.Trigger />
+                    <PostGroupLaneMenu.Trigger />
                   </GroupOptionsDropdown.Item>
-                )}
-              </GroupOptionsDropdown.Menu>
-            </GroupOptionsDropdown.Root>
 
-            {
-              <ViewMoreGroupMenuComponent
-                title={viewMoreGroupLaneTitle}
-                description={ViewMoreGroupLaneDescription}
+                  <GroupOptionsDropdown.Item>
+                    <PostGroupPointMenu.Trigger />
+                  </GroupOptionsDropdown.Item>
+
+                  {(isAdmin || permissions.has(PermissionEnum.GROUPS_EDIT)) && (
+                    <GroupOptionsDropdown.Item>
+                      <PatchGroupMenu.Trigger />
+                    </GroupOptionsDropdown.Item>
+                  )}
+
+                  {(isAdmin ||
+                    permissions.has(
+                      PermissionEnum.GROUPS_ENABLE_AND_DISABLE
+                    )) && (
+                    <GroupOptionsDropdown.Item>
+                      <PatchGroupStatusMenu.Trigger />
+                    </GroupOptionsDropdown.Item>
+                  )}
+                </GroupOptionsDropdown.Menu>
+              </GroupOptionsDropdown.Root>
+
+              {
+                <ViewMoreGroupMenuComponent
+                  title={viewMoreGroupLaneTitle}
+                  description={ViewMoreGroupLaneDescription}
+                />
+              }
+
+              <PostGroupLaneMenuComponent
+                title={postGroupLaneTitle}
+                description={postGroupLaneDescription}
               />
-            }
 
-            <PostGroupLaneMenuComponent
-              title={postGroupLaneTitle}
-              description={postGroupLaneDescription}
-            />
-
-            {(isAdmin ||
-              permissions.has(PermissionEnum.GROUPS_ENABLE_AND_DISABLE)) && (
-              <PatchGroupStatusMenuComponent
-                title={patchStatusTitle}
-                description={patchStatusDescription}
+              <PostGroupPointMenuComponent
+                title={postGroupPointTitle}
+                description={postGroupPointDescription}
               />
-            )}
 
-            {(isAdmin || permissions.has(PermissionEnum.GROUPS_EDIT)) && (
-              <PatchGroupMenuComponent
-                title={patchTitle}
-                description={patchDescription}
-              />
-            )}
-          </PatchGroupStatusMenu.Provider>
-        </PatchGroupMenu.Provider>
-      </PostGroupLaneMenu.Provider>
+              {(isAdmin ||
+                permissions.has(PermissionEnum.GROUPS_ENABLE_AND_DISABLE)) && (
+                <PatchGroupStatusMenuComponent
+                  title={patchStatusTitle}
+                  description={patchStatusDescription}
+                />
+              )}
+
+              {(isAdmin || permissions.has(PermissionEnum.GROUPS_EDIT)) && (
+                <PatchGroupMenuComponent
+                  title={patchTitle}
+                  description={patchDescription}
+                />
+              )}
+            </PatchGroupStatusMenu.Provider>
+          </PatchGroupMenu.Provider>
+        </PostGroupLaneMenu.Provider>
+      </PostGroupPointMenu.Provider>
     </ViewMoreGroupMenu.Provider>
   )
 }
