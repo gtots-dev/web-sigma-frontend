947d71523b838fa8c46c1b187310c376e9c89ca6
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-12-09 13:35:34 +0000

    feat: Serviço de habilitar/desabilitar grupo (Router API)

---
+++ src/modules/api/infrastructure/services/patch-group-status-router-api.service.ts
@@ -0,0 +1,38 @@
+import type { ExecuteRequest } from '@/modules/shared/infrastructure/services/execute-request.service'
+import type { HttpRequestConfig } from '@/modules/shared/domain/interfaces/http-request-config.interface'
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
+import { HttpResponseGroupValidator } from '@/modules/groups/domain/validators/http-response-group.validator'
+import type { GroupEnableAndDisableInterface } from '@/modules/groups/domain/interfaces/group-enable-and-disable.interface'
+import type { PatchGroupStatusRouterApiGateway } from '../../domain/gateways/patch-group-status-router-api.gateway'
+
+export class PatchGroupStatusRouterApiService
+  implements PatchGroupStatusRouterApiGateway
+{
+  constructor(
+    private readonly executeRequest: ExecuteRequest,
+    private readonly params: UrlParams
+  ) {}
+
+  getHttpRequestConfig(
+    { operationId, contractId, groupId }: UrlParams,
+    groupEnableAndDisable: GroupEnableAndDisableInterface
+  ): HttpRequestConfig<GroupEnableAndDisableInterface> {
+    return {
+      method: 'PATCH',
+      data: groupEnableAndDisable,
+      url: `api/operations/${operationId}/contracts/${contractId}/groups/${groupId}/status`
+    }
+  }
+
+  async execute(
+    groupEnableAndDisable: GroupEnableAndDisableInterface
+  ): Promise<void> {
+    const settingsAuthHTTP = this.getHttpRequestConfig(
+      this.params,
+      groupEnableAndDisable
+    )
+    const { success, status } =
+      await this.executeRequest.execute<null>(settingsAuthHTTP)
+    HttpResponseGroupValidator.validate(success, status)
+  }
+}
