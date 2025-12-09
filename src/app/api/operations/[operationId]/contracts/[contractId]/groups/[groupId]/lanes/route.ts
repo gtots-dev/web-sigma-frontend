89759ead0df50f4ccaaad37fb45bf7c49b133472
Lucas Martinez <lucas.martinez@gtots.com.br>
2025-12-09 13:30:49 +0000

    feat: Rota de adicionar faixa ao grupo (Router API)

---
+++ src/app/api/operations/[operationId]/contracts/[contractId]/groups/[groupId]/lanes/route.ts
@@ -0,0 +1,23 @@
+import { HttpStatusCodeEnum } from '@/modules/authentication/domain/enums/status-codes.enum'
+import { RouterApiFactory } from '@/modules/api/infrastructure/factories/router-service-api.factory'
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
+import { PostGroupLaneFactory } from '@/modules/groups/infrastructure/factories/post-group-lane.factory'
+
+const routerApi = RouterApiFactory.create()
+
+export const POST = routerApi.POST<UrlParams>(
+  async ({ operationId, contractId, groupId, laneId }, req) => {
+    const groupLane = await req?.json()
+    const postGroupLane = PostGroupLaneFactory.create({
+      operationId,
+      contractId,
+      groupId,
+      laneId
+    })
+    await postGroupLane.execute(groupLane)
+    return {
+      data: { success: true },
+      status: HttpStatusCodeEnum.OK
+    }
+  }
+)
