1cfdb860f8d3faab51d79c7b3ade4b02fa9064eb
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-01-06 13:07:21 +0000

    refactor: Alterado o formato da tipagem do status para "number"

--- src/modules/shared/infrastructure/errors/http-response.error.ts
+++ src/modules/shared/infrastructure/errors/http-response.error.ts
@@ -1,8 +1,7 @@
-// shared/infrastructure/errors/http-response.error.ts
 export class HttpResponseError extends Error {
   constructor(
-    public readonly status: string,
-    message: string
+    message: string,
+    public readonly status: number
   ) {
     super(message)
     this.name = 'HttpResponseError'
