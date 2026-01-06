153917472356e7425751c57f8aa9767bedf2e097
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-01-06 14:01:37 +0000

    refactor: Simplificação do solicitação e construção da router api

--- src/app/api/operations/[operationId]/contracts/route.ts
+++ src/app/api/operations/[operationId]/contracts/route.ts
@@ -1,7 +1,6 @@
 import { GetContractsFactory } from '@/modules/contracts/infrastructure/factories/get-contracts.factory'
 import { PostContractFactory } from '@/modules/contracts/infrastructure/factories/post-contract.factory'
 import { PatchContractFactory } from '@/modules/contracts/infrastructure/factories/patch-contract.factory'
-import { HttpStatusCodeEnum } from '@/modules/authentication/domain/enums/status-codes.enum'
 import { RouterApiFactory } from '@/modules/api/infrastructure/factories/router-service-api.factory'
 import type { ContractEntity } from '@/modules/contracts/domain/entities/contract.entity'
 import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
@@ -11,29 +10,20 @@ const routerApi = RouterApiFactory.create()
 export const GET = routerApi.GET<UrlParams, ContractEntity[]>(
   async ({ operationId }) => {
     const getContracts = GetContractsFactory.create({ operationId })
-    const response = await getContracts.execute()
-    return { data: response, status: HttpStatusCodeEnum.OK }
+    return await getContracts.execute()
   }
 )
 
 export const POST = routerApi.POST<UrlParams>(async ({ operationId }, req) => {
   const contract = await req?.json()
   const postContract = PostContractFactory.create({ operationId })
-  await postContract.execute(contract)
-  return {
-    data: { success: true },
-    status: HttpStatusCodeEnum.OK
-  }
+  return await postContract.execute(contract)
 })
 
 export const PATCH = routerApi.PATCH<UrlParams>(
   async ({ operationId }, req) => {
     const contract = await req?.json()
     const patchContract = PatchContractFactory.create({ operationId })
-    await patchContract.execute(contract)
-    return {
-      data: { success: true },
-      status: HttpStatusCodeEnum.OK
-    }
+    return await patchContract.execute(contract)
   }
 )
