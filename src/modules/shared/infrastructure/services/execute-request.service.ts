79aa03c21f5274b7b04f3b99e5032175781a477d
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-01-06 12:59:46 +0000

    refactor: Nomenclatura do nome da interface de resposta HTTP

--- src/modules/shared/infrastructure/services/execute-request.service.ts
+++ src/modules/shared/infrastructure/services/execute-request.service.ts
@@ -1,11 +1,11 @@
 import type { HttpClientGateway } from '../../domain/gateways/http-client.gateway'
 import type { HttpRequestConfig } from '../../domain/interfaces/http-request-config.interface'
-import type { HttpResponse } from '../../domain/interfaces/http-response.interface'
+import type { HttpResponseInterface } from '../../domain/interfaces/http-response.interface'
 
 export class ExecuteRequest {
   constructor(private httpClient: HttpClientGateway) {}
 
-  async execute<T>(config: HttpRequestConfig): Promise<HttpResponse<T>> {
+  async execute<T>(config: HttpRequestConfig): Promise<HttpResponseInterface<T>> {
     return this.httpClient.request<T>(config)
   }
 }
