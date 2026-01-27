6de258c150593fe6c3f5aaa9b8d67dcf100fc633
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-01-27 17:58:53 +0000

    refactor: Adaptação da listagem de unidades de processamento

--- src/modules/api/infrastructure/services/get-processing-unit-router-api.service.ts
+++ src/modules/api/infrastructure/services/get-processing-unit-router-api.service.ts
@@ -1,29 +1,24 @@
 import type { ExecuteRequest } from '@/modules/shared/infrastructure/services/execute-request.service'
 import type { HttpRequestConfig } from '@/modules/shared/domain/interfaces/http-request-config.interface'
-import type { HttpResponseInterface } from '@/modules/shared/domain/interfaces/http-response.interface'
 import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
 import type { ProcessingUnitEntity } from '@/modules/processing-units/domain/entities/processing-unit.entity'
 import type { GetProcessingUnitRouterApiGateway } from '../../domain/gateways/get-processing-unit-router-api.gateway'
+import type { HttpResponseInterface } from '@/modules/shared/domain/interfaces/http-response.interface'
 
 export class GetProcessingUnitRouterApiService implements GetProcessingUnitRouterApiGateway {
   constructor(
     private readonly executeRequest: ExecuteRequest,
     private readonly params: UrlParams
   ) {}
-  getHttpRequestConfig({
-    operationId,
-    contractId
-  }: UrlParams): HttpRequestConfig {
+  getHttpRequestConfig(): HttpRequestConfig {
     return {
       method: 'GET',
-      url: `api/operations/${operationId}/contracts/${contractId}/processing-units`
+      url: `api/operations/${this.params.operationId}/contracts/${this.params.contractId}/processing-units`
     }
   }
 
-  async execute(): Promise<ProcessingUnitEntity[]> {
-    const settingsAuthHTTP = this.getHttpRequestConfig(this.params)
-    const { data }: HttpResponseInterface<ProcessingUnitEntity[]> =
-      await this.executeRequest.execute(settingsAuthHTTP)
-    return data
+  async execute(): Promise<HttpResponseInterface<ProcessingUnitEntity[]>> {
+    const settingsAuthHTTP = this.getHttpRequestConfig()
+    return await this.executeRequest.execute(settingsAuthHTTP)
   }
 }
