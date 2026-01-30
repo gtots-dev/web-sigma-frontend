9a1d65cbd09435ff7362dfa3655cb02247e9366f
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-01-30 18:48:51 +0000

    refactor: Adaptação para retorno de requisições GET

--- src/modules/api/domain/gateways/get-contract-lanes-router-api.gateway.ts
+++ src/modules/api/domain/gateways/get-contract-lanes-router-api.gateway.ts
@@ -1,5 +1,6 @@
 import type { LaneWithPointAndGroupInterface } from '@/modules/lanes/domain/interfaces/lane-with-point-and-group.interface'
+import type { HttpResponseInterface } from '@/modules/shared/domain/interfaces/http-response.interface'
 
 export interface GetContractLanesRouterApiGateway {
-  execute(): Promise<LaneWithPointAndGroupInterface[]>
+  execute(): Promise<HttpResponseInterface<LaneWithPointAndGroupInterface[]>>
 }
