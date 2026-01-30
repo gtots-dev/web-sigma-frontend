ddfdc2a4a03b765436eb04f54abe87234c6c3a71
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-01-30 15:15:03 +0000

    refactor: Adaptação das respostas dos Gateways para promises do backend

--- src/modules/api/domain/gateways/get-operations-router-api.gateway.ts
+++ src/modules/api/domain/gateways/get-operations-router-api.gateway.ts
@@ -1,5 +1,6 @@
-import type { OperationEntity } from "@/modules/operations/domain/entities/operation.entity";
+import type { OperationEntity } from '@/modules/operations/domain/entities/operation.entity'
+import type { HttpResponseInterface } from '@/modules/shared/domain/interfaces/http-response.interface'
 
 export interface GetOperationsRouterApiGateway {
-  execute(): Promise<OperationEntity[]>
+  execute(): Promise<HttpResponseInterface<OperationEntity[]>>
 }
