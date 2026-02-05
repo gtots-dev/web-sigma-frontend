c0417ed42cfe0b6e4d402a17929482a4d36fc790
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-02-05 17:03:08 +0000

    refactor: Mudanças no envio e recebimento de dados de cada função relacionadas ao usuário

--- src/modules/users/presentation/stores/user.store.ts
+++ src/modules/users/presentation/stores/user.store.ts
@@ -4,16 +4,22 @@ import type { UserEntity } from '../../domain/entities/user.entity'
 import { PatchUserRouterApiFactory } from '@/modules/api/infrastructure/factories/patch-user-router-api.factory'
 import { PostUserRouterApiFactory } from '@/modules/api/infrastructure/factories/post-user-router-api.factory'
 import { HttpResponseError } from '@/modules/shared/infrastructure/errors/http-response.error'
-import type { UserWithFiles } from '../../domain/types/user-with-files'
 import type { UserEnableAndDisableInterface } from '../../domain/interfaces/user-enable-and-disable.interface'
 import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
 import { PatchUserStatusRouterApiFactory } from '@/modules/api/infrastructure/factories/patch-user-status-router-api.factory'
+import type { HttpResponseInterface } from '@/modules/shared/domain/interfaces/http-response.interface'
 
 type UserState = {
   users: UserEntity[]
   getUsers: ({ operationId }: UrlParams) => Promise<void>
-  addUser: ({ operationId }: UrlParams, user: UserEntity) => Promise<void>
-  patchUser: ({ operationId }: UrlParams, user: UserWithFiles) => Promise<void>
+  addUser: (
+    { operationId }: UrlParams,
+    user: UserEntity
+  ) => Promise<HttpResponseInterface<UserEntity>>
+  patchUser: (
+    { operationId, userId }: UrlParams,
+    user: UserEntity
+  ) => Promise<void>
   updateUserStatus: (
     { operationId }: UrlParams,
     userEnableAndDisable: UserEnableAndDisableInterface
@@ -40,16 +46,17 @@ export const useUserStore = create<UserState>((set) => ({
       const postUsersRouterApiFactory = PostUserRouterApiFactory.create({
         operationId
       })
-      await postUsersRouterApiFactory.execute(user)
+      return await postUsersRouterApiFactory.execute(user)
     } catch (error) {
       if (error instanceof HttpResponseError) throw error
     }
   },
 
-  patchUser: async ({ operationId }: UrlParams, user: UserWithFiles) => {
+  patchUser: async ({ operationId, userId }: UrlParams, user: UserEntity) => {
     try {
       const patchUsersRouterApiFactory = PatchUserRouterApiFactory.create({
-        operationId
+        operationId,
+        userId
       })
       await patchUsersRouterApiFactory.execute(user)
     } catch (error) {
