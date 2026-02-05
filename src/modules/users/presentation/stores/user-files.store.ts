f65b45022a338124035d7e0267e3b14a1a543dbd
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-02-05 16:54:00 +0000

    feat: Função de envio de arquivo separado

--- src/modules/users/presentation/stores/user-files.store.ts
+++ src/modules/users/presentation/stores/user-files.store.ts
@@ -3,14 +3,36 @@ import type { UserFileInterface } from '../../domain/interfaces/user-file.interf
 import { GetUserFilesRouterApiFactory } from '@/modules/api/infrastructure/factories/get-user-files-router-api.factory'
 import { HttpResponseError } from '@/modules/shared/infrastructure/errors/http-response.error'
 import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
+import type { UserFilesInterface } from '../../domain/interfaces/user-files.interface'
+import { PostUserFilesRouterApiFactory } from '@/modules/api/infrastructure/factories/post-user-files-router-api.factory'
 
 type UserFilesState = {
   files: UserFileInterface[]
+  postUserFiles: (
+    { userId, operationId }: UrlParams,
+    files: UserFilesInterface
+  ) => Promise<void>
   getUserFiles: ({ userId, operationId }: UrlParams) => Promise<void>
 }
 
 export const useUserFilesStore = create<UserFilesState>((set) => ({
   files: [],
+
+  postUserFiles: async (
+    { userId, operationId }: UrlParams,
+    files: UserFilesInterface
+  ) => {
+    try {
+      const postUserFilesRouterApiFactory =
+        PostUserFilesRouterApiFactory.create({
+          userId,
+          operationId
+        })
+      await postUserFilesRouterApiFactory.execute(files)
+    } catch (error) {
+      if (error instanceof HttpResponseError) throw error
+    }
+  },
 
   getUserFiles: async ({ userId, operationId }: UrlParams) => {
     try {
