ab183ad0a8ac4f87cfcb6ccbc6fad42e86ba953b
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-02-05 17:02:28 +0000

    refactor: retornado o envio de arquivo junto a inserção do usuário

--- src/modules/users/presentation/schemas/post-user-form.schema.ts
+++ src/modules/users/presentation/schemas/post-user-form.schema.ts
@@ -2,6 +2,9 @@ import { MESSAGES_PASSWORD_RESET } from '@/modules/shared/presentation/messages/
 import { MESSAGES_USERS } from '@/modules/shared/presentation/messages/users'
 import { z } from 'zod'
 
+const VALID_TYPES = ['image/png', 'image/jpg', 'image/jpeg', 'application/pdf']
+const MAX_SIZE_MB = 10
+
 export const PostUserFormSchema = z.object({
   name: z
     .string()
@@ -46,6 +49,21 @@ export const PostUserFormSchema = z.object({
     .max(150, {
       message: MESSAGES_USERS['5.36']
     }),
+  files: z
+    .array(z.instanceof(File))
+    .refine(
+      (files: File[]) =>
+        files.every((file: File) => VALID_TYPES.includes(file.type)),
+      {
+        message: MESSAGES_USERS['5.20']
+      }
+    )
+    .refine(
+      (files: File[]) =>
+        files.every((file: File) => file.size <= MAX_SIZE_MB * 1024 * 1024),
+      { message: MESSAGES_USERS['5.31'] }
+    )
+    .optional(),
   days_passwd_reg_deadline: z
     .number()
     .int()
