a8a4554b73cf484af0f80e8ed8f17037996bfb63
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-02-05 17:01:30 +0000

    refactor: Desacoplamento de Usuario e Arquivos ao adicionar um novo usuário

--- src/modules/users/presentation/hooks/use-patch-user-submit.hook.ts
+++ src/modules/users/presentation/hooks/use-patch-user-submit.hook.ts
@@ -2,18 +2,36 @@ import { useCallback } from 'react'
 import { toast } from '@/modules/shared/presentation/components/hooks/use-toast'
 import { useUserStore } from '../stores/user.store'
 import { HttpResponseError } from '@/modules/shared/infrastructure/errors/http-response.error'
-import type { UserWithFiles } from '../../domain/types/user-with-files'
 import { useParams } from 'next/navigation'
 import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
+import type { UserWithFiles } from '../../domain/types/user-with-files'
+import { useUserFilesStore } from '../stores/user-files.store'
 
 export function useEditUserSubmit() {
+  const { postUserFiles } = useUserFilesStore()
   const { patchUser, getUsers } = useUserStore()
   const { operationId }: UrlParams = useParams()
 
   const onAction = useCallback(
     async (data: UserWithFiles, onSuccess: VoidFunction): Promise<void> => {
+      const user = {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        login_name: data.login_name,
+        company: data.company,
+        position: data.position,
+        days_passwd_reg_deadline: data.days_passwd_reg_deadline,
+        description: data.description
+      }
+
+      const files = {
+        files: data.files
+      }
       try {
-        await patchUser({ operationId }, data)
+        await patchUser({ operationId, userId: String(user.id) }, user)
+        if (files.files?.length > 0)
+          await postUserFiles({ operationId, userId: String(user.id) }, files)
         await getUsers({ operationId })
         toast({
           title: 'Usuário atualizado com sucesso!',
@@ -30,7 +48,7 @@ export function useEditUserSubmit() {
         }
       }
     },
-    [patchUser, getUsers, operationId]
+    [patchUser, postUserFiles, getUsers, operationId]
   )
 
   return { onAction }
