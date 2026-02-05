fb41d1277d4f9749daa0eb69074839377e5441fa
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-02-05 17:01:52 +0000

    refactor: Desacoplamento de Usuario e Arquivos ao adicionar um novo usuário

--- src/modules/users/presentation/hooks/use-post-user-submit.hook.ts
+++ src/modules/users/presentation/hooks/use-post-user-submit.hook.ts
@@ -6,16 +6,35 @@ import { useUserStore } from '../stores/user.store'
 import { HttpResponseError } from '@/modules/shared/infrastructure/errors/http-response.error'
 import { useParams } from 'next/navigation'
 import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
-import type { UserEntity } from '../../domain/entities/user.entity'
+import type { UserWithFiles } from '../../domain/types/user-with-files'
+import { useUserFilesStore } from '../stores/user-files.store'
 
 export function useAddUserSubmit() {
+  const { postUserFiles } = useUserFilesStore()
   const { addUser, getUsers } = useUserStore()
   const { operationId }: UrlParams = useParams()
 
   const onAction = useCallback(
-    async (data: UserEntity, onSuccess: VoidFunction): Promise<void> => {
+    async (data: UserWithFiles, onSuccess: VoidFunction): Promise<void> => {
+      const user = {
+        name: data.name,
+        email: data.email,
+        login_name: data.login_name,
+        company: data.company,
+        position: data.position,
+        days_passwd_reg_deadline: data.days_passwd_reg_deadline,
+        description: data.description
+      }
+      const files = {
+        files: data.files
+      }
       try {
-        await addUser({ operationId }, data)
+        const { data: responseUser } = await addUser({ operationId }, user)
+        if (files.files?.length > 0)
+          await postUserFiles(
+            { operationId, userId: String(responseUser.id) },
+            files
+          )
         await getUsers({ operationId })
         toast({
           title: 'Usuário adicionado com sucesso!',
