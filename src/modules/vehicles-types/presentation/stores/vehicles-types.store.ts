1b3f6b3e126abfd6cb702a23665f3cd4e56a0776
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-03-31 15:49:22 +0000

    feat: Função de cadastro de tipo de veículos

--- src/modules/vehicles-types/presentation/stores/vehicles-types.store.ts
+++ src/modules/vehicles-types/presentation/stores/vehicles-types.store.ts
@@ -3,10 +3,17 @@ import { HttpResponseError } from '@/modules/shared/infrastructure/errors/http-r
 import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
 import type { VehiclesTypesInterface } from '../../domain/interfaces/vehicle-type.interface'
 import { GetVehiclesTypesRouterApiFactory } from '@/modules/api/infrastructure/factories/get-vehicles-types-router-api.factory'
+import type { VehicleEntity } from '../../domain/entities/vehicle-types.entity'
+import { PostVehicleTypeRouterApiFactory } from '@/modules/api/infrastructure/factories/post-vehicle-type-router-api.factory'
+import type { HttpResponseInterface } from '@/modules/shared/domain/interfaces/http-response.interface'
 
 type VehiclesTypeState = {
   vehiclesTypes: VehiclesTypesInterface[]
   getVehiclesTypes: ({ operationId, contractId }: UrlParams) => Promise<void>
+  postVehicleType: (
+    { operationId, contractId }: UrlParams,
+    vehicleType: VehicleEntity
+  ) => Promise<HttpResponseInterface<VehicleEntity>>
 }
 
 export const useVehiclesTypeStore = create<VehiclesTypeState>((set) => ({
@@ -24,6 +31,22 @@ export const useVehiclesTypeStore = create<VehiclesTypeState>((set) => ({
       if (error instanceof HttpResponseError) {
         throw error
       }
+    }
+  },
+
+  postVehicleType: async (
+    { operationId, contractId }: UrlParams,
+    vehicleType: VehicleEntity
+  ) => {
+    try {
+      const postVehicleTypeRouterApiFactory =
+        PostVehicleTypeRouterApiFactory.create({
+          operationId,
+          contractId
+        })
+      return await postVehicleTypeRouterApiFactory.execute(vehicleType)
+    } catch (error) {
+      if (error instanceof HttpResponseError) throw error
     }
   }
 }))
