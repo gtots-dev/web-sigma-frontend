116004fb20930f1f221984d09c998125926314e7
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-03-31 14:47:38 +0000

    feat: Tabela de tipos de veículos

---
+++ src/modules/vehicles-types/presentation/components/table-vehicles-types/table-vehicles-types-header.component.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import {
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/modules/shared/presentation/components/shadcn/table'
+import { useMediaQuery } from '@/modules/shared/presentation/hooks/use-media-query'
+
+export function TableVehiclesTypesHeaderComponent() {
+  const isLarge = useMediaQuery('(min-width: 1024px)')
+  const isExtraLarge = useMediaQuery('(min-width: 1230px)')
+  const baseClass = 'h-9 px-5 sm:px-10 text-xs'
+
+  return (
+    <TableHeader>
+      <TableRow className="bg-muted !border-0">
+        <TableHead
+          {...(isLarge ? { colSpan: 2 } : {})}
+          className={`${baseClass} w-[30%] rounded-tl-lg`}
+        >
+          Nome
+        </TableHead>
+
+        {isExtraLarge && <TableHead className={baseClass}>Cor</TableHead>}
+
+        <TableHead className={`${baseClass} rounded-tr-lg text-right`}>
+          Opções
+        </TableHead>
+      </TableRow>
+    </TableHeader>
+  )
+}
