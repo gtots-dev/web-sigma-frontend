661a77cf4ef9aeace648214d2d7bd45ff4fdf4b9
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-03-31 14:21:44 +0000

    feat: Esquema do formulário de cadastro de tipos de veículos

---
+++ src/modules/vehicles-types/presentation/schemas/post-vehicle-form.schema.ts
@@ -0,0 +1,24 @@
+import { MESSAGES_VEHICLES } from '@/modules/shared/presentation/messages/vehicles'
+import { z } from 'zod'
+
+export const PostVehicleFormSchema = z.object({
+  name: z
+    .string()
+    .nonempty({
+      message: MESSAGES_VEHICLES['20.6']
+    })
+    .max(150, {
+      message: MESSAGES_VEHICLES['20.7']
+    }),
+  color: z
+    .string()
+    .nonempty({
+      message: MESSAGES_VEHICLES['20.8']
+    })
+    .regex(/^#([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/, {
+      message: MESSAGES_VEHICLES['20.9']
+    }),
+  code: z.number().optional()
+})
+
+export type PostVehicleFormType = z.infer<typeof PostVehicleFormSchema>
