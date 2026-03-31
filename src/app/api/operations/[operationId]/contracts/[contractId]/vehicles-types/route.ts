999781f5d5e3d11f6cb15f6cf096f0bd5ad01dd5
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-03-31 15:49:53 +0000

    feat: Rota de cadastro de tipos de veículos

--- src/app/api/operations/[operationId]/contracts/[contractId]/vehicles-types/route.ts
+++ src/app/api/operations/[operationId]/contracts/[contractId]/vehicles-types/route.ts
@@ -1,6 +1,7 @@
 import { RouterApiFactory } from '@/modules/api/infrastructure/factories/router-service-api.factory'
 import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
 import { GetVehiclesTypesFactory } from '@/modules/vehicles-types/infrastructure/factories/get-vehicles-types.factory'
+import { PostVehicleTypeFactory } from '@/modules/vehicles-types/infrastructure/factories/post-vehicle-type.factory'
 
 const routerApi = RouterApiFactory.create()
 
@@ -11,5 +12,16 @@ export const GET = routerApi.POST<UrlParams>(
       contractId
     })
     return await getVehiclesTypes.execute()
+  }
+)
+
+export const POST = routerApi.POST<UrlParams>(
+  async ({ operationId, contractId }, req) => {
+    const vehicleType = await req?.json()
+    const postVehicleType = PostVehicleTypeFactory.create({
+      operationId,
+      contractId
+    })
+    return await postVehicleType.execute(vehicleType)
   }
 )
