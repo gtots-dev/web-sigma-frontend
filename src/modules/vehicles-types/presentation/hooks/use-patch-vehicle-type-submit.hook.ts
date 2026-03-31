bdc4a9eef991f9f2b485096a5a43388002b44609
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-03-31 16:37:54 +0000

    feat: Hook de envio de dados de atualização do tipo de veiculo

---
+++ src/modules/vehicles-types/presentation/hooks/use-patch-vehicle-type-submit.hook.ts
@@ -0,0 +1,42 @@
+'use client'
+
+import { useCallback } from 'react'
+import { toast } from '@/modules/shared/presentation/components/hooks/use-toast'
+import { HttpResponseError } from '@/modules/shared/infrastructure/errors/http-response.error'
+import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
+import { useParams } from 'next/navigation'
+import type { VehicleTypeEntity } from '../../domain/entities/vehicle-types.entity'
+import { useVehiclesTypeStore } from '../stores/vehicles-types.store'
+
+export function usePatchVehicleTypeSubmit() {
+  const { operationId, contractId }: UrlParams = useParams()
+  const { getVehiclesTypes, patchVehicleType } = useVehiclesTypeStore()
+
+  const onAction = useCallback(
+    async (
+      vehicleType: VehicleTypeEntity,
+      onSuccess: VoidFunction
+    ): Promise<void> => {
+      try {
+        await patchVehicleType({ operationId, contractId }, vehicleType)
+        toast({
+          title: 'Tipos de veículos adicionado com sucesso!',
+          variant: 'success'
+        })
+        await getVehiclesTypes({ operationId, contractId })
+        onSuccess?.()
+      } catch (error) {
+        if (error instanceof HttpResponseError) {
+          toast({
+            title: 'Erro ao adicionar o tipos de veículos',
+            description: error.message,
+            variant: 'destructive'
+          })
+        }
+      }
+    },
+    [getVehiclesTypes, patchVehicleType, operationId, contractId]
+  )
+
+  return { onAction }
+}
