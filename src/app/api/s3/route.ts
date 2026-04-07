c079c8a2398dcbc9ee059c270b533269f3f0580c
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-04-07 08:20:56 +0000

    fix: Alterado o domínio para receber direto pelo retorno do backend

--- src/app/api/s3/route.ts
+++ src/app/api/s3/route.ts
@@ -11,8 +11,7 @@ export const POST = routerApi.POST<UrlParams>(async (_, req) => {
   const parsedUrl = new URL(formJson.url)
   const url = parsedUrl.pathname + parsedUrl.search
   const filename = parsedUrl.pathname.split('/').pop()
-
-  const httpClient = HttpClientFactory.create(process.env.HOST_S3)
+  const httpClient = HttpClientFactory.create(parsedUrl.origin)
   const executeRequest = ExecuteRequestFactory.create(httpClient)
   const { data: fileBlob } = await executeRequest.execute<File>({
     method: 'GET',
