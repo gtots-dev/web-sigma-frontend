1e2c40152123a8b713f32c844985f26556a59662
Juliano <jfperini@gmail.com>
2026-04-07 17:26:47 -0300

    Teste para para abrir o arquivo baixado diretamente da URL

--- src/modules/shared/presentation/hooks/use-download-file.ts
+++ src/modules/shared/presentation/hooks/use-download-file.ts
@@ -1,15 +1,14 @@
 import { useCallback } from 'react'
 
 export function useDownloadFile() {
-  const download = useCallback((file: File, fileName: string) => {
-    const url = URL.createObjectURL(file)
+  const download = useCallback((file: string, fileName: string) => {
+    const url = file
     const a = document.createElement('a')
     a.href = url
     a.download = fileName
     document.body.appendChild(a)
     a.click()
     document.body.removeChild(a)
-    URL.revokeObjectURL(url)
   }, [])
 
   return { download }
