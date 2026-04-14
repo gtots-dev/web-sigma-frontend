eb6d1c3ebf46e37f8c1aed3fafa22664c0de9049
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-04-14 14:51:39 +0000

    feat: Função de atualização do "access_token"

--- src/modules/authentication/infrastructure/auth/jwt-callback.auth.ts
+++ src/modules/authentication/infrastructure/auth/jwt-callback.auth.ts
@@ -1,16 +1,40 @@
-import type { User } from 'next-auth'
+import type { Session, User } from 'next-auth'
 import type { JWT } from 'next-auth/jwt'
+import { JwtTokenDecodeFactory } from '@/modules/shared/infrastructure/factories/jwt-decode.factory'
+
+type JWTCallbackParams = {
+  token: JWT
+  user?: User
+  trigger?: 'signIn' | 'update' | 'signUp'
+  session?: Session
+}
 
 export async function JWTCallbackAuth({
   token,
-  user
-}: {
-  token: JWT
-  user?: User
-}): Promise<JWT> {
-  if (user) {
-    token.access_token = user.accessToken as string
-    token.token_type = 'Bearer' as string
+  user,
+  trigger,
+  session
+}: JWTCallbackParams): Promise<JWT> {
+  const jwtFactory = JwtTokenDecodeFactory.create()
+
+  if (trigger === 'update' && session) {
+    return {
+      ...token,
+      ...session
+    }
   }
+
+  if (user) {
+    const accessToken = user.accessToken as string
+    const decoded = jwtFactory.decode(accessToken)
+
+    return {
+      ...token,
+      access_token: accessToken,
+      token_type: 'Bearer',
+      authType: decoded.type
+    }
+  }
+
   return token
 }
