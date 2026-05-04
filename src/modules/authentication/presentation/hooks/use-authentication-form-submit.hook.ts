48d048982e5c175a82f6118623600e2bb3b6defb
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-05-04 13:04:40 +0000

    refactor: Redirecionamento para seção de autenticação de dois fatores (Requisição embutida na pagina)

--- src/modules/authentication/presentation/hooks/use-authentication-form-submit.hook.ts
+++ src/modules/authentication/presentation/hooks/use-authentication-form-submit.hook.ts
@@ -8,11 +8,9 @@ import { AuthSignInFactory } from '../../infrastructure/factories/auth-sign-in.f
 import { AuthSignOutFactory } from '../../infrastructure/factories/auth-sign-out.factory'
 import { getSession } from 'next-auth/react'
 import type { Session } from 'next-auth'
-import { useTwoFactorStore } from '@/modules/two-factor/presentation/stores/two-factor.store'
 
 export function useAuthenticationFormSubmitHook() {
   const router = useRouter()
-  const { postTwoFactor } = useTwoFactorStore()
   const authSignIn = AuthSignInFactory.create()
   const authSignOut = AuthSignOutFactory.create()
 
@@ -28,8 +26,7 @@ export function useAuthenticationFormSubmitHook() {
       await authSignIn.signIn(data)
       const session = await getSession()
       if (!session) throw new Error('Sessão não encontrada')
-      if ((session as Session).authType === '2fa_pending') {
-        await postTwoFactor()
+      if ((session as Session).authType == '2fa_pending') {
         router.push(PATHNAMES.TWO_FACTOR)
         return
       }
