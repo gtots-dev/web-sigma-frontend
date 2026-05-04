aedd8db69aee49f3cd0099ce4912ce0ceab10866
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-05-04 14:06:37 +0000

    feat: Rota de verificação de dois fatores

--- src/app/api/2fa/verify/route.ts
+++ src/app/api/2fa/verify/route.ts
@@ -1,10 +1,13 @@
 import { RouterApiFactory } from '@/modules/api/infrastructure/factories/router-service-api.factory'
+import { applySetCookie } from '@/modules/shared/infrastructure/cookies/service/apply-set-cookie.service'
 import { PostTwoFactorVerifyFactory } from '@/modules/two-factor/infrastructure/factories/post-two-factor-verify.factory'
 
 const routerApi = RouterApiFactory.create()
 
 export const POST = routerApi.POST<void>(async (_, req) => {
-  const postTwoFactorVerify = PostTwoFactorVerifyFactory.create()
   const code = await req?.json()
-  return await postTwoFactorVerify.execute(code)
+  const postTwoFactorVerify = PostTwoFactorVerifyFactory.create()
+  const response = await postTwoFactorVerify.execute(code)
+  await applySetCookie(response.headers['set-cookie'])
+  return response
 })
