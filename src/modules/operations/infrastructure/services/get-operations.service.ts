25a5901eac39d41b79825a321724e34fd6989bda
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-05-04 14:12:03 -0300

    refactor: Remoção da dependência do AuthTokenProvider nos módulos de grupos, operações e permissões

--- src/modules/operations/infrastructure/services/get-operations.service.ts
+++ src/modules/operations/infrastructure/services/get-operations.service.ts
@@ -2,29 +2,23 @@ import type { ExecuteRequest } from '@/modules/shared/infrastructure/services/ex
 import type { GetOperationsGateway } from '../../domain/gateways/get-operations.gateway'
 import type { HttpRequestConfig } from '@/modules/shared/domain/interfaces/http-request-config.interface'
 import type { HttpResponseInterface } from '@/modules/shared/domain/interfaces/http-response.interface'
-import type { TokenEntities } from '@/modules/authentication/domain/entities/token.entity'
 import type { OperationEntity } from '../../domain/entities/operation.entity'
-import type { AuthTokenProvider } from '@/modules/api/infrastructure/providers/token.provider'
 
 export class GetOperationsService implements GetOperationsGateway {
   constructor(
-    private readonly executeRequest: ExecuteRequest,
-    private readonly auth: AuthTokenProvider
+    private readonly executeRequest: ExecuteRequest
   ) {}
 
-  getHttpRequestConfig(token: TokenEntities): HttpRequestConfig {
+  getHttpRequestConfig(): HttpRequestConfig {
     return {
       method: 'GET',
       url: '/operations',
-      headers: token.access_token && {
-        Authorization: `${token.token_type} ${token.access_token}`
-      }
+      requiresAuth: true
     }
   }
 
   async execute(): Promise<HttpResponseInterface<OperationEntity[]>> {
-    const token = await this.auth.getToken()
-    const settingsAuthHTTP = this.getHttpRequestConfig(token)
+    const settingsAuthHTTP = this.getHttpRequestConfig()
     return await this.executeRequest.execute(settingsAuthHTTP)
   }
 }
