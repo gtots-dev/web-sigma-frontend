fa4df8b8655487773eb1bb72f997a6a6251f08e8
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-05-04 19:26:34 +0000

    feat: Interceptor de injeção do cookie 'X-2FA-Code'

--- src/modules/shared/infrastructure/factories/request.factory.ts
+++ src/modules/shared/infrastructure/factories/request.factory.ts
@@ -1,13 +1,15 @@
 import type { FetchHttpClient } from '../http/fetch/fetch-http-client'
 import { nextAuthInterceptor } from '../interceptors/next-auth-interceptor'
 import { nextCookieInterceptor } from '../interceptors/next-cookie.interceptor'
+import { next2faInterceptor } from '../interceptors/next-2fa.interceptor'
 import { ExecuteRequest } from '../services/execute-request.service'
 
 export class ExecuteRequestFactory {
   static create(httpClient: FetchHttpClient): ExecuteRequest {
     return new ExecuteRequest(httpClient, [
       nextAuthInterceptor,
-      nextCookieInterceptor
+      nextCookieInterceptor,
+      next2faInterceptor
     ])
   }
 }
