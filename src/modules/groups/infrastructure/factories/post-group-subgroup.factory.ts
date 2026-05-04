08bba8679f9ccd3d360c05d6fe96f3cd599aeb17
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-05-04 14:06:08 -0300

    refactor: Remoção da dependência do AuthTokenProvider nos módulos de grupos e permissões

--- src/modules/groups/infrastructure/factories/post-group-subgroup.factory.ts
+++ src/modules/groups/infrastructure/factories/post-group-subgroup.factory.ts
@@ -1,7 +1,6 @@
 import { HttpClientFactory } from '@/modules/shared/infrastructure/factories/http-client.factory'
 import { ExecuteRequestFactory } from '@/modules/shared/infrastructure/factories/request.factory'
 import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
-import { AuthTokenFactory } from '@/modules/api/infrastructure/factories/auth-token.factory'
 import type { PostGroupSubgroupGateway } from '../../domain/gateways/post-group-subgroup.gateway'
 import { PostGroupSubgroupService } from '../services/post-group-subgroup.service'
 
@@ -9,7 +8,6 @@ export class PostGroupSubgroupFactory {
   static create(params: UrlParams): PostGroupSubgroupGateway {
     const httpClient = HttpClientFactory.create(process.env.HOST_API)
     const executeRequest = ExecuteRequestFactory.create(httpClient)
-    const authToken = AuthTokenFactory.create()
-    return new PostGroupSubgroupService(executeRequest, authToken, params)
+    return new PostGroupSubgroupService(executeRequest, params)
   }
 }
