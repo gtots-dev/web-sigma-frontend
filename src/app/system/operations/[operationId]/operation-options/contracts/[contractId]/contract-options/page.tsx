0b2bd64f21824e4dc142e0112260ec2b28c7ff71
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-05-12 11:29:59 +0000

    feat: Opção de redirecionamento

--- src/app/system/operations/[operationId]/operation-options/contracts/[contractId]/contract-options/page.tsx
+++ src/app/system/operations/[operationId]/operation-options/contracts/[contractId]/contract-options/page.tsx
@@ -8,7 +8,7 @@ import { PATHNAMES } from '@/modules/shared/infrastructure/configs/pathnames.con
 import { MESSAGES_OPTIONS_CONTRACT } from '@/modules/shared/presentation/messages/options-contract'
 import { PermissionEnum } from '@/modules/system/domain/enums/permissions.enum'
 import { loadAuthContext } from '@/modules/system/presentation/contexts/load-auth.context'
-import { ChartLine, Settings, type LucideIcon } from 'lucide-react'
+import { MonitorDot, ChartLine, Settings, type LucideIcon } from 'lucide-react'
 import { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
 import { SectionRedirectLink } from '@/modules/shared/presentation/components/section-redirect-link'
 
@@ -76,6 +76,16 @@ export default async function ContractOptionsPage({
         Number(rawContractId)
       ),
       icon: ChartLine,
+      accessAllowed: true
+    },
+    {
+      title: MESSAGES_OPTIONS_CONTRACT['16.11'],
+      description: MESSAGES_OPTIONS_CONTRACT['16.12'],
+      pathName: PATHNAMES.MONITORING(
+        Number(rawOperationId),
+        Number(rawContractId)
+      ),
+      icon: MonitorDot,
       accessAllowed: true
     }
   ]
