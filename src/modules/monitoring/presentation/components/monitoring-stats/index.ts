c786580e3adcc28a65121e0381a6f7b0a645f05f
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-06-26 14:08:40 -0300

    refactor: Modularizado e segmentado componente de estatísticas de monitoramento

--- src/modules/monitoring/presentation/components/monitoring-stats/index.ts
+++ src/modules/monitoring/presentation/components/monitoring-stats/index.ts
@@ -1,1 +1,8 @@
-export { MonitoringStats as Stats } from './monitoring-stats.component'
+import { MonitoringStatsRoot } from './monitoring-stats-root.component'
+import { MonitoringStatsContent } from './monitoring-stats-content.component'
+import { MonitoringStatsTriggerMaximized } from './monitoring-stats-trigger-maximized.component'
+
+export const Stats = Object.assign(MonitoringStatsRoot, {
+  Content: MonitoringStatsContent,
+  TriggerMaximized: MonitoringStatsTriggerMaximized
+})
