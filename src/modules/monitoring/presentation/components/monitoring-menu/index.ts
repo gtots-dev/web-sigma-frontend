0a36c8603a255963f4ac81d74f0c46d3c9d46285
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-06-26 16:48:23 -0300

    clean(monitoring): remove unused menu subcomponents

--- src/modules/monitoring/presentation/components/monitoring-menu/index.ts
+++ src/modules/monitoring/presentation/components/monitoring-menu/index.ts
@@ -1,25 +1,14 @@
 import { MonitoringMenuRoot } from './monitoring-menu-root.component'
 import { MonitoringMenuAccentBar } from './monitoring-menu-accent-bar.component'
-import { MonitoringMenuStatusBar } from './monitoring-menu-status-bar.component'
 import { MonitoringMenuHeader } from './monitoring-menu-header.component'
-import { MonitoringMenuHealth } from './monitoring-menu-health.component'
 import { MonitoringMenuConnection } from './monitoring-menu-connection.component'
-import { MonitoringMenuMeta } from './monitoring-menu-meta.component'
 import { MonitoringMenuFooter } from './monitoring-menu-footer.component'
 import { MonitoringMenuDetails } from './monitoring-menu-details.component'
 
-import { MonitoringMenuPayload } from './monitoring-menu-payload.component'
-import { MonitoringMenuTimestamp } from './monitoring-menu-timestamp.component'
-
 export const Menu = Object.assign(MonitoringMenuRoot, {
   AccentBar: MonitoringMenuAccentBar,
-  StatusBar: MonitoringMenuStatusBar,
   Header: MonitoringMenuHeader,
-  Health: MonitoringMenuHealth,
   Connection: MonitoringMenuConnection,
-  Meta: MonitoringMenuMeta,
-  Payload: MonitoringMenuPayload,
-  Timestamp: MonitoringMenuTimestamp,
   Footer: MonitoringMenuFooter,
   Details: MonitoringMenuDetails
 })
