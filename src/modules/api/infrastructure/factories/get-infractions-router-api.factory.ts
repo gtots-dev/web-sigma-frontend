bf502fe120cbfc5e18bdc0f62fb0d368c759d80f
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-07-29 18:08:41 +0000

    feat: Estendido os parâmetros para possibilidade de paginação e filtros

--- src/modules/api/infrastructure/factories/get-infractions-router-api.factory.ts
+++ src/modules/api/infrastructure/factories/get-infractions-router-api.factory.ts
@@ -3,9 +3,16 @@ import { ExecuteRequestFactory } from '@/modules/shared/infrastructure/factories
 import type { UrlParams } from '@/modules/shared/domain/interfaces/url-params.interface'
 import type { GetInfractionsRouterApiGateway } from '../../domain/gateways/get-infractions-router-api.gateway'
 import { GetInfractionsRouterApiService } from '../services/get-infractions-router-api.service'
+import type { PaginationInterface } from '@/modules/shared/domain/interfaces/pagination.interfaces'
+import type { InfractionsFiltersInterface } from '@/modules/infractions/domain/interfaces/infractions-filters.interface'
+
+export interface GetInfractionsParams extends UrlParams {
+  pagination: PaginationInterface
+  filters?: InfractionsFiltersInterface
+}
 
 export class GetInfractionsRouterApiFactory {
-  static create(params: UrlParams): GetInfractionsRouterApiGateway {
+  static create(params: GetInfractionsParams): GetInfractionsRouterApiGateway {
     const httpClient = HttpClientFactory.create('/')
     const executeRequest = ExecuteRequestFactory.create(httpClient)
     return new GetInfractionsRouterApiService(executeRequest, params)
