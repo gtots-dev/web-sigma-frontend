5adec50aecf7ec6994e5d84c9f9226f0a1be1f1f
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-07-30 17:59:00 +0000

    refactor: Utilização do filtro global

--- src/modules/infractions/presentation/components/infractions-filters/index.ts
+++
@@ -1,5 +0,0 @@
-import { SystemFilters } from '@/modules/shared/presentation/components/system-filters'
-
-export const InfractionsFilters = SystemFilters
-
-export * from '@/modules/shared/presentation/components/system-filters'
