055367d73a94d292a05bd651221c9ff93ad897b4
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-08-04 10:36:46 +0000

    refactor: Alterado tag pai para atender o hydration

--- src/modules/system/presentation/components/header-section/header-section-description.component.tsx
+++ src/modules/system/presentation/components/header-section/header-section-description.component.tsx
@@ -1,7 +1,9 @@
 import type { ComponentProps } from 'react'
 import { cn } from '@/modules/shared/presentation/lib/utils'
+import { Separator } from '@/modules/shared/presentation/components/shadcn/separator'
 
-export interface HeaderSectionDescriptionComponentProps extends ComponentProps<'p'> {}
+export interface HeaderSectionDescriptionComponentProps
+  extends ComponentProps<'div'> {}
 
 export function HeaderSectionDescriptionComponent({
   className,
@@ -9,14 +11,16 @@ export function HeaderSectionDescriptionComponent({
   ...props
 }: HeaderSectionDescriptionComponentProps) {
   return (
-    <p
+    <div
       className={cn(
         'text-xs leading-[1.3rem] text-muted-foreground flex items-center gap-1.5 truncate',
         className
       )}
       {...props}
     >
+      <span className="w-1.5 h-1.5 rounded-full bg-primary/60 shrink-0 hidden lg:inline-block" />
+      <Separator orientation="vertical" />
       <span className="truncate">{children}</span>
-    </p>
+    </div>
   )
 }
