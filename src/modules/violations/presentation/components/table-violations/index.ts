4255a4f282dc6e2932bb67c32e5c734dfb14a39b
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-08-05 23:59:57 +0000

    feat: Tabela de listagem de violações

---
+++ src/modules/violations/presentation/components/table-violations/index.ts
@@ -0,0 +1,11 @@
+import { TableViolationsBodyComponent } from './table-violations-body.component'
+import { TableViolationsHeaderComponent } from './table-violations-header.component'
+import { TableViolationsItemComponent } from './table-violations-item.component'
+import { TableViolationsRootComponent } from './table-violations-root.component'
+
+export const TableViolations = {
+  Root: TableViolationsRootComponent,
+  Header: TableViolationsHeaderComponent,
+  Body: TableViolationsBodyComponent,
+  Item: TableViolationsItemComponent
+}
