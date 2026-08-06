baa81296abc399cfeaafd528eabfbf1b5d12b9a6
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-08-06 01:46:41 +0000

    feat: Componente de menu de criar de restrição

---
+++ src/modules/restrictions/presentation/components/post-restriction-menu/post-restriction-menu.component.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { Button } from '@/modules/shared/presentation/components/shadcn/button'
+import { PostRestrictionMenu } from '.'
+import { usePostRestrictionMenuContext } from '../../contexts/post-restriction-menu.context'
+import { usePostRestrictionSubmit } from '../../hooks/use-post-restriction-submit.hook'
+import type { RestrictionEntity } from '../../domain/entities/restriction.entity'
+import { RestrictionForm } from '../restriction-form'
+
+interface PostRestrictionMenuComponentProps {
+  title: string
+  description: string
+}
+
+export function PostRestrictionMenuComponent({
+  title,
+  description
+}: PostRestrictionMenuComponentProps) {
+  const { isOpen, close } = usePostRestrictionMenuContext()
+  const { onAction } = usePostRestrictionSubmit()
+
+  return (
+    <PostRestrictionMenu.Root isOpen={isOpen} close={close}>
+      <PostRestrictionMenu.Content>
+        <PostRestrictionMenu.Header title={title} description={description} />
+        <RestrictionForm.Form>
+          <RestrictionForm.Input.Name require />
+          <RestrictionForm.Input.Code require />
+        </RestrictionForm.Form>
+
+        <PostRestrictionMenu.Footer>
+          <Button
+            className="w-full sm:w-[150px]"
+            variant="outline"
+            onClick={close}
+          >
+            Cancelar
+          </Button>
+          <RestrictionForm.Submit<RestrictionEntity>
+            onSubmit={(data: RestrictionEntity) => onAction(data, close)}
+          />
+        </PostRestrictionMenu.Footer>
+      </PostRestrictionMenu.Content>
+    </PostRestrictionMenu.Root>
+  )
+}
