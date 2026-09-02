35f3bac0b28f70bf0693ab8158f0c40dbd46a862
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-09-02 14:42:48 +0000

    feat: Gateways de listagem de capturas

--- src/modules/infractions/domain/gateways/post-infractions.gateway.ts
+++ src/modules/infractions/domain/gateways/post-infractions.gateway.ts
@@ -1,6 +1,9 @@
 import type { Infraction } from '../interfaces/infraction.interface'
 import type { PostInfractionsPayload } from '../interfaces/post-infractions-payload.interface'
+import type { HttpResponseInterface } from '@/modules/shared/domain/interfaces/http-response.interface'
 
 export interface PostInfractionsGateway {
-  execute(payload?: PostInfractionsPayload): Promise<Infraction[]>
+  execute(
+    payload?: PostInfractionsPayload
+  ): Promise<HttpResponseInterface<Infraction[]>>
 }
