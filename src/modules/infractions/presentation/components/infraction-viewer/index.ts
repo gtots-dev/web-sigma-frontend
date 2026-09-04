ec3a4561c4a786b07769ce555462bd341fca1f63
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-09-04 11:29:47 +0000

    refactor: Separado componentes em responsabilidades únicas

--- src/modules/infractions/presentation/components/infraction-viewer/index.ts
+++ src/modules/infractions/presentation/components/infraction-viewer/index.ts
@@ -1,11 +1,13 @@
+import { InfractionImageViewer } from '../infraction-image-viewer'
+import { InfractionVideoViewer } from '../infraction-video-viewer'
 import { InfractionViewerRoot } from './infraction-viewer-root.component'
-import { InfractionViewerImage } from './infraction-viewer-image.component'
 import { InfractionViewerEmpty } from './infraction-viewer-empty.component'
 import { InfractionViewerStrip } from './infraction-viewer-strip.component'
 
 export const InfractionViewer = {
   Root: InfractionViewerRoot,
-  Image: InfractionViewerImage,
+  Image: InfractionImageViewer,
+  Video: InfractionVideoViewer,
   Empty: InfractionViewerEmpty,
   Strip: InfractionViewerStrip,
 }
