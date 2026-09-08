7676a92b42b956fafd0f4870a51221747ecc4886
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-09-08 09:03:26 +0000

    fix: Nomenclatura da classe de estilo incorreta

--- src/modules/infractions/presentation/components/infraction-image-viewer/infraction-image-viewer-root.component.tsx
+++ src/modules/infractions/presentation/components/infraction-image-viewer/infraction-image-viewer-root.component.tsx
@@ -16,7 +16,7 @@ export function InfractionImageViewerRoot({
   return (
     <div
       onClick={onClick}
-      className={`flex-1 min-h-0 flex items-center justify-center p-4 overflow-hidden relative group rounded-t-xl border-b border-border/40 select-none ${
+      className={`flex-1 min-h-0 flex items-center justify-center p-4 overflow-hidden relative group rounded-xl select-none ${
         onClick ? 'cursor-pointer' : ''
       } ${className}`}
     >
