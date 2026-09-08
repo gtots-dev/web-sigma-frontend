41249f9eba2713dd48bf536e097cd027b662bd62
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-09-08 09:06:10 +0000

    refactor: Removido camada main, para otimização de componente

--- src/modules/infractions/presentation/components/infraction-video-viewer/index.ts
+++ src/modules/infractions/presentation/components/infraction-video-viewer/index.ts
@@ -1,22 +1,11 @@
-import { InfractionVideoViewerMain } from './infraction-video-viewer-main.component'
 import { InfractionVideoViewerRoot } from './infraction-video-viewer-root.component'
 import { InfractionVideoViewerPlayer } from './infraction-video-viewer-player.component'
 import { InfractionVideoViewerSequence } from './infraction-video-viewer-sequence.component'
-import {
-  InfractionVideoViewerProvider,
-  useInfractionVideoViewerContext
-} from '../../contexts/infraction-video-viewer-context'
+import { InfractionVideoViewerProvider } from '../../contexts/infraction-video-viewer-context'
 
-export const InfractionVideoViewer = Object.assign(InfractionVideoViewerMain, {
+export const InfractionVideoViewer = {
   Root: InfractionVideoViewerRoot,
-  Main: InfractionVideoViewerMain,
   Player: InfractionVideoViewerPlayer,
   Sequence: InfractionVideoViewerSequence,
   Provider: InfractionVideoViewerProvider
-})
-
-export * from './infraction-video-viewer-root.component'
-export * from './infraction-video-viewer-player.component'
-export * from './infraction-video-viewer-sequence.component'
-export * from './infraction-video-viewer-main.component'
-export * from '../../contexts/infraction-video-viewer-context'
+}
