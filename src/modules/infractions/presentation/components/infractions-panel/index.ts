1229a5390fcddddd92fa5c310c459f0a954ceb80
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-09-08 09:09:31 +0000

    feat: Menus laterais do painel de visualização de captura com função de retração

--- src/modules/infractions/presentation/components/infractions-panel/index.ts
+++ src/modules/infractions/presentation/components/infractions-panel/index.ts
@@ -1,7 +1,10 @@
 import { InfractionsPanelRoot } from './infractions-panel-root.component'
 import { InfractionsPanelEmpty } from './infractions-panel-empty.component'
+import { InfractionsPanelRightSidebar } from './infractions-panel-right-sidebar.component'
 
 export const InfractionsPanel = {
   Root: InfractionsPanelRoot,
   Empty: InfractionsPanelEmpty,
+  RightSidebar: InfractionsPanelRightSidebar
 }
+
