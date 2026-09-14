7c9be96d4885ef78839b713b32e4eda9050989a8
Lucas Martinez <lucas.martinez@gtots.com.br>
2026-09-14 16:11:28 +0000

    feat: Log caso inicie com erro na variável de ambiente

--- src/modules/shared/infrastructure/factories/web-socket.factory.ts
+++ src/modules/shared/infrastructure/factories/web-socket.factory.ts
@@ -6,6 +6,13 @@ export class WebSocketFactory {
   static create<TIncoming = unknown, TOutgoing = unknown>(
     baseURL: string
   ): WebSocketGateway<TIncoming, TOutgoing> {
+    
+    if (!baseURL) {
+      console.error(
+        '[WebSocketFactory] HOST_API não está definida. O WebSocket não será instanciado com base URL válida.'
+      )
+    }
+
     return new NativeWebSocketService<TIncoming, TOutgoing>(
       [webSocketAuthInterceptor],
       baseURL
