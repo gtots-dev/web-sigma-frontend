28dd4aeee127386eb22117a3f330f8619f2edd52
Juliano <jfperini@gmail.com>
2026-09-16 14:33:09 -0300

    feat: Adiciona nova variável de ambiente

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   env: {
     HOST_API: process.env.HOST_API,
+    NEXT_PUBLIC_HOST_API: process.env.NEXT_PUBLIC_HOST_API,
     AUTH_SECRET: process.env.AUTH_SECRET,
     SECRET_KEY_ACCESS_TOKEN: process.env.SECRET_KEY_ACCESS_TOKEN
   },
